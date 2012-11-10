b39bb379c4f48a405524029bdf9610bc838fdc99
jvanrosmalen <jazz_cama@hotmail.com>
2012-11-10 14:25:44 +0100

    Heavy, normal and light Railgun. Railgun Hyperion.

---
+++ source/server/model/ships/EA/hyperionRail.php
@@ -0,0 +1,66 @@
+<?php
+class HyperionRail extends BaseShip{
+    
+    function __construct($id, $userid, $name,  $slot){
+        parent::__construct($id, $userid, $name,  $slot);
+        
+	$this->pointCost = 725;
+	$this->faction = "EA";
+        $this->phpclass = "HyperionRail";
+        $this->imagePath = "img/ships/hyperion.png";
+        $this->shipClass = "Hyperion Rail Cruiser";
+        $this->shipSizeClass = 3;
+        
+        $this->forwardDefense = 14;
+        $this->sideDefense = 16;
+        
+        $this->turncost = 1;
+        $this->turndelaycost = 1;
+        $this->accelcost = 3;
+        $this->rollcost = 2;
+        $this->pivotcost = 3;
+		
+        
+         
+        $this->addPrimarySystem(new Reactor(5, 23, 0, 0));
+        $this->addPrimarySystem(new CnC(5, 16, 0, 0));
+        $this->addPrimarySystem(new Scanner(5, 18, 3, 6));
+        $this->addPrimarySystem(new Engine(6, 18, 0, 7, 4));
+	$this->addPrimarySystem(new Hangar(5, 8));
+        $this->addPrimarySystem(new StdParticleBeam(2, 4, 1, 0, 360));
+	$this->addPrimarySystem(new StdParticleBeam(2, 4, 1, 0, 360));
+	$this->addPrimarySystem(new StdParticleBeam(2, 4, 1, 0, 360));
+        
+		
+        $this->addFrontSystem(new Thruster(3, 8, 0, 3, 1));
+        $this->addFrontSystem(new Thruster(3, 8, 0, 3, 1));
+        $this->addFrontSystem(new InterceptorMkI(2, 4, 1, 240, 60));
+        $this->addFrontSystem(new InterceptorMkI(2, 4, 1, 300, 120));
+	$this->addFrontSystem(new RailGun(3, 9, 6, 300, 60));
+
+        $this->addAftSystem(new Thruster(4, 9, 0, 2, 2));
+	$this->addAftSystem(new Thruster(4, 12, 0, 3, 2));
+        $this->addAftSystem(new Thruster(4, 9, 0, 2, 2));
+        $this->addAftSystem(new JumpEngine(4, 16, 3, 24));
+	$this->addAftSystem(new InterceptorMkI(2, 4, 1, 120, 300));
+        $this->addAftSystem(new InterceptorMkI(2, 4, 1, 60, 240));
+        
+	$this->addLeftSystem(new Thruster(3, 13, 0, 5, 3));
+	$this->addLeftSystem(new RailGun(4, 9, 6, 240, 0));
+	$this->addLeftSystem(new RailGun(4, 9, 6, 180, 300));
+	$this->addLeftSystem(new MediumPulse(3, 6, 3, 180, 0));
+		
+	$this->addRightSystem(new Thruster(3, 13, 0, 5, 4));
+	$this->addRightSystem(new RailGun(4, 9, 6, 0, 120));
+	$this->addRightSystem(new RailGun(4, 9, 6, 60, 180));
+	$this->addRightSystem(new MediumPulse(3, 6, 3, 0, 180));
+        
+        //0:primary, 1:front, 2:rear, 3:left, 4:right;
+        $this->addFrontSystem(new Structure( 5, 52));
+        $this->addAftSystem(new Structure( 4, 42));
+        $this->addLeftSystem(new Structure( 4, 60));
+        $this->addRightSystem(new Structure( 4, 60));
+        $this->addPrimarySystem(new Structure( 5, 54));
+    }
+}
+?>
--- source/server/model/weapons/matter.php
+++ source/server/model/weapons/matter.php
@@ -1,58 +1,129 @@
 <?php
 
-	class Matter extends Weapon{
-	
-		
-	
-		function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
+    class Matter extends Weapon
+    {
+        function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc)
+        {
             parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
         }
-		
-		protected function getSystemArmour($system, $gamedata, $fireOrder){
-			return 0;
-		}
-		
-		protected function getOverkillSystem($target, $shooter, $system, $pos, $fireOrder, $gamedata){
-			return null;
-		}
-		
-		public function setSystemDataWindow($turn){
 
-			$this->data["Weapon type"] = "Matter";
-			$this->data["Damage type"] = "Standard";
-			
-			parent::setSystemDataWindow($turn);
-		}
-    
-	
-	
-	}
+        protected function getSystemArmour($system, $gamedata, $fireOrder)
+        {
+            return 0;
+        }
 
-		class MatterCannon extends Matter{
+        protected function getOverkillSystem($target, $shooter, $system, $pos, $fireOrder, $gamedata)
+        {
+            return null;
+        }
 
-		public $name = "matterCannon";
+        public function setSystemDataWindow($turn)
+        {
+            $this->data["Weapon type"] = "Matter";
+            $this->data["Damage type"] = "Standard";
+
+            parent::setSystemDataWindow($turn);
+        }
+    }
+
+    class MatterCannon extends Matter
+    {
+	public $name = "matterCannon";
         public $displayName = "Matter Cannon";
         public $animation = "trail";
         public $animationColor = array(250, 250, 190);
-		public $projectilespeed = 25;
+	public $projectilespeed = 25;
         public $animationWidth = 4;
-		public $animationExplosionScale = 0.20;
+	public $animationExplosionScale = 0.20;
         
         public $loadingtime = 2;
-
 		
         public $rangePenalty = 0.5;
         public $fireControl = array(-2, 3, 3); // fighters, <mediums, <capitals 
 
-
-		function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
+	function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc)
+        {
             parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
         }
 
-		public function getDamage($fireOrder){        return Dice::d(10, 2)+2;   }
+	public function getDamage($fireOrder){        return Dice::d(10, 2)+2;   }
         public function setMinDamage(){     $this->minDamage = 4 - $this->dp;      }
         public function setMaxDamage(){     $this->maxDamage = 22 - $this->dp;      }
-
-	}
-
+    }
+    
+    class HeavyRailGun extends Matter
+    {
+        public $name = "heavyRailGun";
+        public $displayName = "Heavy Railgun";
+        public $animation = "trail";
+        public $animationColor = array(250, 250, 190);
+	public $projectilespeed = 20;
+        public $animationWidth = 6;
+	public $animationExplosionScale = 0.30;
+        
+        public $loadingtime = 4;
+		
+        public $rangePenalty = 0.33;
+        public $fireControl = array(-3, 2, 2); // fighters, <mediums, <capitals
+        
+        function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc)
+        {
+            parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
+        }
+        
+    	public function getDamage($fireOrder){        return Dice::d(10, 5)+7;   }
+        public function setMinDamage(){     $this->minDamage = 12 - $this->dp;      }
+        public function setMaxDamage(){     $this->maxDamage = 57 - $this->dp;      }    
+    }
+    
+    class RailGun extends Matter
+    {
+        public $name = "railGun";
+        public $displayName = "Railgun";
+        public $animation = "trail";
+        public $animationColor = array(250, 250, 190);
+	public $projectilespeed = 25;
+        public $animationWidth = 5;
+	public $animationExplosionScale = 0.25;
+        
+        public $loadingtime = 3;
+		
+        public $rangePenalty = 0.5;
+        public $fireControl = array(-3, 2, 2); // fighters, <mediums, <capitals
+        
+        function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc)
+        {
+            parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
+        }
+        
+    	public function getDamage($fireOrder){        return Dice::d(10, 3)+3;   }
+        public function setMinDamage(){     $this->minDamage = 6 - $this->dp;      }
+        public function setMaxDamage(){     $this->maxDamage = 33 - $this->dp;      }    
+    }
+    
+    class LightRailGun extends Matter
+    {
+        public $name = "lightRailGun";
+        public $displayName = "Light Railgun";
+        public $animation = "trail";
+        public $animationColor = array(250, 250, 190);
+	public $projectilespeed = 30;
+        public $animationWidth = 4;
+	public $animationExplosionScale = 0.20;
+        
+        public $loadingtime = 2;
+		
+        public $rangePenalty = 1.0;
+        public $fireControl = array(3, 2, 0); // fighters, <mediums, <capitals
+        
+        function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc)
+        {
+            parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
+        }
+        
+    	public function getDamage($fireOrder){        return Dice::d(10, 1)+5;   }
+        public function setMinDamage(){     $this->minDamage = 6 - $this->dp;      }
+        public function setMaxDamage(){     $this->maxDamage = 15 - $this->dp;      }    
+    }
+    
 ?>
