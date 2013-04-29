4d380c4d45be1862a7a678ff67a8c826a0b60e99
jvanrosmalen <jazz_cama@hotmail.com>
2013-04-29 23:59:47 +0200

    Lahas and Brahassa

---
+++ source/server/model/ships/balosian/lahas.php
@@ -0,0 +1,64 @@
+<?php
+class Lahas extends BaseShip{
+    
+    function __construct($id, $userid, $name,  $movement){
+        parent::__construct($id, $userid, $name,  $movement);
+        
+	$this->pointCost = 765;
+	$this->faction = "Balosian";
+        $this->phpclass = "Lahas";
+        $this->imagePath = "img/ships/brahassa.png";
+        $this->shipClass = "Lahas Command Cruiser";
+        $this->shipSizeClass = 3;
+        
+        $this->forwardDefense = 15;
+        $this->sideDefense = 17;
+        
+        $this->turncost = 1;
+        $this->turndelaycost = 1;
+        $this->accelcost = 3;
+        $this->rollcost = 3;
+        $this->pivotcost = 3;
+         
+        $this->addPrimarySystem(new Reactor(5, 25, 0, 0));
+        $this->addPrimarySystem(new CnC(5, 12, 0, 0));
+        $this->addPrimarySystem(new Scanner(5, 12, 4, 7));
+	$this->addPrimarySystem(new Engine (5, 15, 0, 10, 3));
+	$this->addPrimarySystem(new Hangar(5, 14));
+	$this->addPrimarySystem(new Hangar(5, 14));
+        $this->addPrimarySystem(new JumpEngine(5, 10, 3, 36));
+
+       	$this->addFrontSystem(new StdParticleBeam(3, 4, 1, 240, 60));
+       	$this->addFrontSystem(new StdParticleBeam(3, 4, 1, 60, 240));
+       	$this->addFrontSystem(new IonCannon(4, 6, 4, 300, 60));
+       	$this->addFrontSystem(new IonCannon(4, 6, 4, 300, 60));
+	$this->addFrontSystem(new Thruster(4, 8, 0, 4, 1));
+	$this->addFrontSystem(new Thruster(4, 8, 0, 4, 1));
+		
+	$this->addAftSystem(new StdParticleBeam(3, 4, 1, 120, 0));
+       	$this->addAftSystem(new StdParticleBeam(3, 4, 1, 0, 240));
+       	$this->addAftSystem(new IonCannon(4, 6, 4, 120, 240));
+	$this->addAftSystem(new Thruster(3, 15, 0, 5, 2));
+	$this->addAftSystem(new Thruster(3, 15, 0, 5, 2));
+	
+	$this->addLeftSystem(new StdParticleBeam(3, 4, 1, 240, 0));
+	$this->addLeftSystem(new StdParticleBeam(3, 4, 1, 240, 0));
+	$this->addLeftSystem(new IonCannon(4, 6, 4, 240, 0));
+	$this->addLeftSystem(new IonCannon(4, 6, 4, 240, 0));
+	$this->addLeftSystem(new Thruster(3, 10, 0, 4, 3));
+
+	$this->addRightSystem(new StdParticleBeam(3, 4, 1, 0, 120));
+	$this->addRightSystem(new StdParticleBeam(3, 4, 1, 0, 120));
+	$this->addRightSystem(new IonCannon(4, 6, 4, 0, 120));
+	$this->addRightSystem(new IonCannon(4, 6, 4, 0, 120));
+	$this->addRightSystem(new Thruster(3, 10, 0, 4, 4));
+        
+        //0:primary, 1:front, 2:rear, 3:left, 4:right;
+        $this->addFrontSystem(new Structure(5, 60));
+        $this->addAftSystem(new Structure(4, 52));
+        $this->addLeftSystem(new Structure(4, 39));
+        $this->addRightSystem(new Structure(4, 39));
+        $this->addPrimarySystem(new Structure(5, 60));
+    }
+}
+?>
