dfefbc1a2715fa1e8847175c8a9d1fc02123b399
Aatu Riuttamaki <aatu.riuttamaki@gmail.com>
2013-05-07 22:37:37 +0300

    changed heavy particle cannon effect

--- source/server/model/weapons/particle.php
+++ source/server/model/weapons/particle.php
@@ -3,27 +3,27 @@
 
     class Particle extends Weapon{
 
-    
+
         function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
             parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
         }
-        
+
         public function setSystemDataWindow($turn){
 
             $this->data["Weapon type"] = "Particle";
             $this->data["Damage type"] = "Standard";
-            
+
             parent::setSystemDataWindow($turn);
         }
-    
-        
-    
+
+
+
     }
-    
+
     class StdParticleBeam extends Particle{
 
         public $trailColor = array(30, 170, 255);
-        
+
         public $name = "stdParticleBeam";
         public $displayName = "Standard Particle Beam";
         public $animation = "beam";
@@ -32,15 +32,15 @@
         public $projectilespeed = 12;
         public $animationWidth = 4;
         public $trailLength = 10;
-        
+
         public $intercept = 2;
-   
-        
+
+
         public $loadingtime = 1;
-      
-        
+
+
         public $rangePenalty = 1;
-        public $fireControl = array(4, 4, 4); // fighters, <mediums, <capitals 
+        public $fireControl = array(4, 4, 4); // fighters, <mediums, <capitals
 
 
         function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
@@ -56,7 +56,7 @@
     class AdvParticleBeam extends Particle{
 
         public $trailColor = array(30, 170, 255);
-        
+
         public $name = "advParticleBeam";
         public $displayName = "Advanced Particle Beam";
         public $animation = "beam";
@@ -66,11 +66,11 @@
         public $animationWidth = 5;
         public $trailLength = 10;
         public $iconPath = "stdParticleBeam.png";
-        
+
         public $intercept = 2;
         public $loadingtime = 1;
         public $rangePenalty = 0.66;
-        public $fireControl = array(5, 5, 5); // fighters, <mediums, <capitals 
+        public $fireControl = array(5, 5, 5); // fighters, <mediums, <capitals
 
         function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
             parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
@@ -81,11 +81,11 @@
         public function setMaxDamage(){     $this->maxDamage = 18 - $this->dp;      }
 
     }
-    
+
     class TwinArray extends Particle{
 
         public $trailColor = array(30, 170, 255);
-        
+
         public $name = "twinArray";
         public $displayName = "Twin array";
         public $animation = "trail";
@@ -94,15 +94,15 @@
         public $projectilespeed = 12;
         public $animationWidth = 3;
         public $trailLength = 10;
-        
+
         public $intercept = 2;
-   
-        
+
+
         public $loadingtime = 1;
         public $guns = 2;
-        
+
         public $rangePenalty = 2;
-        public $fireControl = array(6, 5, 4); // fighters, <mediums, <capitals 
+        public $fireControl = array(6, 5, 4); // fighters, <mediums, <capitals
 
 
         function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
@@ -114,11 +114,11 @@
         public function setMaxDamage(){     $this->maxDamage = 14 - $this->dp;      }
 
     }
-    
+
     class HeavyArray extends Particle{
 
         public $trailColor = array(30, 170, 255);
-        
+
         public $name = "heavyArray";
         public $displayName = "Heavy array";
         public $animation = "trail";
@@ -127,14 +127,14 @@
         public $projectilespeed = 20;
         public $animationWidth = 4;
         public $trailLength = 15;
-        
+
         public $intercept = 2;
-        
+
         public $loadingtime = 1;
         public $guns = 2;
-        
+
         public $rangePenalty = 1;
-        public $fireControl = array(2, 3, 4); // fighters, <mediums, <capitals 
+        public $fireControl = array(2, 3, 4); // fighters, <mediums, <capitals
 
 
         function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
@@ -146,11 +146,11 @@
         public function setMaxDamage(){     $this->maxDamage = 26 - $this->dp;      }
 
     }
-    
+
     class ParticleCannon extends Raking{
-        
+
         public $trailColor = array(30, 170, 255);
-        
+
         public $name = "particleCannon";
         public $displayName = "Particle Cannon";
         public $animation = "beam";
@@ -160,12 +160,12 @@
         public $animationWidth = 8;
         public $trailLength = 24;
         public $damageType = "raking";
-        
+
         public $intercept = 1;
         public $loadingtime = 2;
-      
+
         public $rangePenalty = 0.5;
-        public $fireControl = array(2, 4, 5); // fighters, <mediums, <capitals 
+        public $fireControl = array(2, 4, 5); // fighters, <mediums, <capitals
 
         function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
             parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
@@ -174,33 +174,33 @@
         public function setSystemDataWindow($turn){
             $this->data["Weapon type"] = "Particle";
             $this->data["Damage type"] = "Raking";
-           
+
             parent::setSystemDataWindow($turn);
         }
-        
+
         public function getDamage($fireOrder){ return Dice::d(10, 2)+15;   }
         public function setMinDamage(){     $this->minDamage = 17 - $this->dp;      }
         public function setMaxDamage(){     $this->maxDamage = 35 - $this->dp;      }
     }
-    
+
     class HvyParticleCannon extends Raking{
-        
-        public $trailColor = array(30, 170, 255);
-        
+
+        public $trailColor = array(252, 252, 252);
+
         public $name = "hvyParticleCannon";
         public $displayName = "Heavy Particle Cannon";
         public $animation = "beam";
-        public $animationColor = array(255, 250, 230);
+        public $animationColor = array(252, 252, 252);
         public $animationExplosionScale = 0.45;
-        public $projectilespeed = 13;
-        public $animationWidth = 10;
-        public $trailLength = 24;
+        public $projectilespeed = 33;
+        public $animationWidth = 7;
+        public $trailLength = 2400;
         public $damageType = "raking";
-        
+
         public $loadingtime = 6;
-      
+
         public $rangePenalty = 0.33;
-        public $fireControl = array(0, 4, 6); // fighters, <mediums, <capitals 
+        public $fireControl = array(0, 4, 6); // fighters, <mediums, <capitals
 
         function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
             parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
@@ -209,20 +209,20 @@
         public function setSystemDataWindow($turn){
             $this->data["Weapon type"] = "Particle";
             $this->data["Damage type"] = "Raking";
-           
+
             parent::setSystemDataWindow($turn);
         }
-        
+
         public function getDamage($fireOrder){ return Dice::d(10, 6)+60;   }
         public function setMinDamage(){     $this->minDamage = 66 - $this->dp;      }
         public function setMaxDamage(){     $this->maxDamage = 120 - $this->dp;      }
     }
-    
-    
+
+
     class PairedParticleGun extends LinkedWeapon{
 
         public $trailColor = array(30, 170, 255);
-        
+
         public $name = "pairedParticleGun";
         public $displayName = "Paired Particle guns";
         public $animation = "trail";
@@ -231,15 +231,15 @@
         public $projectilespeed = 12;
         public $animationWidth = 2;
         public $trailLength = 10;
-        
+
         public $intercept = 2;
-        
+
         public $loadingtime = 1;
         public $shots = 2;
         public $defaultShots = 2;
-        
+
         public $rangePenalty = 2;
-        public $fireControl = array(0, 0, 0); // fighters, <mediums, <capitals 
+        public $fireControl = array(0, 0, 0); // fighters, <mediums, <capitals
         private $damagebonus = 0;
 
 
@@ -247,20 +247,20 @@
             $this->damagebonus = $damagebonus;
             $this->defaultShots = $nrOfShots;
             $this->shots = $nrOfShots;
-            
+
             if($nrOfShots === 3){
                 $this->iconPath = "pairedParticleGun3.png";
             }
-            
+
             parent::__construct(0, 1, 0, $startArc, $endArc);
-           
+
         }
-        
+
         public function setSystemDataWindow($turn){
 
             $this->data["Weapon type"] = "Particle";
             $this->data["Damage type"] = "Standard";
-            
+
             parent::setSystemDataWindow($turn);
         }
 
@@ -269,9 +269,9 @@
         public function setMaxDamage(){     $this->maxDamage = 6+$this->damagebonus - $this->dp;      }
 
     }
-    
-    
-    
+
+
+
 
 ?>
 
