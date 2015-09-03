e03b8040176190a0ed42e8583d20a666b8e1a330
Thomas Solway <thomas@solway.me>
2015-09-03 07:31:03 -0600

    Equata Corrections closes #115

--- source/server/model/ships/centauri_old/equata.php
+++ source/server/model/ships/centauri_old/equata.php
@@ -23,12 +23,11 @@ class Equata extends HeavyCombatVessel{
         $this->accelcost = 3;
         $this->rollcost = 2;
         $this->pivotcost = 3;
-        $this->iniativebonus = 30;
-        
+        $this->iniativebonus = 30;  
          
         $this->addPrimarySystem(new Reactor(6, 17, 0, 0));
         $this->addPrimarySystem(new CnC(6, 12, 0, 0));
-        $this->addPrimarySystem(new ElintArray(6, 6, 2));
+        $this->addPrimarySystem(new ElintScanner(5, 20, 5, 7));
         $this->addPrimarySystem(new Engine(5, 15, 0, 8, 3));
         $this->addPrimarySystem(new Hangar(5, 7));
         $this->addPrimarySystem(new Thruster(3, 10, 0, 4, 3));
@@ -40,15 +39,13 @@ class Equata extends HeavyCombatVessel{
         $this->addFrontSystem(new AssaultLaser(4, 6, 4, 300, 60));
         $this->addFrontSystem(new LightParticleBeamShip(2, 2, 1, 300, 120));
         
-        $this->addAftSystem(new AssaultLaser(4, 6, 4, 120, 240));
-        $this->addAftSystem(new Thruster(4, 16, 0, 5, 2));
-        $this->addAftSystem(new Thruster(4, 16, 0, 5, 2));
+        $this->addAftSystem(new Thruster(4, 16, 0, 4, 2));
+        $this->addAftSystem(new Thruster(4, 16, 0, 4, 2));
         $this->addAftSystem(new JumpEngine(3, 15, 3, 20));
         $this->addAftSystem(new LightParticleBeamShip(2, 2, 1, 0, 180));
         $this->addAftSystem(new LightParticleBeamShip(2, 2, 1, 0, 180));
         $this->addAftSystem(new LightParticleBeamShip(2, 2, 1, 180, 360));
         $this->addAftSystem(new LightParticleBeamShip(2, 2, 1, 180, 360));
-        
         
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
         $this->addFrontSystem(new Structure( 4, 50));
