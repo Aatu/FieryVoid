a9abcf1fd7b8909cbd8267518881e201cb5ecd49
Thomas Solway <thomas@solway.me>
2015-09-03 07:48:47 -0600

    Optine Corrections

--- source/server/model/ships/centauri_old/optine.php
+++ source/server/model/ships/centauri_old/optine.php
@@ -19,9 +19,7 @@ class Optine extends BaseShip{
         $this->accelcost = 4;
         $this->rollcost = 3;
         $this->pivotcost = 4;
-
-        
-         
+       
         $this->addPrimarySystem(new Reactor(6, 25, 0, 0));
         $this->addPrimarySystem(new CnC(6, 16, 0, 0));
         $this->addPrimarySystem(new Scanner(6, 23, 4, 8));
@@ -30,28 +28,33 @@ class Optine extends BaseShip{
         
 		$this->addFrontSystem(new Thruster(4, 10, 0, 4, 1));
         $this->addFrontSystem(new Thruster(4, 10, 0, 4, 1));
-        $this->addFrontSystem(new AssaultLaser(3, 5, 4, 240, 360));
-        $this->addFrontSystem(new AssaultLaser(3, 5, 4, 300, 60));
-        $this->addFrontSystem(new AssaultLaser(3, 5, 4, 300, 60));
-        $this->addFrontSystem(new AssaultLaser(3, 5, 4, 0, 120));
-        $this->addFrontSystem(new LightParticleBeamShip(2, 2, 1, 270, 90));
+        $this->addFrontSystem(new AssaultLaser(3, 6, 4, 240, 360));
+        $this->addFrontSystem(new AssaultLaser(3, 6, 4, 300, 60));
+        $this->addFrontSystem(new AssaultLaser(3, 6, 4, 300, 60));
+        $this->addFrontSystem(new AssaultLaser(3, 6, 4, 0, 120));
+        $this->addFrontSystem(new LightParticleBeamShip(3, 2, 1, 270, 90));
 		
         $this->addAftSystem(new Thruster(4, 10, 0, 3, 2));
         $this->addAftSystem(new Thruster(4, 12, 0, 4, 2));
         $this->addAftSystem(new Thruster(4, 10, 0, 3, 2));
 		$this->addAftSystem(new JumpEngine(5, 25, 3, 20));
-        
-        $this->addLeftSystem(new Thruster(4, 14, 0, 5, 3));
-        $this->addLeftSystem(new AssaultLaser(3, 5, 4, 300, 360));
-        $this->addLeftSystem(new ParticleProjector(3, 6, 1, 180, 300));
-        $this->addLeftSystem(new ParticleProjector(3, 6, 1, 240, 360));
-
-        $this->addRightSystem(new Thruster(4, 14, 0, 5, 4));
-        $this->addRightSystem(new AssaultLaser(3, 5, 4, 0, 60));
-        $this->addRightSystem(new ParticleProjector(3, 6, 1, 0, 120));
-        $this->addRightSystem(new ParticleProjector(3, 6, 1, 60, 180));
-        
-        
+		$this->addAftSystem(new AssaultLaser(3, 6, 4, 60, 180));
+		$this->addAftSystem(new AssaultLaser(3, 6, 4, 180, 300));
+		$this->addAftSystem(new LightParticleBeamShip(3, 2, 1, 60, 240));
+		$this->addAftSystem(new LightParticleBeamShip(3, 2, 1, 120, 300));
+		
+		$this->addLeftSystem(new Thruster(4, 14, 0, 5, 3));
+        $this->addLeftSystem(new AssaultLaser(3, 6, 4, 300, 360));
+		$this->addLeftSystem(new LightParticleBeamShip(3, 2, 1, 180, 360));
+		$this->addLeftSystem(new LightParticleBeamShip(3, 2, 1, 180, 360));
+		$this->addLeftSystem(new LightParticleBeamShip(3, 2, 1, 180, 360));
+		
+		$this->addRightSystem(new Thruster(4, 14, 0, 5, 4));
+        $this->addRightSystem(new AssaultLaser(3, 6, 4, 0, 60));
+		$this->addRightSystem(new LightParticleBeamShip(3, 2, 1, 0, 180));
+		$this->addRightSystem(new LightParticleBeamShip(3, 2, 1, 0, 180));
+		$this->addRightSystem(new LightParticleBeamShip(3, 2, 1, 0, 180));
+		
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
         $this->addFrontSystem(new Structure( 5, 38));
         $this->addAftSystem(new Structure( 5, 35));
