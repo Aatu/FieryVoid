8ecefa95ad6c685d7e2861379ea91dcf9ed7a18f
Thomas Solway <thomas@solway.me>
2015-10-30 15:37:25 -0600

    Update of SCSs with addition of hitChart array to Barque

--- source/server/model/ships/raiders/barque.php
+++ source/server/model/ships/raiders/barque.php
@@ -1,5 +1,5 @@
 <?php
-class Aspar extends HeavyCombatVessel{
+class Barque extends HeavyCombatVessel{
     
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
@@ -56,6 +56,75 @@ class Aspar extends HeavyCombatVessel{
         $this->addFrontSystem(new Structure( 3, 32));
         $this->addPrimarySystem(new Structure( 3, 36));
         $this->addAftSystem(new Structure( 3, 32));
+        
+        $this->hitChart = array(
+        	0=> array(
+        		1 => "structure",
+        		2 => "structure",
+        		3 => "structure",
+        		4 => "structure",
+        		5 => "structure",
+        		6 => "structure",
+        		7 => "structure",
+        		8 => "cargoBay",
+        		9 => "cargoBay",
+        		10 => "thruster",
+        		11 => "thruster",
+        		12 => "thruster",
+        		13 => "scanner",
+        		14 => "scanner",
+        		15 => "engine",
+        		16 => "engine",
+        		17 => "hanger",
+        		18 => "reactor",
+        		19 => "reactor",
+        		20 => "CnC",	
+        	),
+        	1=> array(
+        		1 => "thruster",
+        		2 => "thruster",
+        		3 => "thruster",
+        		4 => "thruster",
+        		5 => "mediumPulse",
+        		6 => "mediumPulse",
+        		7 => "twinArray",
+        		8 => "twinArray",
+        		9 => "heavyPlasma",
+        		10 => "structure",
+        		11 => "structure",
+        		12 => "structure",
+        		13 => "structure",
+        		14 => "structure",
+        		15 => "structure",
+        		16 => "structure",
+        		17 => "structure",
+        		18 => "structure",
+        		19 => "primary",
+        		20 => "primary",        			
+        	),
+        	2=> array(
+        		1 => "thruster",
+        		2 => "thruster",
+        		3 => "thruster",
+        		4 => "thruster",
+        		5 => "thruster",
+        		6 => "thruster",
+        		7 => "thruster",
+        		8 => "stdParticleBeam",
+        		9 => "stdParticleBeam",
+        		10 => "structure",
+        		11 => "structure",
+        		12 => "structure",
+        		13 => "structure",
+        		14 => "structure",
+        		15 => "structure",
+        		16 => "structure",
+        		17 => "structure",
+        		18 => "structure",
+        		19 => "primary",
+        		20 => "primary",        			
+        	),
+        );
     }
 }
 
