2e959b26e1bdca58c2442e6868e143702c31f5f7
Thomas Solway <thomas@solway.me>
2015-11-17 12:54:23 -0700

    Corrected Xebec

--- source/server/model/ships/raiders/xebec.php
+++ source/server/model/ships/raiders/xebec.php
@@ -21,21 +21,24 @@ class Xebec extends MediumShip{
         $this->pivotcost = 2;
     	$this->iniativebonus = 30;
          
-        $this->addPrimarySystem(new StdParticleBeam(2, 4, 1, 180, 360));
-        $this->addPrimarySystem(new MediumLaser(3, 6, 5, 240, 60));
-        $this->addPrimarySystem(new MediumLaser(3, 6, 5, 300, 120));
-        $this->addPrimarySystem(new StdParticleBeam(2, 4, 1, 0, 180));
+        $this->addFrontSystem(new StdParticleBeam(2, 4, 1, 180, 360));
+        $this->addFrontSystem(new MediumLaser(3, 6, 5, 240, 60));
+        $this->addFrontSystem(new MediumLaser(3, 6, 5, 300, 120));
+        $this->addFrontSystem(new StdParticleBeam(2, 4, 1, 0, 180));
 
         $this->addPrimarySystem(new Reactor(3, 9, 0, 0));
         $this->addPrimarySystem(new Engine(3, 18, 0, 12, 2));
         $this->addPrimarySystem(new CnC(4, 5, 0, 0));
         $this->addPrimarySystem(new Scanner(3, 12, 3, 5));
-        $this->addPrimarySystem(new CargoBay(2, 18));
-        $this->addPrimarySystem(new CargoBay(2, 18));
         $this->addPrimarySystem(new Hangar(3, 2));
     	$this->addPrimarySystem(new Thruster(3, 15, 0, 5, 3));
         $this->addPrimarySystem(new Thruster(3, 15, 0, 5, 4));
 		
+        $temp1 = new CargoBay(2, 18);
+        $temp2 = new CargoBay(2, 18);
+        $temp1->displayName = "Cargo Bay A";
+        $temp2->displayName = "Cargo Bay B";
+        
         $this->addFrontSystem(new Thruster(3, 8, 0, 4, 1));
         $this->addFrontSystem(new Thruster(3, 8, 0, 4, 1));
 
@@ -43,6 +46,8 @@ class Xebec extends MediumShip{
         $this->addAftSystem(new Thruster(2, 12, 0, 4, 2));
         $this->addAftSystem(new Thruster(3, 12, 0, 4, 2));
         $this->addAftSystem(new StdParticleBeam(2, 4, 1, 0, 360));
+        $this->addAftSystem($temp1);
+        $this->addAftSystem($temp2);
 	
         $this->addPrimarySystem(new Structure( 4, 48));
         
@@ -97,12 +102,12 @@ class Xebec extends MediumShip{
         				3 => "thruster",
         				4 => "thruster",
         				5 => "thruster",
-        				6 => "cargo Bay",
-        				7 => "cargo Bay",
-        				8 => "cargo Bay",
-        				9 => "cargo Bay",
-        				10 => "cargo Bay",
-        				11 => "cargo Bay",
+        				6 => "cargo Bay A",
+        				7 => "cargo Bay A",
+        				8 => "cargo Bay A",
+        				9 => "cargo Bay B",
+        				10 => "cargo Bay B",
+        				11 => "cargo Bay B",
         				12 => "structure",
         				13 => "structure",
         				14 => "structure",
