49a2c73243b8dd7efe77a451f29091e2423ee3b0
Zero1627 <reisen@poczta.onet.pl>
2016-03-20 16:32:10 +0100

    Missile firing arcs corrected; hit table added; proxy reload rack added

--- source/server/model/ships/orieni/vigilant.php
+++ source/server/model/ships/orieni/vigilant.php
@@ -8,9 +8,9 @@ class Vigilant extends BaseShip{
 		$this->faction = "Orieni";
         $this->phpclass = "Vigilant";
         $this->imagePath = "img/ships/vigilant.png";
-        $this->shipClass = "Vigilant Combat Support Ship";
+        $this->shipClass = "Vigilant Combat Support Ship (2007)";
         $this->shipSizeClass = 3;
-        $this->fighters = array("normal"=>6);
+        $this->fighters = array("light"=>6);
         $this->canvasSize = 200;
 		
         $this->forwardDefense = 17;
@@ -21,8 +21,8 @@ class Vigilant extends BaseShip{
         $this->accelcost = 4;
         $this->rollcost = 3;
         $this->pivotcost = 3;
-
         $this->limited = 10;
+
         
         $this->addPrimarySystem(new Reactor(5, 20, 0, 0));
         $this->addPrimarySystem(new CnC(6, 16, 0, 0));
@@ -32,15 +32,17 @@ class Vigilant extends BaseShip{
         $this->addPrimarySystem(new JumpEngine(5, 30, 6, 25));
         $this->addPrimarySystem(new HKControlNode(5, 12, 1, 1));
         $this->addPrimarySystem(new SMissileRack(5, 6, 0, 0, 360));
+		$reloadRack = new CargoBay(6, 9);
+		$reloadRack->name = 'Reload Rack';
+	$this->addPrimarySystem($reloadRack);
         
         $this->addFrontSystem(new Thruster(3, 12, 0, 3, 1));
         $this->addFrontSystem(new Thruster(3, 12, 0, 3, 1));
-
-        $this->addFrontSystem(new SMissileRack(5, 6, 0, 240, 90));
+        $this->addFrontSystem(new SMissileRack(5, 6, 0, 240, 60));
         $this->addFrontSystem(new RapidGatling(2, 4, 1, 240, 120));
         $this->addFrontSystem(new RapidGatling(2, 4, 1, 240, 120));
         $this->addFrontSystem(new RapidGatling(2, 4, 1, 240, 120));
-        $this->addFrontSystem(new SMissileRack(5, 6, 0, 240, 90));
+        $this->addFrontSystem(new SMissileRack(5, 6, 0, 300, 120));
 
         $this->addAftSystem(new RapidGatling(2, 4, 1, 60, 300));
         $this->addAftSystem(new RapidGatling(2, 4, 1, 60, 300));
@@ -53,7 +55,6 @@ class Vigilant extends BaseShip{
         $this->addLeftSystem(new CargoBay(2, 25));
         $this->addLeftSystem(new RapidGatling(2, 4, 1, 180, 360));
         $this->addLeftSystem(new RapidGatling(2, 4, 1, 180, 360));
-
 
         $this->addRightSystem(new Thruster(4, 20, 0, 6, 4));
         $this->addRightSystem(new CargoBay(2, 25));
@@ -68,11 +69,65 @@ class Vigilant extends BaseShip{
         $this->addLeftSystem(new Structure(4, 45));
         $this->addRightSystem(new Structure(4, 45));
         $this->addPrimarySystem(new Structure(5, 56));
+
+
+
+
+	//d20 hit chart
+	$this->hitChart = array(
+		
+		//PRIMARY
+		0=> array( 
+			7 => "Structure",
+			8 => "Class-S Missile Rack",
+			10 => "Jump Engine",
+			12 => "Scanner",
+			14 => "Engine",
+			15 => "Reload Rack"
+			17 => "Hangar",
+			18 => "HK-Control Node",
+			19 => "Reactor",
+			20 => "CnC",
+		),
+
+		//Forward
+		1=> array(
+			4 => "Thruster",
+			8 => "Class-S Missile Rack",
+			11 => "Rapid Gatling Railgun",
+			18 => "Structure",
+			20 => "Primary",
+		),
+
+		//Aft
+		2=> array(
+			6 => "Thruster",
+			9 => "Rapid Gatling Railgun",
+			18 => "Structure",
+			20 => "Primary",
+		),
+
+		//Port
+		3=> array(
+			4 => "Thruster",
+			6 => "Rapid Gatling Railgun",
+			11 => "Cargo Bay",
+			18 => "Structure",
+			20 => "Primary",
+		),
+
+		//Starboard
+		4=> array(
+			4 => "Thruster",
+			6 => "Rapid Gatling Railgun",
+			11 => "Cargo Bay",
+			18 => "Structure",
+			20 => "Primary",
+		),
+
+	);
+
         
     }
-
 }
-
-
-
 ?>
