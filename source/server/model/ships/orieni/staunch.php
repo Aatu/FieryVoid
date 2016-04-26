940528106ea1d1f3e59fcd9132f1f2ab1eda22d2
Zero1627 <reisen@poczta.onet.pl>
2016-04-26 09:33:46 +0200

    Staunch hit chart added

--- source/server/model/ships/orieni/staunch.php
+++ source/server/model/ships/orieni/staunch.php
@@ -1,11 +1,11 @@
 <?php
 class Staunch extends MediumShip{
-    
+    /Orieni Staunch Strike Frigate, V6*/
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-		$this->pointCost = 400;
-		$this->faction = "Orieni";
+	$this->pointCost = 400;
+	$this->faction = "Orieni";
         $this->phpclass = "Staunch";
         $this->imagePath = "img/ships/steadfast.png";
         $this->shipClass = "Staunch Strike Frigate";
@@ -17,14 +17,14 @@ class Staunch extends MediumShip{
         
         $this->turncost = 0.5;
         $this->turndelaycost = 0.5;
-        $this->accelcost = 2;
-        $this->rollcost = 1;
-        $this->pivotcost = 2;
-		$this->iniativebonus = 60;
+        $this->accelcost = 3;
+        $this->rollcost = 2;
+        $this->pivotcost = 3;
+	$this->iniativebonus = 60;
 
         $this->occurence = "uncommon";
          
-        $this->addPrimarySystem(new Reactor(4, 15, 0, 0));
+        $this->addPrimarySystem(new Reactor(4, 15, 0, 3));
         $this->addPrimarySystem(new CnC(4, 15, 0, 0));
         $this->addPrimarySystem(new Scanner(3, 12, 3, 6));
         $this->addPrimarySystem(new Engine(3, 15, 0, 10, 2));
@@ -49,6 +49,33 @@ class Staunch extends MediumShip{
        
         $this->addPrimarySystem(new Structure(4, 46));
 		
+
+
+	//d20 hit chart
+	$this->hitChart = array(
+		
+		0=> array( //PRIMARY
+			8 => "Thruster",
+			11 => "Scanner",
+			15 => "Engine",
+			17 => "Hangar",
+			19 => "Reactor",
+			20 => "C&C",
+		),
+		1=> array( //Fwd
+			5 => "Thruster",
+			10 => "Gauss Cannon",
+			11 => "Rapid Gatling Railgun",
+			17 => "Structure",
+			20 => "Primary",
+		),
+		2=> array( //Aft
+			7 => "Thruster",
+			9 => "Rapid Gatling Railgun",
+			17 => "Structure",
+			20 => "Primary",
+		),
+	);
 		
         }
     }
