6e4b3fa905eca1aa4ba0ec27c0be79a87c84bb82
Zero1627 <reisen@poczta.onet.pl>
2016-07-25 09:50:12 +0200

    Update varconfig.php
    a touch here

--- source/server/varconfig.php
+++ source/server/varconfig.php
@@ -3,6 +3,6 @@ $database_name='B5CGM';
 $database_user='aatu';
 $database_password='Kiiski';
 //$database_user='root';
-//$database_password='';
+//$database_password=''; 
 $secret_phrase='molecular pulsar';
 ?>
