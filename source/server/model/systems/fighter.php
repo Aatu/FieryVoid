dfc3932732e760ab62afb16f8f0a13526ced4a74
Zero1627 <reisen@poczta.onet.pl>
2016-10-12 16:21:56 +0200

    testCritical and armor

--- source/server/model/systems/fighter.php
+++ source/server/model/systems/fighter.php
@@ -71,22 +71,16 @@
             return parent::isDestroyed();
         }
 		
-		public function addFrontSystem($system){
-            
+	public function addFrontSystem($system){
 			$this->addSystem($system, 1);
-            
-        
         }
         
         public function addAftSystem($system){
-            
-			$this->addSystem($system, 2);
-            
-        
+		$this->addSystem($system, 2);
         }
 		
 		
-		protected function addSystem($system, $loc){
+	protected function addSystem($system, $loc){
             $system->location = $loc;
             $this->systems[] = $system;
         }
@@ -106,66 +100,56 @@
      
 		}
 		
-		public function testCritical($ship, $gamedata, $crits, $add = 0){
-			$d = Dice::d(10);
-			
-                        $dropOutBonus = $gamedata->getShipById($this->flightid)->getDropOutBonus();
-                        
-			if (($d + $dropOutBonus) > $this->getRemainingHealth()){
-				$crit = new DisengagedFighter(-1, $ship->id, $this->id, "DisengagedFighter", $gamedata->turn);
-				$crit->updated = true;
-                $this->criticals[] =  $crit;
-                $crits[] = $crit;
-			}
-						
-			return $crits;
-			 
+	public function testCritical($ship, $gamedata, $crits, $add = 0){
+		$d = Dice::d(10);
+		
+		$bonusCrit = 0;	//one-time penalty to dropout roll
+		foreach($crits as $key=>$value) {
+		  if($value instanceof NastierCrit){
+			$bonusCrit+= $value->$outputMod;
+			  unset($crits[$key]);
+		  }
 		}
+		$crits = array_values($crits); //in case some criticals were deleted!
+		
+		$dropOutBonus = $gamedata->getShipById($this->flightid)->getDropOutBonus();
+		if (($d + $dropOutBonus - $bonusCrit) > $this->getRemainingHealth()){
+			$crit = new DisengagedFighter(-1, $ship->id, $this->id, "DisengagedFighter", $gamedata->turn);
+			$crit->updated = true;
+			$this->criticals[] =  $crit;
+			$crits[] = $crit;
+		}
+		return $crits;
+	}
 		
 		
 		public function isOfflineOnTurn($turn = null){
-		
 			return false;
-		
 		}
 		
 		public function isOverloadingOnTurn($turn = null){
-			
 			return false;
-		
 		}
 		
-		 public function getArmourPos($gamedata, $pos){
-			$target = $gamedata->getShipById($this->flightid); 
-			$tf = $target->getFacingAngle();
-			$shooterCompassHeading = mathlib::getCompassHeadingOfPos($target, $pos);
-			
-			return $this->doGetArmour($tf,  $shooterCompassHeading);
+		
+		
+	public function getArmourPos($gamedata, $pos){ 
+		$target = $gamedata->getShipById($this->flightid); 
+		$loc = $target->doGetHitSectionPos($pos, 0); //finds array with relevant data!
+		return $loc["armour"];
+	}
+        
+        public function getArmour($target, $shooter, $dmgType){ //for fighter no need to note where fire went, as all calculations are done on raw flight data
+		$target = $gamedata->getShipById($this->flightid); 
+		$loc = $target->doGetHitSection($shooter, 0); //finds array with relevant data!
+		return $loc["armour"];
         }
+		
+		
+		
         
-        public function getArmour($target, $shooter, $dmgType){
-            $tf = $target->getFacingAngle();
-            $shooterCompassHeading = mathlib::getCompassHeadingOfShip($target, $shooter);
-          
-            return $this->doGetArmour($tf,  $shooterCompassHeading);
-            
-        }
-        
-        
-        public function doGetArmour($tf, $shooterCompassHeading){
-            if (mathlib::isInArc($shooterCompassHeading, Mathlib::addToDirection(330,$tf), Mathlib::addToDirection(30,$tf) )){
-               return $this->armour[0];
-            }else if (mathlib::isInArc($shooterCompassHeading, Mathlib::addToDirection(150,$tf), Mathlib::addToDirection(210,$tf) )){
-                return $this->armour[1];
-            }else if (mathlib::isInArc($shooterCompassHeading, Mathlib::addToDirection(210,$tf), Mathlib::addToDirection(330,$tf) )){
-                return $this->armour[2];
-            }  else if (mathlib::isInArc($shooterCompassHeading, Mathlib::addToDirection(30,$tf), Mathlib::addToDirection(150,$tf) )){
-                return $this->armour[3];
-            } 
-                
-            return $this->armour[0];
-        }
-        
+		
+		
         public function onAdvancingGamedata($ship)
         {
             foreach ($this->systems as $system)
