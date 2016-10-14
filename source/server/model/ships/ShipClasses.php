1ba8524a2d86e1b13dbd105e78bf891ddbb3aa8f
Zero1627 <reisen@poczta.onet.pl>
2016-10-14 13:13:33 +0200

    Update ShipClasses.php

--- source/server/model/ships/ShipClasses.php
+++ source/server/model/ships/ShipClasses.php
@@ -408,11 +408,10 @@
         }
 
         
-        public function getLastTurnMovement($turn){ //returns last actual move, as of current turn
+        public function getLastTurnMovement($turn){ //returns last actual move, as of indicated turn
 		$movement =  null;
 		foreach ($this->movement as $move){ //should be sorted from oldest to newest...
-			if($move->type == "start") $movement = $move; //if nothing was done later, use starting position
-			if($move->type != "move") continue; //not a real move
+			if($move->type == "start") continue; //not a real move
 			if($move->turn > $turn) continue; //future move
 			$movement = $move;  
 		}
