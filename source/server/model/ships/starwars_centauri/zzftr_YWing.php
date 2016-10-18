2f56287bf0436c5a9cd6ec6d514f287329d2b518
Zero1627 <reisen@poczta.onet.pl>
2016-10-18 16:51:16 +0200

    Update zzftr_YWing.php

--- source/server/model/ships/starwars_centauri/zzftr_YWing.php
+++ source/server/model/ships/starwars_centauri/zzftr_YWing.php
@@ -1,49 +1,40 @@
 <?php
-class swftrywingx extends FighterFlight{
+class swftrywing extends FighterFlight{
     
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-	$this->pointCost = 60*6;
-	$this->faction = "Centauri Galactic Empire";
-        $this->phpclass = "swftrywingx";
-        $this->shipClass = "Y-Wing Demonstrator flight";
-	$this->imagePath = "img/starwars/yWing.png";
+        $this->pointCost = 60*6;
+        $this->faction = "EA";
+        $this->phpclass = "swftrywing";
+        $this->shipClass = "TEST TEST flight";
+        $this->imagePath = "img/ships/auroraStarfury.png";
         
-        $this->forwardDefense = 7;
-        $this->sideDefense = 9;
-        $this->freethrust = 8;
-        $this->offensivebonus = 6;
+        $this->forwardDefense = 8;
+        $this->sideDefense = 6;
+        $this->freethrust = 12;
+        $this->offensivebonus = 5;
         $this->jinkinglimit = 6;
         $this->turncost = 0.33;
-	    
-	$this->hasNavigator = true;
         
-	$this->iniativebonus = 17 *5; //incl. navigator bonus
+    	$this->iniativebonus = 80;
         $this->populate();
     }
-    
-	
-	
-	
     public function populate(){
         $current = count($this->systems);
         $new = $this->flightSize;
         $toAdd = $new - $current;
         for ($i = 0; $i < $toAdd; $i++){
-          $armour = array(2, 1, 1, 1);
-          $fighter = new Fighter("swftrywingx", $armour, 15, $this->id);
-          $fighter->displayName = "Y-Wing Technology Demonstrator";
-          $fighter->imagePath = "img/starwars/yWing.png";
-          $fighter->iconPath = "img/starwars/yWing_large.png";
-			
-          $fighter->addFrontSystem(new SWFighterLaser(330, 30, 2, 2));
-          $fighter->addFrontSystem(new SWFighterIon(0, 360, 1, 2));
-          //to be added: torpedo launchers, shields
-			
-	  $this->addSystem($fighter);
-			
-	}
+            $armour = array(3, 2, 2, 2);
+            $fighter = new Fighter("swftrywing", $armour, 13, $this->id);
+            $fighter->displayName = "Starfury Heavy Fighter";
+            $fighter->imagePath = "img/ships/auroraStarfury.png";
+            $fighter->iconPath = "img/ships/auroraStarfury_largei.png";
+            $frontGun = new PairedParticleGun(330, 30, 4);
+            $frontGun->displayName = "Uni-Pulse Cannon";
+            $fighter->addFrontSystem($frontGun);
+            $this->addSystem($fighter);
+       }
     }
 }
 ?>
