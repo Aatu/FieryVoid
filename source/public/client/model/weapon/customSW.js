2955cf040004710ac17d598b544c8b0440732ce8
Zero1627 <reisen@poczta.onet.pl>
2016-10-24 16:42:05 +0200

    Update customSW.js

--- source/public/client/model/weapon/customSW.js
+++ source/public/client/model/weapon/customSW.js
@@ -4,4 +4,9 @@ var SWFighterLaser = function(json, ship){
 SWFighterLaser.prototype = Object.create( Particle.prototype );
 SWFighterLaser.prototype.constructor = SWFighterLaser;
 
+var SWFighterIon = function(json, ship){
+    Particle.call( this, json, ship);
+}
+SWFighterIon.prototype = Object.create( Particle.prototype );
+SWFighterIon.prototype.constructor = SWFighterIon;
 
