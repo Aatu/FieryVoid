a79b9702d219e5546c7dc4cd129a00d1301a980b
Zero1627 <reisen@poczta.onet.pl>
2016-10-25 15:31:43 +0200

    Update customSW.js

--- source/public/client/model/weapon/customSW.js
+++ source/public/client/model/weapon/customSW.js
@@ -4,9 +4,27 @@ var SWFighterLaser = function(json, ship){
 SWFighterLaser.prototype = Object.create( Particle.prototype );
 SWFighterLaser.prototype.constructor = SWFighterLaser;
 
+
 var SWFighterIon = function(json, ship){
     Particle.call( this, json, ship);
 }
 SWFighterIon.prototype = Object.create( Particle.prototype );
 SWFighterIon.prototype.constructor = SWFighterIon;
 
+
+var SWFtrProtonTorpedo = function(json, ship)
+{
+    Ammo.call( this, json, ship);
+    this.range = 15;
+    this.hitChanceMod = 0;
+}
+SWFtrProtonTorpedo.prototype = Object.create( Ammo.prototype );
+SWFtrProtonTorpedo.prototype.constructor = SWFtrProtonTorpedo;
+
+
+var SWFtrProtonTorpedoLauncher = function(json, ship)
+{
+    FighterMissileRack.call( this, json, ship);
+}
+SWFtrProtonTorpedo.prototype = Object.create( FighterMissileRack.prototype );
+SWFtrProtonTorpedo.prototype.constructor = SWFtrProtonTorpedo;
