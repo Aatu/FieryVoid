cdf239f5a2a7f33b3906f229d1c6461216be78df
Zero1627 <reisen@poczta.onet.pl>
2016-11-04 16:17:23 +0100

    Update AoE.php

--- source/server/model/weapons/AoE.php
+++ source/server/model/weapons/AoE.php
@@ -1,11 +1,88 @@
 <?php
 
-    class AoE extends Weapon{
-
+    class AoE extends Weapon{ //directly tailored for EMine, really
+	    public $damageType = "Flash"; 
+	    public $weaponClass = "Ballistic";
+	    
         function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
             parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
         }
         
+	public function calculateHit($gamedata, $fireOrder){
+		$fireOrder->needed = round(100-(100*0.25*0.4)); //chance of not hitting target hex: 25%; chance of dissipating: 40$ of that
+		$fireOrder->updated = true;
+	}
+	    
+	public function fire($gamedata, $fireOrder){ //sadly here it really has to be completely redefined... or at least I see no option to avoid this
+		$this->changeFiringMode($fireOrder->firingMode);//changing firing mode may cause other changes, too!
+           	$shooter = $gamedata->getShipById($fireOrder->shooterid);
+
+		$movement = $shooter->getLastTurnMovement($fireOrder->turn);
+		$posLaunch = mathlib::hexCoToPixel($movement->x, $movement->y);//at moment of launch!!!	
+		
+            	$target = array("x"=>$fireOrder->x, "y"=>$fireOrder->y);
+            
+            	$this->calculateHit($gamedata, $fireOrder);
+		
+		$rolled = Dice::d(100);
+		$fireOrder->rolled = $rolled;
+            	if ($rolled > $needed){ //miss!
+			$fireOrder->pubnotes .= "Charge dissipates. ";  
+		}else{//hit!
+			$fireOrder->shotshit++;
+			if($rolled>75){ //deviation!
+				$maxdis = mathlib::getDistanceHex($posLaunch, mathlib::hexCoToPixel($target["x"], $target["y"]));
+				$dis = Dice::d(6); //deviation distance
+				$dis = min($dis,floor($maxdis));
+				$direction = Dice::d(6); //deviation direction
+				for ($i=0;$i<$dis;$i++){
+					$target = mathlib::getHexToDirection($rolled, $target["x"], $target["y"]);
+				}
+				$fireOrder->x = $target["x"];
+				$fireOrder->y = $target["y"];
+				$fireOrder->pubnotes .= "Shot deviates $dis hexes. ";   
+			}
+			
+			//do damage to ships in range...
+			$pos = mathlib::hexCoToPixel($fireOrder->x, $fireOrder->y);
+                    	$ships1 = $gamedata->getShipsInDistance($pos);
+			$ships2 = $gamedata->getShipsInDistance($pos, mathlib::$hexWidth+1);
+			foreach($ships2 as $target){
+				if(isset($ships1[ $target->id])){ //ship on target hex!
+					$damage = $this->maxDamage;
+					$sourceHex = $posLaunch;
+				}else{ //ship at range 1!
+					$damage = $this->minDamage;
+					$sourceHex = $target;
+				}
+				
+				$this->AOEdamage( $target, $shooter, $fireOrder, $sourceHex, $damage, $gamedata);
+			}
+		}
+            }
+	    
+            $fireOrder->rolled = max(1,$fireOrder->rolled);//Marks that fire order has been handled, just in case it wasn't marked yet!
+	} //endof function fire
+	    
+
+	public function AOEdamage($target, $shooter, $fireOrder, $sourceHex, $damage, $gamedata){
+            if ($target->isDestroyed()) continue; //no point allocating
+		if ($target instanceof FighterFlight){
+		    foreach ($target->systems as $fighter){
+			if ($fighter == null || $fighter->isDestroyed()){
+			    continue;
+			}
+			$this->doDamage($target, $shooter, $fighter, $damage, $fireOrder, $sourceHex, $gamedata, false);
+		    }
+		}else{
+		    $tmpLocation = $target->getHitSectionPos($sourceHex, $fireOrder->turn);
+		    $system = $target->getHitSystem($shooter, $fireOrder, $this, $tmpLocation);
+		    $this->doDamage($target, $shooter, $system, $damage, $fireOrder, null, $gamedata, false, $tmpLocation);
+		}   
+        }
+
+
+/*old redefinitions - new ones are in their place now!	    
         public function calculateHit($gamedata, $fireOrder){
             $shooter = $gamedata->getShipById($fireOrder->shooterid);
             $shooterPos = $shooter->getCoPos(); 
@@ -88,6 +165,7 @@
             }
                   
         }
+
         
         public function AOEdamage($target, $shooter, $fireOrder, $pos, $amount, $gamedata){
             if ($target->isDestroyed()) return;                    
@@ -96,7 +174,7 @@
             if ($target instanceof FighterFlight){
 			$this->fighterDamage($target, $shooter, $fireOrder, $pos, $amount, $gamedata);
 	    } else {
-                if (/*$target instanceof StarBase*/$target->base || $target instanceof OSAT){
+                if ($target->base || $target instanceof OSAT){
                     $amount = floor($amount/2);
                 }
 
@@ -118,11 +196,12 @@
 			$this->doDamage($target, $shooter, $fighter, $amount, $fireOrder, $pos, $gamedata);
 		}
 	}
-    }
+*/		
+    } //endof class AoE
 
     
+
     class EnergyMine extends AoE{
-    
         public $name = "energyMine";
         public $displayName = "Energy Mine";
         public $range = 50;
@@ -132,7 +211,7 @@
         public $hidetarget = true;
         
         public $flashDamage = true;
-        public $priority = 2;
+        public $priority = 1;
         
             
         public $trailColor = array(141, 240, 255);
@@ -144,6 +223,8 @@
         public $projectilespeed = 12;
         public $animationWidth = 10;
         public $trailLength = 10;
+	    
+	    
         
         function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
             parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
@@ -154,7 +235,9 @@
             $this->data["Weapon type"] = "Ballistic";
         }
         
-        public function getDamage($fireOrder){        return 10;   }
+	    /*getDamage in itself depends on actually hit ship - this function is meaningless, really!*/
+        public function getDamage($fireOrder){        return 10;   } 
+	    /*these are important, though!*/
         public function setMinDamage(){     $this->minDamage = 10;      }
         public function setMaxDamage(){     $this->maxDamage = 30;      }
     
