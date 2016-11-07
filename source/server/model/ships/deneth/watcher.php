602154de224d7995cd7d77126ac3b31a310371e3
Zero1627 <reisen@poczta.onet.pl>
2016-11-07 16:38:06 +0100

    Update watcher.php

--- source/server/model/ships/deneth/watcher.php
+++ source/server/model/ships/deneth/watcher.php
@@ -1,16 +1,20 @@
 <?php
-class WatcherScout extends HeavyCombatVessel{
+class Watcher extends HeavyCombatVessel{
     
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
         $this->pointCost = 500;
         $this->faction = "Deneth";
-        $this->phpclass = "WatcherScout";
+        $this->phpclass = "watcher";
         $this->imagePath = "img/ships/brigantine.png"; //need to change
         $this->shipClass = "Watcher Scout";
+	    
         $this->occurence = "rare";
  	$this->limited = 33;
+	$this->variantOf = 'Protector Heavy Destroyer';
+	$this->isd = 2242;
+	    
         $this->fighters = array("normal"=>6);
         
         $this->forwardDefense = 13;
@@ -25,7 +29,7 @@ class WatcherScout extends HeavyCombatVessel{
         
         $this->addPrimarySystem(new Reactor(4, 20, 0, 0));
         $this->addPrimarySystem(new CnC(4, 12, 0, 0));
-        $this->addPrimarySystem(new ElintScanner(4, 12, 8, 8));
+        $this->addPrimarySystem(new ElintScanner(4, 24, 8, 8));
         $this->addPrimarySystem(new Engine(4, 16, 0, 9, 2));
         $this->addPrimarySystem(new Thruster(4, 13, 0, 4, 3));
         $this->addPrimarySystem(new Thruster(4, 13, 0, 4, 4));
@@ -33,16 +37,16 @@ class WatcherScout extends HeavyCombatVessel{
         
         $this->addFrontSystem(new Thruster(4, 8, 0, 3, 1));
         $this->addFrontSystem(new Thruster(4, 8, 0, 3, 1));
-	$this->addFrontSystem(new AssaultLaser(3, 6, 3, 270, 60));
 	$this->addFrontSystem(new TwinArray(2, 6, 2, 240, 0));
-	$this->addFrontSystem(new TwinArray(2, 6, 2, 0, 120));
 	$this->addFrontSystem(new TwinArray(2, 6, 2, 240, 60));
-	$this->addFrontSystem(new TwinArray(2, 6, 2, 270, 120));		
+	$this->addFrontSystem(new AssaultLaser(3, 6, 3, 300, 60));
+	$this->addFrontSystem(new TwinArray(2, 6, 2, 300, 120));
+	$this->addFrontSystem(new TwinArray(2, 6, 2, 0, 120));		
         
         $this->addAftSystem(new Thruster(3, 9, 0, 3, 2));
         $this->addAftSystem(new Thruster(4, 9, 0, 3, 2));
         $this->addAftSystem(new Thruster(3, 9, 0, 3, 2));
-        $this->addAftSystem(new TwinArray(2, 6, 2, 180, 270));
+        $this->addAftSystem(new TwinArray(2, 6, 2, 180, 300));
         $this->addAftSystem(new TwinArray(2, 6, 2, 60, 180));
         
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
