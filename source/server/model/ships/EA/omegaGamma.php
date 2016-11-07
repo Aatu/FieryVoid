ce3b6660cbb6a278f3853ccee519c68de56d36ed
Zero1627 <reisen@poczta.onet.pl>
2016-11-07 15:45:25 +0100

    Update omegaGamma.php

--- source/server/model/ships/EA/omegaGamma.php
+++ source/server/model/ships/EA/omegaGamma.php
@@ -6,12 +6,16 @@ class OmegaGamma  extends BaseShip{
         
         $this->pointCost = 1100;
         $this->faction = "EA";
-        $this->phpclass = "OmegaBeta";
+        $this->phpclass = "omegaGamma";
         $this->imagePath = "img/ships/omega.png";
-        $this->shipClass = "Omega Destroyer (Beta)";
+        $this->shipClass = "Omega Command Destroyer (Gamma)";
         $this->shipSizeClass = 3;
         $this->canvasSize = 280;
         $this->fighters = array("normal"=>24);
+	    
+	$this->isd = 2259;
+	$this->variantOf = 'Omega Destroyer (Alpha)';
+	$this->occurence = 'rare';
         
         $this->forwardDefense = 16;
         $this->sideDefense = 18;
@@ -21,33 +25,35 @@ class OmegaGamma  extends BaseShip{
         $this->accelcost = 3;
         $this->rollcost = 2;
         $this->pivotcost = 3;
+	    
+	$this->initiative = 1*5; 
          
         $this->addPrimarySystem(new Reactor(6, 25, 0, 0));
-        $this->addPrimarySystem(new CnC(6, 16, 0, 0));
+        $this->addPrimarySystem(new CnC(7, 20, 0, 0)); //I'm afraid to put in second C&C, so I combine their structure and increase armor
         $this->addPrimarySystem(new Scanner(6, 20, 4, 8));
         $this->addPrimarySystem(new Engine(6, 20, 0, 8, 3));
-		$this->addPrimarySystem(new Hangar(6, 26, 24));
+	$this->addPrimarySystem(new Hangar(6, 26, 24));
         $this->addPrimarySystem(new JumpEngine(6, 20, 3, 20));
+	    
         $this->addFrontSystem(new Thruster(3, 10, 0, 4, 1));
         $this->addFrontSystem(new Thruster(3, 10, 0, 4, 1));
-		$this->addFrontSystem(new HeavyPulse(4, 6, 4, 300, 0));
-		$this->addFrontSystem(new HeavyPulse(4, 6, 4, 300, 0));
-		$this->addFrontSystem(new HeavyPulse(4, 6, 4, 0, 60));
-		$this->addFrontSystem(new HeavyPulse(4, 6, 4, 0, 60));
+		$this->addFrontSystem(new HLPA(4, 0, 0, 300, 0));
+		$this->addFrontSystem(new HLPA(4, 0, 0, 300, 0));
+		$this->addFrontSystem(new HLPA(4, 0, 0, 0, 60));
+		$this->addFrontSystem(new HLPA(4, 0, 0, 0, 60));
         $this->addFrontSystem(new InterceptorMkII(2, 4, 2, 240, 60));
         $this->addFrontSystem(new InterceptorMkII(2, 4, 2, 300, 120));
+	    
+        $this->addAftSystem(new Thruster(4, 9, 0, 2, 2));
+	$this->addAftSystem(new Thruster(4, 9, 0, 2, 2));
         $this->addAftSystem(new Thruster(4, 9, 0, 2, 2));
 		$this->addAftSystem(new Thruster(4, 9, 0, 2, 2));
-        $this->addAftSystem(new Thruster(4, 9, 0, 2, 2));
-		$this->addAftSystem(new Thruster(4, 9, 0, 2, 2));
-		$this->addAftSystem(new MediumPulse(4, 6, 3, 180, 240));
-		$this->addAftSystem(new MediumPulse(4, 6, 3, 180, 240));
-		$this->addAftSystem(new MediumPulse(4, 6, 3, 120, 180));
-		$this->addAftSystem(new MediumPulse(4, 6, 3, 120, 180));
+		$this->addAftSystem(new HLPA(4, 0, 0, 180, 240));
+		$this->addAftSystem(new HLPA(4, 0, 0, 120, 180));
         $this->addAftSystem(new InterceptorMkII(2, 4, 2, 120, 300));
         $this->addAftSystem(new InterceptorMkII(2, 4, 2, 60, 240));
         
-		$this->addLeftSystem(new Thruster(5, 15, 0, 5, 3));
+	$this->addLeftSystem(new Thruster(5, 15, 0, 5, 3));
         $this->addLeftSystem(new StdParticleBeam(2, 4, 1, 180, 0));
         $this->addLeftSystem(new StdParticleBeam(2, 4, 1, 180, 0));
         $this->addLeftSystem(new StdParticleBeam(2, 4, 1, 180, 0));
@@ -55,6 +61,7 @@ class OmegaGamma  extends BaseShip{
         $this->addLeftSystem(new StdParticleBeam(2, 4, 1, 180, 0));
         $this->addLeftSystem(new StdParticleBeam(2, 4, 1, 180, 0));
         $this->addLeftSystem(new InterceptorMkII(2, 4, 2, 180, 0));
+	    
         $this->addRightSystem(new Thruster(5, 15, 0, 5, 4));
         $this->addRightSystem(new StdParticleBeam(2, 4, 1, 0, 180));
         $this->addRightSystem(new StdParticleBeam(2, 4, 1, 0, 180));
@@ -84,14 +91,14 @@ class OmegaGamma  extends BaseShip{
                 ),
                 1=> array(
                         3 => "Thruster",
-                        8 => "Heavy Pulse Cannon",
+                        8 => "Heavy Laser-Pulse Array",
                         11 => "Interceptor II",
                         18 => "Structure",
                         20 => "Primary",
                 ),
                 2=> array(
                         6 => "Thruster",
-                        9 => "Medium Pulse Cannon",
+                        9 => "Heavy Laser-Pulse Array",
                         12 => "Interceptor II",
                         18 => "Structure",
                         20 => "Primary",
