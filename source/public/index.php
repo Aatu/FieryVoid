b897719fa91f3efa8c2f39fdf6909cf10225e531
Zero1627 <reisen@poczta.onet.pl>
2016-11-14 01:25:17 +0100

    link Register new player account

--- source/public/index.php
+++ source/public/index.php
@@ -34,7 +34,7 @@
 <!--        <div class="helphide" style="float:right"> <div id="helphideimg"></div>
         </div>-->
 		<div class="panel" style="width:400px;margin:auto;">
-			<br><a href="http://fieryvoid.net/reg.php">Register new player account</a>";
+			<a href="http://fieryvoid.net/reg.php">Register new player account</a><br>;
 			<form method="post">
 				<table>
 				<tr><td><label>Username:</label></td><td><input type="text" name="user"></input></td></tr>
