a9ab4a69406255713189235d9a9873136fcd055e
Zero1627 <reisen@poczta.onet.pl>
2016-11-16 13:14:29 +0100

    Update swCorellianCorvette.php

--- source/server/model/ships/starwars_centauri/swCorellianCorvette.php
+++ source/server/model/ships/starwars_centauri/swCorellianCorvette.php
@@ -23,24 +23,28 @@ class swCorellianCorvette extends MediumShip{
         $this->accelcost = 2;
         $this->rollcost = 2;
         $this->pivotcost = 4;
-	$this->iniativebonus = 12 *5;
+	$this->iniativebonus = 10 *5; //as semi-civilian ship, initiative isn't what it would be for a true warship
         
-        $this->addPrimarySystem(new Reactor(4, 10, 0, 6));
-        $this->addPrimarySystem(new CnC(4, 8, 0, 0));
+        $this->addPrimarySystem(new Reactor(4, 10, 0, 5));
         $this->addPrimarySystem(new Scanner(3, 8, 4, 4));
         $this->addPrimarySystem(new Engine(4, 13, 0, 9, 3));
 	$this->addPrimarySystem(new Hangar(2, 4));
 	$this->addPrimarySystem(new Thruster(2, 10, 0, 4, 3));
+	$this->addPrimarySystem(new Thruster(2, 10, 0, 4, 4));
+	$this->addPrimarySystem(new CargoBay(2, 30));
+	$this->addPrimarySystem(new CargoBay(2, 30));
 	$this->addPrimarySystem(new Thruster(2, 10, 0, 4, 4));
 	    
 	$hyperdrive = new JumpEngine(4, 8, 4, 10);
 	$hyperdrive->displayName = 'Hyperdrive';
 	$this->addPrimarySystem($hyperdrive);
 	    
-	$this->addPrimarySystem(new GuardianArray(0, 4, 2, 185, 175)); //_almost_ all-around! vast thrusters block firing arc directly aft...
-		
+	$this->addPrimarySystem(new SWMediumTLaser(2, 0, 360, 2)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
+	$this->addPrimarySystem(new SWMediumTLaser(2, 0, 360, 2)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
+			    
         $this->addFrontSystem(new Thruster(2, 8, 0, 3, 1));
         $this->addFrontSystem(new Thruster(2, 8, 0, 3, 1));
+        $this->addFrontSystem(new CnC(4, 8, 0, 0));
 	$this->addFrontSystem(new SWRayShield(2,6,3,2,270,90)); //$armour, $maxhealth, $powerReq, $shieldFactor, $startArc, $endArc
 		
         $this->addAftSystem(new Thruster(3, 8, 0, 3, 2));
@@ -48,7 +52,38 @@ class swCorellianCorvette extends MediumShip{
 	$this->addAftSystem(new Thruster(3, 8, 0, 3, 2));
 	$this->addAftSystem(new SWRayShield(2,6,3,2,90,270)); //$armour, $maxhealth, $powerReq, $shieldFactor, $startArc, $endArc
        
-        $this->addPrimarySystem(new Structure( 3, 40));
+        $this->addPrimarySystem(new Structure( 3, 50));
+	    
+	    
+	    
+
+        $this->hitChart = array(
+        		0=> array(
+        				5 => "Thruster",
+        				10 => "Cargo Bay",
+        				13 => "Medium Turbolaser",
+        				15 => "Scanner",
+        				17 => "Engine",
+        				18 => "Hangar",
+        				19 => "Hyperdrive",
+        				20 => "Reactor",
+        		),
+        		1=> array(
+        				5 => "Thruster",
+        				7 => "Ray Shield",
+        				16 => "Structure",
+        				17 => "C&C",
+        				20 => "Primary",
+        		),
+        		2=> array(
+        				9 => "Thruster",
+        				11 => "Ray Shield",
+        				17 => "Structure",
+        				20 => "Primary",
+        		),
+        );
+	    
+	    
     }
 }
 ?>
