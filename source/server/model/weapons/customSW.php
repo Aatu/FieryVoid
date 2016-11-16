7aa66b160874fffa9897f004ebfc838b912fa6bd
Zero1627 <reisen@poczta.onet.pl>
2016-11-16 10:07:28 +0100

    Update customSW.php

--- source/server/model/weapons/customSW.php
+++ source/server/model/weapons/customSW.php
@@ -1,15 +1,96 @@
 <?php
-/*custom weapons - from StarWars universe (to keep them separate)*/
+/*custom weapons - from StarWars universe (to keep them separate)
+
+   WARNING: general classes from this file aren't included in autoload.php file
+   	they would need to be if they are used by a system outside of customSW.php file.
+	
+*/
+
+
+
+
+/*
+	StarWars Ray Shield: does not affect profile
+	protects vs all weapoon classes except Matter, Ballistic and SW Ion
+	doubly effective vs Raking weapons (to simulate longer burst)
+*/
+class SWRayShield extends Shield implements DefensiveSystem{
+    public $name = "swrayshield";
+    public $displayName = "Ray Shield";
+    public $iconPath = "shield.png";
+    public $boostable = true; //$this->boostEfficiency and $this->maxBoostLevel in __construct() 
+    public $baseOutput = 0; //base output, before boost
+	
+	
+ 	public $possibleCriticals = array( //different than usual B5Wars shield
+            16=>"OutputReduced1",
+            23=>array("OutputReduced1", "OutputReduced1")
+	);
+	
+    function __construct($armour, $maxhealth, $powerReq, $shieldFactor, $startArc, $endArc){
+        // shieldfactor is handled as output.
+        parent::__construct($armour, $maxhealth, $powerReq, $shieldFactor, $startArc, $endArc);
+	$this->baseOutput = $shieldFactor;
+	$this->boostEfficiency = $powerReq;
+	$this->maxBoostLevel = min(2,$shieldFactor); //maximum of +2 effect, costs $powerReq each - but can't more than double shield!
+    }
+	
+    public function onConstructed($ship, $turn, $phase){
+        parent::onConstructed($ship, $turn, $phase);
+		$this->tohitPenalty = 0;
+		$this->damagePenalty = $this->getOutput();
+    }
+	
+    public function getDefensiveHitChangeMod($target, $shooter, $pos, $turn, $weapon){ //no defensive hit chance change
+            return 0;
+    }
+    private function checkIsFighterUnderShield($target, $shooter){ //no flying under SW shield
+        return false;
+    }
+	
+    public function getDefensiveDamageMod($target, $shooter, $pos, $turn, $weapon){
+        if($this->isDestroyed($turn-1) || $this->isOfflineOnTurn()) return 0; //destroyed shield gives no protection
+        $output = $this->output + $this->getBoostLevel($turn);
+	//Ballistic, Matter, SWIon - passes through!
+	if($weapon->weaponClass == 'Ballistic' || $weapon->weaponClass == 'Matter' || $weapon->weaponClass == 'SWIon' ) $output = 0;
+        $output -= $this->outputMod;
+	if($weapon->damageType == 'Raking') $output = 2*$output;//Raking - double effect!
+	$output=max(0,$output); //no less than 0!
+        return $output;
+    }
+	
+    public function setSystemDataWindow($turn){
+	parent::setSystemDataWindow($turn);
+	//$this->output = $this->baseOutput + $this->getBoostLevel($turn); //handled in front end
+	$this->data["Basic Strength"] = $this->baseOutput;      
+	$this->data["<font color='red'>Remark</font>"] = "<br>Does not decrease profile."; 
+	$this->data["<font color='red'>Remark</font>"] .= "<br>Does not protect from Ballistic, Matter and StarWars Ion damage."; 
+	$this->data["<font color='red'>Remark</font>"] .= "<br>Doubly effective vs Raking weapons."; 
+    }
+	  
+        private function getBoostLevel($turn){
+            $boostLevel = 0;
+            foreach ($this->power as $i){
+                    if ($i->turn != $turn) continue;
+                    if ($i->type == 2){
+                            $boostLevel += $i->amount;
+                    }
+            }
+            return $boostLevel;
+        }
+	
+} //endof class SWRayShield
+
 
 
 class SWDirectWeapon extends Pulse{
     /*StarWars weapon - extension of Pulse mode!*/
     public $shots = 1;
-    public $firingModes = array( 1 => "Standard");  //and give option of linked fire immediately, too!
+    public $firingModes = array( 1 => "Burst");  
 	
 	//for Pulse mode
-	public $grouping = 25;
-	public $maxpulses = 1;	
+	//public $grouping = 25;
+	//public $maxpulses = 1;	
 	private $useDie = 3; //die used for base number of hits
  
     public $damageType = "Pulse"; //and this should remain!
@@ -20,8 +101,8 @@ class SWDirectWeapon extends Pulse{
         public $animationColor = array(245, 75, 95);
         public $animationExplosionScale = 0.15;
         public $projectilespeed = 11;
-    public $animationWidth = 3;
-    public $trailLength = 8;
+	public $animationWidth = 3;
+	public $trailLength = 8;
 	
     
 	function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc, $nrOfShots){
@@ -37,13 +118,13 @@ class SWDirectWeapon extends Pulse{
 	
         public function setSystemDataWindow($turn){
 		parent::setSystemDataWindow($turn);
-		 $this->data["Special"] = 'Pulse mode: 0..2 +1/'. $this->grouping."%, max. ".$this->maxpulses." pulses";
+		 $this->data["Special"] = 'Burst mode: 0..2 +1/'. $this->grouping."%, max. ".$this->maxpulses." pulses";
 		$this->data["Special"] .= '<br>Minimum of 1 pulse.';
         }
 	
     
 	public function rollPulses($turn, $needed, $rolled){
-		$pulses = $this->getPulses($turn);
+		$pulses = $this->getPulses($turn); //$this->useDie usually
 		$pulses -= 1;
 		$pulses+= $this->getExtraPulses($needed, $rolled);
 		$pulses=min($pulses,$this->maxpulses); //no more than maxpulses
@@ -51,7 +132,31 @@ class SWDirectWeapon extends Pulse{
 		return $pulses;
 	}
 	
-	//these will ned to be overridden!
+	/*derives concentrate salvo mode from data of weapon; damage functions must be prepared to consider it!
+	  to be called in create function where needed
+	  AFTER calling parent::__construct! (else variables like maxpulses may be not yet filled correctly
+	*/
+	public function addSalvoMode{
+		$this->firingModes[2] = 'Salvo';
+		$new = ceil($this->animationExplosionScale*1.25);
+		$this->animationExplosionScaleArray = array(1=>$this->animationExplosionScale, 2=>$new);
+		$new = ceil($this->animationWidth*1.25);
+		$this->animationWidthArray = array(1=>$this->animationWidth, 2=>$new);
+		$new = ceil($this->trailLength*1.25);
+		$this->trailLengthArray = array(1=>$this->trailLengthScale, 2=>$new);
+		$this->maxpulsesArray = array(1=>$this->maxpulses, 2=>1);
+		$this->defaultShotsArray = array(1=>$this->defaultShots, 2=>1);
+		
+		foreach($this->firingModes as $i=>$modeName){ //recalculating min and max damage - taking into account new firing mode!	    
+			$this->changeFiringMode($i);
+			$this->setMinDamage(); $this->minDamageArray[$i] = $this->minDamage;
+			$this->setMaxDamage(); $this->maxDamageArray[$i] = $this->maxDamage;
+		}
+		$this->changeFiringMode(1); //reset mode to basic
+		
+	}
+	
+	//these will ned to be overridden for each particular weapon! (taking into accunt salvo mode if eligible)
 	//public function getDamage($fireOrder){        return ??;   }
 	//public function setMinDamage(){     $this->minDamage = 1+$this->damagebonus ;      }
 	//public function setMaxDamage(){     $this->maxDamage = 6+$this->damagebonus ;      }
@@ -61,8 +166,8 @@ class SWDirectWeapon extends Pulse{
 
 
 /*base class for StarWars Ion weapons*/
-class SWIon extends SWDirectWeapon{	
-	/*compared to SW Lasers: a bit better range, but poor FC and damage (and possible RoF as well)*/
+/*compared to SW Lasers: a bit better range, but poor FC and damage (and possible RoF as well)*/
+class SWIon extends SWDirectWeapon{
     public $name = "swion";
     public $priority = 10; //Ions usually fire last, to take advantage of induced criticals
  
@@ -70,6 +175,15 @@ class SWIon extends SWDirectWeapon{
 	  
     //public $systemKiller = true; //let's not go overhead - do NOT use $systemKiller...
 
+    //animation for fighter gun - bigger guns need to change size and speed attributes :)
+    public $animation = "beam";
+    public $projectilespeed = 9;
+    public $animationExplosionScale = 0.15;
+    public $animationWidth = 3;
+    public $trailLength = 8;
+    public $animationColor =  array( 100, 100, 245);
+    public $trailColor = array( 100, 100, 245);
+	
 	
     public function setSystemDataWindow($turn){
       parent::setSystemDataWindow($turn);
@@ -95,9 +209,9 @@ class SWIon extends SWDirectWeapon{
 /*static class to handle accumulating Ion damage*/
 class SWIonHandler{
 	private static $accumulatedIonDmg = array();
-	private static $power = 1.5; //effect magnitude from hit: damage ^power
-	private static $free = 2; //this much damage doesn't cause anything
-	private static $threshold = 7; //this much damage (after $free) causes power shortage
+	private static $power = 1.4; //effect magnitude from hit: damage ^power
+	private static $free = 1; //this much damage doesn't cause anything
+	private static $threshold = 6; //this much damage (after $free) causes power shortage
 	private static $turn = 0; //turn for which data is held
 
 	
@@ -135,81 +249,6 @@ class SWIonHandler{
 }//endof class SWIonHandlerHandler
 
 
-
-
-/*
-	StarWars Ray Shield: does not affect profile
-	protects vs all weapoon classes except Matter, Ballistic and SW Ion
-	doubly effective vs Raking weapons (to simulate longer burst)
-*/
-class SWRayShield extends Shield implements DefensiveSystem{
-    public $name = "swrayshield";
-    public $displayName = "Ray Shield";
-    public $iconPath = "shield.png";
-    public $boostable = true; //$this->boostEfficiency and $this->maxBoostLevel in __construct() 
-    public $baseOutput = 0; //base output, before boost
-	
-	
- 	public $possibleCriticals = array( //different than usual B5Wars shield
-            16=>"OutputReduced1",
-            23=>array("OutputReduced1", "OutputReduced1")
-	);
-	
-    function __construct($armour, $maxhealth, $powerReq, $shieldFactor, $startArc, $endArc){
-        // shieldfactor is handled as output.
-        parent::__construct($armour, $maxhealth, $powerReq, $shieldFactor, $startArc, $endArc);
-	$this->baseOutput = $shieldFactor;
-	$this->boostEfficiency = $powerReq;
-	$this->maxBoostLevel = min(2,$shieldFactor); //maximum of +2 effect, costs $powerReq each - but can't more than double shield!
-    }
-	
-    public function onConstructed($ship, $turn, $phase){
-        parent::onConstructed($ship, $turn, $phase);
-		$this->tohitPenalty = 0;
-		$this->damagePenalty = $this->getOutput();
-    }
-	
-   public function getDefensiveHitChangeMod($target, $shooter, $pos, $turn, $weapon){ //no defensive hit chance change
-            return 0;
-    }
-	
-    
-    private function checkIsFighterUnderShield($target, $shooter){ //no flying under SW shield
-        return false;
-    }
-	
-    public function getDefensiveDamageMod($target, $shooter, $pos, $turn, $weapon){
-        if($this->isDestroyed($turn-1) || $this->isOfflineOnTurn()) return 0; //destroyed shield gives no protection
-        $output = $this->output + $this->getBoostLevel($turn);
-	//Ballistic, Matter, SWIon - passes through!
-	if($weapon->weaponClass == 'Ballistic' || $weapon->weaponClass == 'Matter' || $weapon->weaponClass == 'SWIon' ) $output = 0;
-        $output -= $this->outputMod;
-	//Raking - double effect!
-	if($weapon->damageType == 'Raking') $output = 2*$output;
-	$output=max(0,$output); //no less than 0!
-        return $output;
-    }
-	
-    public function setSystemDataWindow($turn){
-	parent::setSystemDataWindow($turn);
-	//$this->output = $this->baseOutput + $this->getBoostLevel($turn); //handled in front end
-	$this->data["Basic Strength"] = $this->baseOutput;      
-	$this->data["<font color='red'>Remark</font>"] = "<br>Does not decrease profile."; 
-	$this->data["<font color='red'>Remark</font>"] .= "<br>Does not protect from Ballistic, Matter and StarWars Ion damage."; 
-    }
-	  
-        private function getBoostLevel($turn){
-            $boostLevel = 0;
-            foreach ($this->power as $i){
-                    if ($i->turn != $turn) continue;
-                    if ($i->type == 2){
-                            $boostLevel += $i->amount;
-                    }
-            }
-            return $boostLevel;
-        }
-	
-} //endof class SWRayShield
 
 
 
@@ -253,19 +292,11 @@ class SWFighterIon extends SWIon{
     public $displayName = "Fighter Ion Cannon";
 	public $iconPath = "starwars/swFighterIon1.png";	  
 	  
-    //public $animation = "trail";
-	public $animation = "beam";
-    public $projectilespeed = 9;
-    public $animationExplosionScale = 0.15;
-    public $animationWidth = 3;
-    public $trailLength = 8;
-    public $animationColor =  array( 100, 100, 245);
-    public $trailColor = array( 100, 100, 245);
+
 	
 
     public $loadingtime = 1;
     public $rangePenalty = 1.5; //poor FC, but good range compared to Lasers! Perhaps lower rate of fire, too - but that would not be noticeable on fighter weapons (maybe in damage)
-    public $firingModes = array( 1 => "Standard");  
     public $fireControl = array(-3, -1, -1); // fighters, <mediums, <capitals
 
     public $exclusive = false; //can be always overridden in particular fighter!
@@ -274,11 +305,9 @@ class SWFighterIon extends SWIon{
 	
 	function __construct($startArc, $endArc, $damagebonus, $nrOfShots){
 		$this->damagebonus = $damagebonus;
-		$this->shots = $nrOfShots;
-		$this->defaultShots = $nrOfShots;
 		$this->intercept = 0;
 
-		parent::__construct(0, 1, 0, $startArc, $endArc);
+		parent::__construct(0, 1, 0, $startArc, $endArc, $nrOfShots);
 	}    
 
 			  
