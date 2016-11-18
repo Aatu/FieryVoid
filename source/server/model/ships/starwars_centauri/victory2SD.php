454cdfe9fe7be5708350769e6b970be7f1ba1768
Zero1627 <reisen@poczta.onet.pl>
2016-11-18 13:24:22 +0100

    Update victory2SD.php

--- source/server/model/ships/starwars_centauri/victory2SD.php
+++ source/server/model/ships/starwars_centauri/victory2SD.php
@@ -32,8 +32,9 @@ class Victory2SD extends BaseShip{
 	    
      
 	    
+	    
 	$this->addPrimarySystem(new CnC(6, 20, 0, 0));
-        $this->addPrimarySystem(new Reactor(5, 26, 0, 10));
+        $this->addPrimarySystem(new Reactor(5, 28, 0, 10));
         $this->addPrimarySystem(new Scanner(4, 14, 4, 3)); //split to Aft, too
         $this->addPrimarySystem(new Engine(4, 20, 0, 6, 5)); //split to Aft, too
 	$hyperdrive = new JumpEngine(4, 24, 8, 20);
@@ -41,13 +42,12 @@ class Victory2SD extends BaseShip{
 	$this->addPrimarySystem($hyperdrive);
 	$this->addPrimarySystem(new Hangar(3, 48, 12));
          
-
         
 		
         $this->addFrontSystem(new Thruster(3, 18, 0, 4, 1));
         $this->addFrontSystem(new Thruster(3, 18, 0, 4, 1));
 	$this->addFrontSystem(new SWRayShield(3,13,8,4,330,30)); //$armour, $maxhealth, $powerReq, $shieldFactor, $startArc, $endArc
-	$this->addFrontSystem(new SWCapitalConcussion(3, 300, 60, 4)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
+	$this->addFrontSystem(new SWHeavyIon(3, 300, 60, 3)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
 	$this->addFrontSystem(new SWHeavyTLaser(3, 300, 60, 2)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
 	$this->addFrontSystem(new SWHeavyTLaser(3, 300, 60, 2)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
 	$this->addFrontSystem(new SWHeavyLaser(3, 300, 60, 4)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
@@ -57,35 +57,33 @@ class Victory2SD extends BaseShip{
         $this->addAftSystem(new Thruster(2, 18, 0, 4, 2));
 	$this->addAftSystem(new Thruster(2, 18, 0, 4, 2));
 	$this->addAftSystem(new Thruster(2, 18, 0, 4, 2));
-        $this->addAftSystem(new Scanner(3, 12, 4, 3)); //split to Primary, too
+        $this->addAftSystem(new Scanner(3, 14, 5, 4)); //split to Primary, too
         $this->addAftSystem(new Engine(4, 15, 0, 4, 5)); //split to Primary, too
 	$this->addAftSystem(new SWRayShield(2,10,5,3,210,330)); //$armour, $maxhealth, $powerReq, $shieldFactor, $startArc, $endArc
 	$this->addAftSystem(new SWHeavyLaser(2, 120, 240, 4)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
 	$this->addAftSystem(new SWHeavyLaser(2, 120, 240, 4)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
-	    
+	$this->addAftSystem(new SWHeavyIon(2, 120, 240, 1));
         
 	    
 	$this->addLeftSystem(new Thruster(3, 20, 0, 4, 3));
 	$this->addLeftSystem(new SWRayShield(3,13,8,3,210,330)); //$armour, $maxhealth, $powerReq, $shieldFactor, $startArc, $endArc
-	$this->addLeftSystem(new SWHeavyTLaser(3, 240, 0, 2)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
-	$this->addLeftSystem(new SWHeavyTLaser(3, 240, 0, 2));
-	$this->addLeftSystem(new SWHeavyTLaser(3, 240, 0, 2));
+	$this->addLeftSystem(new SWHeavyTLaser(3, 240, 0, 3)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
+	$this->addLeftSystem(new SWHeavyTLaser(3, 240, 0, 3));
+	$this->addLeftSystem(new SWHeavyTLaser(3, 240, 0, 3));
 	$this->addLeftSystem(new SWHeavyTLaser(3, 270, 30, 2)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
 	$this->addLeftSystem(new SWHeavyLaser(3, 240, 0, 4)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
-	$this->addLeftSystem(new SWCapitalConcussion(3, 240, 0, 4));
-	$this->addLeftSystem(new SWCapitalConcussion(3, 240, 0, 4));
+	$this->addLeftSystem(new SWHeavyIon(3, 240, 0, 3));
 
 		
 	    
 	$this->addRightSystem(new Thruster(3, 20, 0, 4, 4));
 	$this->addRightSystem(new SWRayShield(3,13,8,3,30,150)); //$armour, $maxhealth, $powerReq, $shieldFactor, $startArc, $endArc
-	$this->addRightSystem(new SWHeavyTLaser(3, 0, 120, 2)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
-	$this->addRightSystem(new SWHeavyTLaser(3, 0, 120, 2));
-	$this->addRightSystem(new SWHeavyTLaser(3, 0, 120, 2));
+	$this->addRightSystem(new SWHeavyTLaser(3, 0, 120, 3)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
+	$this->addRightSystem(new SWHeavyTLaser(3, 0, 120, 3));
+	$this->addRightSystem(new SWHeavyTLaser(3, 0, 120, 3));
 	$this->addRightSystem(new SWHeavyTLaser(3, 330, 90, 2)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
 	$this->addRightSystem(new SWHeavyLaser(3, 0, 120, 4)); //armor, arc and number of weapon in common housing: structure and power data are calculated!
-	$this->addRightSystem(new SWCapitalConcussion(3, 0, 120, 4));
-	$this->addRightSystem(new SWCapitalConcussion(3, 0, 120, 4));
+	$this->addLeftSystem(new SWHeavyIon(3, 0, 120, 3));
 	    
 	    
         
@@ -112,14 +110,15 @@ class Victory2SD extends BaseShip{
 		    6 => "Ray Shield",
                     9 => "Heavy Turbolaser",
                     10 => "Heavy Laser",
-                    12 => "Concussion Missile Battery",
+                    11 => "Heavy Ion Cannon",
                     18 => "Structure",
                     20 => "Primary",
             ),
             2=> array(
                     7 => "Thruster",
 		    8 => "Ray Shield",
-                    10 => "Heavy Laser",
+                    9 => "Heavy Laser",
+		    10 => "Heavy Ion Cannon",
                     12 => "Scanner",
                     13 => "Engine",
                     17 => "Structure",
@@ -128,18 +127,18 @@ class Victory2SD extends BaseShip{
             3=> array(
                     4 => "Thruster",
 		    5 => "Ray Shield",
-                    9 => "Heavy Turbolaser",
-                    10 => "Heavy Laser",
-                    13 => "Concussion Missile Battery",
+                    10 => "Heavy Turbolaser",
+                    11 => "Heavy Laser",
+                    13 => "Heavy Ion Cannon",
                     18 => "Structure",
                     20 => "Primary",
             ),
             4=> array(
                     4 => "Thruster",
 		    5 => "Ray Shield",
-                    9 => "Heavy Turbolaser",
-                    10 => "Heavy Laser",
-                    13 => "Concussion Missile Battery",
+                    10 => "Heavy Turbolaser",
+                    11 => "Heavy Laser",
+                    13 => "Heavy Ion Cannon",
                     18 => "Structure",
                     20 => "Primary",
             ),
