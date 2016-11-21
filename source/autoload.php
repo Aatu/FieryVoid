2a9e1277f70eb7f79cfb0c35f97eba0d50ba7798
Zero1627 <reisen@poczta.onet.pl>
2016-11-21 23:05:22 +0100

    Update autoload.php

--- source/autoload.php
+++ source/autoload.php
@@ -107,6 +107,8 @@ spl_autoload_register(
                 'swfighterlaser' => '/server/model/weapons/customSW.php',
                 'swftrprotontorpedolauncher' => '/server/model/weapons/customSW.php', //weapon
                 'swftrprotontorpedo' => '/server/model/weapons/customSW.php', //ammo
+                'swftrconcmissilelauncher' => '/server/model/weapons/customSW.php', //weapon
+                'swftconcmissile' => '/server/model/weapons/customSW.php', //ammo
                 
                 'swlightlaser' => '/server/model/weapons/customSW.php',
                 'swmediumlaser' => '/server/model/weapons/customSW.php',
