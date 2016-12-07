ce39ec380ac2c6e554918d140f7a82245786e175
Zero1627 <reisen@poczta.onet.pl>
2016-12-07 13:43:00 +0100

    Update swDreadnought.php

--- source/server/model/ships/starwars_centauri/swDreadnought.php
+++ source/server/model/ships/starwars_centauri/swDreadnought.php
@@ -8,7 +8,7 @@ class swDreadnought extends HeavyCombatVessel{
 	$this->faction = "StarWars Galactic Empire";
         $this->phpclass = "swDreadnought";
         $this->imagePath = "img/starwars/dreadnaught.png";
-	    $this->canvasSize = 100;
+	    //$this->canvasSize = 100;
         $this->shipClass = "Dreadnought";
 	
 	$this->fighters = array("fighter flights"=>2);
