652edcba2f15e1d2bf62afc83271abd8aa9a2656
Zero1627 <reisen@poczta.onet.pl>
2017-01-16 13:28:28 +0100

    Update rakartaCannon.php

--- source/server/model/ships/brakiri/rakartaCannon.php
+++ source/server/model/ships/brakiri/rakartaCannon.php
@@ -10,7 +10,7 @@ class RakartaCannon extends MediumShip{
 		$this->imagePath = "img/ships/rakarta.png";
 		$this->shipClass = "Rakarta Frigate (non-Kam-Lassit)";
 		$this->canvasSize = 100;
-		$this->shipClass = "Rakarta Frigate";
+		$this->variantOf = "Rakarta Frigate";
 
 		$this->forwardDefense = 11;
 		$this->sideDefense = 13;
