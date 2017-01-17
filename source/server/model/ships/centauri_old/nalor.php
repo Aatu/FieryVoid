f6697306e14f2ef2240f921491c27a826043d79e
Zero1627 <reisen@poczta.onet.pl>
2017-01-17 10:11:11 +0100

    Update nalor.php

--- source/server/model/ships/centauri_old/nalor.php
+++ source/server/model/ships/centauri_old/nalor.php
@@ -25,7 +25,7 @@ class Nalor extends BaseShip{
         $this->pivotcost = 3;
         $this->occurence = "uncommon";    
          
-        $this->addPrimarySystem(new Reactor(6, 16, 0, 0));
+        $this->addPrimarySystem(new Reactor(6, 16, 0, 3));
         $this->addPrimarySystem(new CnC(6, 14, 0, 0));
         $this->addPrimarySystem(new Scanner(5, 18, 4, 8));
         $this->addPrimarySystem(new Engine(5, 20, 0, 9, 3));
