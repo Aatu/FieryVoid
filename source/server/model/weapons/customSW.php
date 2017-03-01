c5baa421a3716d169ab9eeeaca1d350743356098
Zero1627 <reisen@poczta.onet.pl>
2017-03-01 09:15:52 +0100

    Update customSW.php

--- source/server/model/weapons/customSW.php
+++ source/server/model/weapons/customSW.php
@@ -1191,9 +1191,9 @@ class SWTractorBeam extends SWDirectWeapon{
 	}    
 	
 	protected function onDamagedSystem($ship, $system, $damage, $armour, $gamedata, $fireOrder){ //target is held critical on PRIMARY Structure!
-	      $primaryStruct = $ship->getStructureSystem(0); //primary Structure is where the crit will reside - it has to be there!
-	      if($system->isDestroyed()) return; //destroyed system - critical is irrelevant
-	      $crit = new SWTargetHeld(-1, $ship->id, $primaryStruct->id, $gamedata->turn); //for ship system and fighter alike
+	      $primaryStruct = $ship->getStructureSystem(0); //primary Structure is where the crit will reside - it has to be there! (weapon does not target fighters)
+	      if($primaryStruct->isDestroyed()) return; //destroyed system - critical is irrelevant
+	      $crit = new SWTargetHeld(-1, $ship->id, $primaryStruct->id, $gamedata->turn); 
 	      $system->criticals[] =  $crit;
 	}
 	
