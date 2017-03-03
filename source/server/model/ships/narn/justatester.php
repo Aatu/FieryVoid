90a5d5cb155790cf62139e4fc0ef9c2d306352f4
Zero1627 <reisen@poczta.onet.pl>
2017-03-03 12:18:37 +0100

    Update justatester.php

--- source/server/model/ships/narn/justatester.php
+++ source/server/model/ships/narn/justatester.php
@@ -1,5 +1,5 @@
 <?php
-class narnabp extends FighterFlight{
+class trakhtest extends FighterFlight{
     /*T'Rakh Narn Armed Breaching Pod*/
     
     function __construct($id, $userid, $name,  $slot){
@@ -7,7 +7,7 @@ class narnabp extends FighterFlight{
         
 		    $this->pointCost = 30*6;
 		    $this->faction = "Narn";
-        $this->phpclass = "narnabp";
+        $this->phpclass = "trakhtest";
         $this->shipClass = "T'Khar Assault Shuttles";
 		    $this->imagePath = "img/ships/gorith.png";
         
@@ -32,7 +32,7 @@ class narnabp extends FighterFlight{
         for ($i = 0; $i < $toAdd; $i++){
 			
 			$armour = array(2, 2, 2, 2);
-			$fighter = new Fighter("narnabp", $armour, 8, $this->id);
+			$fighter = new Fighter("trakhtest", $armour, 8, $this->id);
 			$fighter->displayName = "T'Khar Assault Shuttle";
 			$fighter->imagePath = "img/ships/gorith.png";
 			$fighter->iconPath = "img/ships/gorith_large.png";
