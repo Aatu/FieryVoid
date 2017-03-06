3f122ee7dcc055e6aab9d323cb185d903e513487
Zero1627 <reisen@poczta.onet.pl>
2017-03-06 16:23:15 +0100

    Update and rename erlorraEarlyPods to erlorraEarlyPods.php

--- source/server/model/ships/llort/erlorraEarlyPods.php
+++ source/server/model/ships/llort/erlorraEarlyPods.php
@@ -51,11 +51,11 @@ class ErlorraEarlyPods extends BaseShip{
         $this->addLeftSystem(new ScatterGun(3, 8, 3, 180, 360));
   
         $this->addRightSystem(new Thruster(4, 13, 0, 4, 4));
-        $this->addRightySystem(new CargoBay(2, 20));
-        $this->addRightySystem(new CargoBay(2, 20));
-        $this->addRightySystem(new CargoBay(2, 20));
-        $this->addRightySystem(new CargoBay(2, 20));
-        $this->addRightySystem(new CargoBay(2, 20));
+        $this->addRightSystem(new CargoBay(2, 20));
+        $this->addRightSystem(new CargoBay(2, 20));
+        $this->addRightSystem(new CargoBay(2, 20));
+        $this->addRightSystem(new CargoBay(2, 20));
+        $this->addRightSystem(new CargoBay(2, 20));
         $this->addRightSystem(new TwinArray(2, 6, 2, 90, 180));
         
         
