3ab9b588d358f327501f8b3026ca47995f279de4
Zero1627 <reisen@poczta.onet.pl>
2017-03-07 13:35:55 +0100

    Update phalanMNav.php

--- source/server/model/ships/centauri_old/phalanMNav.php
+++ source/server/model/ships/centauri_old/phalanMNav.php
@@ -30,8 +30,8 @@ class PhalanMNav extends FighterFlight{
         for ($i = 0; $i < $toAdd; $i++){
 			
 			$armour = array(2, 1, 1, 1);
-			$fighter = new Fighter("phalani", $armour, 12, $this->id);
-			$fighter->displayName = "Centauri Phalan-M Missile Fighter";
+			$fighter = new Fighter("PhalanMNav", $armour, 12, $this->id);
+			$fighter->displayName = "Phalan-M Missile Fighter";
 			$fighter->imagePath = "img/ships/phalan.png";
 			$fighter->iconPath = "img/ships/phalan_large.png";
 			
