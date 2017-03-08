83e3536acdf73f8dcdba9ae78161dedaac70f854
Zero1627 <reisen@poczta.onet.pl>
2017-03-08 13:47:51 +0100

    Update zzftrAWing.php

--- source/server/model/ships/starwars_centauri/zzftrAWing.php
+++ source/server/model/ships/starwars_centauri/zzftrAWing.php
@@ -42,10 +42,10 @@ class zzftrawing extends FighterFlight{
             	$ConcussionMissileLauncher = new SWFtrConcMissileLauncher(3, 330, 30, 2);//single dual launcher! like for direct fire
             	$fighter->addFrontSystem($ConcussionMissileLauncher);
 
-             /*
+             
             	//Ray Shield, 1 points
             	$fighter->addAftSystem(new SWRayShield(0, 1, 0, 1, 0, 360));
-            */
+            
             
             
             $this->addSystem($fighter);
