522e0c313a30f863918688f21fcdc5968bdfaf35
Zero1627 <reisen@poczta.onet.pl>
2017-03-13 09:17:49 +0100

    Update customSW.php

--- source/server/model/weapons/customSW.php
+++ source/server/model/weapons/customSW.php
@@ -1232,22 +1232,27 @@ class SWTractorBeam extends SWDirectWeapon{
 	}    
 	
 	protected function onDamagedSystem($ship, $system, $damage, $armour, $gamedata, $fireOrder){ //target is held critical on PRIMARY Structure!
-	
-	      //$primaryStruct = $ship->getStructureSystem(0); //primary Structure is where the crit will reside - it has to be there! (weapon does not target fighters)
-	      //if($primaryStruct->isDestroyed()) return; //destroyed system - critical is irrelevant
+	      $primaryStruct = $ship->getStructureSystem(0); //primary Structure is where the crit will reside - it has to be there! (weapon does not target fighters)
+	      if($primaryStruct->isDestroyed()) return; //destroyed system - critical is irrelevant
+		$crit = new swtargetheld(-1, $ship->id, $primaryStruct->id, $gamedata->turn); 
+		$crit->updated = true;
+                $crit->inEffect = false;
+	      $primaryStruct->criticals[] =  $crit;
+		//to C&C too, to show to the player - but effective one will be on Structure!
+		$CnC = $ship->getSystemByName("C&C");
+		$crit = new swtargetheld(-1, $ship->id, $CnC->id, $gamedata->turn); 
+		$crit->updated = true;
+                $crit->inEffect = false;
+	      $CnC->criticals[] =  $crit;
+		
+		
+		
+		/*
 		$crit = new swtargetheld(-1, $ship->id, $system->id, $gamedata->turn); 
 		$crit->updated = true;
                 $crit->inEffect = false;
 	      $system->criticals[] =  $crit;
-		
-		//just a test whether this works at all...
-		$crit = new ArmorReduced(-1, $ship->id, $system->id, "ArmorReduced", $gamedata->turn);
-		$crit->updated = true;
-                $crit->inEffect = false;
-     	 	$system->criticals[] =  $crit;
-	      
-	      
-	      
+*/
 	}
 	
 	public function getDamage($fireOrder){ return  0;   }
