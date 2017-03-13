fbe8e0534e1ae8c57e4050a65d65f40397cf3814
Zero1627 <reisen@poczta.onet.pl>
2017-03-13 09:35:13 +0100

    Update cricialClasses.php

--- source/server/model/cricialClasses.php
+++ source/server/model/cricialClasses.php
@@ -303,7 +303,7 @@ class NastierCrit extends Critical{ /*next critical (or dropout!) roll will be n
 
 class swtargetheld extends Critical{ /*next turn target is being held by tractor beam!*/
     public $description = "Held by tractor beam! Initiative and remaining thrust reduced.";
-    //public $oneturn = true;	
+    public $oneturn = true;	
     function __construct($id, $shipid, $systemid, $turn){
             parent::__construct($id, $shipid, $systemid, 'swtargetheld', $turn);
     }
