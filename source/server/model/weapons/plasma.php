2345cc9a9eb59186bca690d1f0fff472dd342257
Zero1627 <reisen@poczta.onet.pl>
2017-03-15 10:00:28 +0100

    Update plasma.php

--- source/server/model/weapons/plasma.php
+++ source/server/model/weapons/plasma.php
@@ -342,8 +342,8 @@ class PairedPlasmaBlaster extends LinkedWeapon{
         }
 
 
-        protected function getSystemArmourStandard($system, $gamedata, $fireOrder, $pos=null){
-            $armor = parent::getSystemArmourStandard($system, $gamedata, $fireOrder, $pos);
+        protected function getSystemArmourStandard($target, $system, $gamedata, $fireOrder, $pos=null){
+            $armor = parent::getSystemArmourStandard($target, $system, $gamedata, $fireOrder, $pos);
 
             if (is_numeric($armor)){
                 $toIgnore = ceil($armor /2);
