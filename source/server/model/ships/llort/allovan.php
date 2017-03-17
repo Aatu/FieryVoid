0ed1f0ec091bc904e65695b7ae2d1bd091c10b1d
Zero1627 <reisen@poczta.onet.pl>
2017-03-17 10:51:34 +0100

    Update allovan.php

--- source/server/model/ships/llort/allovan.php
+++ source/server/model/ships/llort/allovan.php
@@ -29,7 +29,7 @@ class Allovan extends HeavyCombatVessel{
         $this->addPrimarySystem(new Thruster(3, 11, 0, 4, 3));
         $this->addPrimarySystem(new Thruster(3, 11, 0, 4, 4));
 
-        $this->addFrontSystem(new Thruster(2, 5, 0, 2, 3));
+        $this->addFrontSystem(new Thruster(3, 8, 0, 2, 1));
         $this->addFrontSystem(new Thruster(3, 8, 0, 2, 1));
         $this->addFrontSystem(new MediumBolter(4, 8, 4, 300, 60));
         $this->addFrontSystem(new LightBolter(4, 6, 2, 120, 360));
