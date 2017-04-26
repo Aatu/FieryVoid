0fe0fbc189428e60672459a9beacb625ac3b2f44
Zero1627 <reisen@poczta.onet.pl>
2017-04-26 15:58:20 +0200

    Update drakhRaiderFlt.php

--- source/server/model/ships/drakh/drakhRaiderFlt.php
+++ source/server/model/ships/drakh/drakhRaiderFlt.php
@@ -47,7 +47,7 @@ class DrakhRaiderFlt extends FighterFlight{
 		$CombPhaseDisruptor->displayName = "Combined Phase Disruptor";
  		$CombPhaseDisruptor->exclusive = true;
 		//and switch icon too...
-		$CombPhaseDisruptor->iconPath = "LightMolecularDisruptor.png";
+		$CombPhaseDisruptor->iconPath = "molecularDisruptor.png.png";
 		$CombPhaseDisruptor->weaponClass = 'Molecular';
         	$fighter->addFrontSystem($CombPhaseDisruptor);
             
