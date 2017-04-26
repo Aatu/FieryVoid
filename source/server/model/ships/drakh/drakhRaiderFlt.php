2ec8327cba4ea7d98d8ed7f1a63f0508ef0a8242
Zero1627 <reisen@poczta.onet.pl>
2017-04-26 14:01:18 +0200

    Create drakhRaiderFlt.php

---
+++ source/server/model/ships/drakh/drakhRaiderFlt.php
@@ -0,0 +1,62 @@
+<?php
+class DrakhRaiderFlt extends FighterFlight{
+
+    function __construct($id, $userid, $name,  $slot){
+        parent::__construct($id, $userid, $name,  $slot);
+        
+        $this->pointCost = 150*6;
+        $this->faction = "Drakh";
+        $this->phpclass = "DrakhRaiderFlt";
+        $this->shipClass = "Raider Assault Fighters";
+        $this->imagePath = "img/ships/DrakhRaider.png";
+        
+        $this->isd = 2210;
+        $this->unofficial = true;
+        $this->gravitic = true;
+        
+        $this->forwardDefense = 9;
+        $this->sideDefense = 10;
+        $this->freethrust = 10;
+        $this->offensivebonus = 7;
+        $this->jinkinglimit = 4; //deliberately low jinking limit and Init
+        $this->turncost = 0.33;
+        
+    	$this->iniativebonus = 14 *5; 
+        
+        $this->populate();
+    }
+    
+    
+    public function populate(){
+        $current = count($this->systems);
+        $new = $this->flightSize;
+        $toAdd = $new - $current;
+        for ($i = 0; $i < $toAdd; $i++){
+            $armour = array(4, 3, 4, 4);
+            $fighter = new Fighter("DrakhRaiders", $armour, 12, $this->id);
+	          $fighter->advancedArmor = true; 
+            $fighter->displayName = "Raider Assault Fighters";
+            $fighter->imagePath = "img/starwars/DrakhRaider.png";
+            $fighter->iconPath = "img/starwars/DrakhRaider_large.png"; 
+
+
+            
+        $fighter->addFrontSystem(new LightPhaseDisruptor(330, 30, 5));
+
+	$CombPhaseDisruptor = new LightGravitonBeam(330, 30, 0);
+	$CombPhaseDisruptor->displayName = "Combined Phase Disruptor";
+ 	$CombPhaseDisruptor->exclusive = true;
+        $fighter->addFrontSystem($CombPhaseDisruptor);
+            
+        //Ray Shield, 1 points
+        $fighter->addAftSystem(new SWRayShield(0, 1, 0, 1, 0, 360));
+            
+            
+            
+            $this->addSystem($fighter);
+       }
+    }
+    
+    
+}
+?>
