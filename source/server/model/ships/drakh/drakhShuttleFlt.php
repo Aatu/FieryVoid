1b79cec06283476e12aa1f01fc6277eeeb63e3c8
Zero1627 <reisen@poczta.onet.pl>
2017-04-26 15:42:47 +0200

    Create drakhShuttleFlt.php

---
+++ source/server/model/ships/drakh/drakhShuttleFlt.php
@@ -0,0 +1,59 @@
+<?php
+class DrakhShuttle extends FighterFlight{
+    
+    function __construct($id, $userid, $name,  $slot){
+        parent::__construct($id, $userid, $name,  $slot);
+        
+		    $this->pointCost = 90*6;
+		    $this->faction = "Drakh";
+        $this->phpclass = "DrakhShuttle";
+        $this->shipClass = "Armed Shuttle";
+		    $this->imagePath = "img/ships/DrakhShuttle.png";
+        
+        $this->forwardDefense = 9;
+        $this->sideDefense = 8;
+        $this->freethrust = 10;
+        $this->offensivebonus = 5;
+        $this->jinkinglimit = 2;
+        $this->pivotcost = 2; //shuttles have pivot cost higher
+        $this->turncost = 0.33;
+        $this->turndelaycost = 0.33;
+        $this->advancedArmor = true;   
+        
+		    $this->iniativebonus = 10*5;
+      
+        $this->populate();
+    }
+    
+    
+    public function populate(){
+        $current = count($this->systems);
+        $new = $this->flightSize;
+        $toAdd = $new - $current;
+        for ($i = 0; $i < $toAdd; $i++){
+			
+        $armour = array(2, 1, 2, 2);
+        $fighter = new Fighter("DrakhShuttle", $armour, 18, $this->id);
+        $fighter->displayName = "Armed Shuttle";
+        $fighter->imagePath = "img/ships/DrakhShuttle.png";
+        $fighter->iconPath = "img/ships/DrakhShuttle_Large.png";
+
+		
+			
+		$fighter->addFrontSystem(new LightFusionCannon(330, 30, 3, 3));
+
+		$DisruptionGun = new LightFusionCannon(330, 30, 7, 1);
+		$DisruptionGun->displayName = "Phased Disruption Gun";
+        	$fighter->addFrontSystem($DisruptionGun);
+            
+        	//Ray Shield, 1 points
+        	$fighter->addAftSystem(new SWRayShield(0, 1, 0, 1, 0, 360));
+			
+		$this->addSystem($fighter);
+			
+	}
+		
+		
+    }
+}
+?>
