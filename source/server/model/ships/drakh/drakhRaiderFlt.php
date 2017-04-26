fa13a81f62b83f98363233210172f9041220e482
Zero1627 <reisen@poczta.onet.pl>
2017-04-26 14:11:50 +0200

    Update drakhRaiderFlt.php

--- source/server/model/ships/drakh/drakhRaiderFlt.php
+++ source/server/model/ships/drakh/drakhRaiderFlt.php
@@ -34,7 +34,7 @@ class DrakhRaiderFlt extends FighterFlight{
         $toAdd = $new - $current;
         for ($i = 0; $i < $toAdd; $i++){
             $armour = array(4, 3, 4, 4);
-            $fighter = new Fighter("DrakhRaiders", $armour, 12, $this->id);
+            $fighter = new Fighter("DrakhRaiderFlt", $armour, 12, $this->id);
             $fighter->displayName = "Raider Assault Fighters";
             $fighter->imagePath = "img/starwars/DrakhRaider.png";
             $fighter->iconPath = "img/starwars/DrakhRaider_large.png"; 
