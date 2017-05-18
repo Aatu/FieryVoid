f7c6103db4a64932a218acb1aa1712419a11b4e4
Zero1627 <reisen@poczta.onet.pl>
2017-05-18 13:07:56 +0200

    Update ShipSystem.php

--- source/server/model/systems/ShipSystem.php
+++ source/server/model/systems/ShipSystem.php
@@ -75,20 +75,20 @@ class ShipSystem{
         $this->setSystemDataWindow($turn);
     }
     
-    public function setDamage($damage){ //$damage object and $condense = whether damage should be condensed for transfer
+    public function setDamage($damage){ //$damage object 
 	//let's try to reduce amount of data transferred - don't note every hit, just total damage and Destroyed status... at least for past turns!
 	$count = count($this->damage); 
 	$currTurn = TacGamedata::$currentTurn -1; //let's say current AND PREVIOUS turn damage will be fully transferred...
 	if (($count == 0) || ($damage->turn >= $currTurn) ) { //no entries yet, add!
 		$this->damage[] = $damage;
 	}else{ //modify existing entry... unless damage is from current turn!
-		foreach( $this->damage as $key=>$oldDmg ){
+		//foreach( $this->damage as $key=>$oldDmg ){
 			/*if($oldDmg->turn < $currTurn){
 				$oldDmg->damage += $damage->damage; //shields and armour absorbtion irrelevant
 				if ($damage->destroyed == true) $oldDmg->destroyed = true;
 				return; //nothing else to do
 			}*/	
-		}
+		//}
 		$this->damage[] = $damage; //no eligible entry found!
 	}
 	
