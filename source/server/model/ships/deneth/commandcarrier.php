6c09a2087d292aaaa96dcd0151e1cfed75279b4c
Zero1627 <reisen@poczta.onet.pl>
2017-07-08 11:01:56 +0200

    Update commandcarrier.php

--- source/server/model/ships/deneth/commandcarrier.php
+++ source/server/model/ships/deneth/commandcarrier.php
@@ -4,72 +4,77 @@ class DCommandCarrier extends BaseShip{
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-	$this->pointCost = 550;
+        $this->pointCost = 480;
         $this->faction = "Deneth";
         $this->phpclass = "dcommandcarrier";
-        $this->imagePath = "img/ships/sakar.png"; 
-        $this->shipClass = "Command Carrier";
+        $this->imagePath = "img/ships/dragonship.png";
+        $this->shipClass = "Command Carrier Proxy";
         $this->shipSizeClass = 3;
-	    
-        $this->limited = 33;
-	$this->unofficial = true;
-	$this->isd = 2230;
-	    
-        $this->fighters = array("normal"=>48);
-        $this->forwardDefense = 17;
-        $this->sideDefense = 17;
-
-	
+	$this->fighters = array("LCVs" => 4);        
+        $this->forwardDefense = 15;
+        $this->sideDefense = 16;
         
-        $this->turncost = 1.33;
-        $this->turndelaycost = 1.33;
-        $this->accelcost = 4;
-        $this->rollcost = 4;
+        //$this->limited = 10;
+	//$this->occurence = "rare";
+	$this->isd = 2226;
+	//$this->unofficial = true;        
+        
+        $this->turncost = 1;
+        $this->turndelaycost = 1;
+        $this->accelcost = 3;
+        $this->rollcost = 2;
         $this->pivotcost = 3;
-                 
-        $this->addPrimarySystem(new Reactor(5, 18, 0, 0));
-        $this->addPrimarySystem(new CnC(5, 25, 0, 0));
-        $this->addPrimarySystem(new Scanner(5, 16, 4, 7));
-        $this->addPrimarySystem(new Engine(5, 20, 0, 12, 3));
-		$this->addPrimarySystem(new Hangar(5, 4));
-        
-        $this->addFrontSystem(new Thruster(4, 10, 0, 4, 1));
-        $this->addFrontSystem(new Thruster(4, 10, 0, 4, 1));
-        $this->addFrontSystem(new StdParticleBeam(2, 4, 1, 240, 60));
-        $this->addFrontSystem(new StdParticleBeam(2, 4, 1, 240, 60));
-        $this->addFrontSystem(new StdParticleBeam(2, 4, 1, 300, 120));
-        $this->addFrontSystem(new StdParticleBeam(2, 4, 1, 300, 120));
-        $this->addFrontSystem(new Hangar(4, 12));
-        $this->addFrontSystem(new Hangar(4, 12));
-        
-        $this->addAftSystem(new Thruster(2, 8, 0, 2, 2));
-        $this->addAftSystem(new Thruster(3, 8, 0, 2, 2));
-        $this->addAftSystem(new Thruster(3, 8, 0, 2, 2));
-        $this->addAftSystem(new Thruster(3, 8, 0, 2, 2));
-        $this->addAftSystem(new Thruster(2, 8, 0, 2, 2));
-        $this->addAftSystem(new JumpEngine(5, 24, 3, 20));      
-        $this->addAftSystem(new StdParticleBeam(2, 4, 1, 90, 270));
-        $this->addAftSystem(new StdParticleBeam(2, 4, 1, 120, 240));
-        $this->addAftSystem(new StdParticleBeam(2, 4, 1, 90, 270));
-        
-		$this->addLeftSystem(new Thruster(4, 15, 0, 5, 3));
-        $this->addLeftSystem(new StdParticleBeam(2, 4, 1, 180, 360));
-        $this->addLeftSystem(new Hangar(4, 12));
-        
-		$this->addRightSystem(new Thruster(4, 15, 0, 5, 4));
-        $this->addRightSystem(new StdParticleBeam(2, 4, 1, 0, 180));
-        $this->addRightSystem(new Hangar(4, 12));
+         
+        $this->addPrimarySystem(new Reactor(5, 27, 0, 0));
+        $this->addPrimarySystem(new CnC(5, 12, 0, 0));
+        $this->addPrimarySystem(new Scanner(5, 17, 5, 7));
+        $this->addPrimarySystem(new Engine(5, 20, 0, 12, 2));
+	$this->addPrimarySystem(new CargoBay(5, 12));
+        $this->addPrimarySystem(new Hangar(5, 2));
+        $this->addPrimarySystem(new JumpEngine(5, 15, 4, 24));
+  
+        $this->addFrontSystem(new Thruster(4, 15, 0, 4, 1));
+	$this->addFrontSystem(new Thruster(4, 15, 0, 4, 1));
+        $this->addFrontSystem(new AssaultLaser(4, 6, 4, 300, 60));
+	$this->addFrontSystem(new AssaultLaser(4, 6, 4, 300, 60));
+        $this->addFrontSystem(new TwinArray(2, 6, 2, 240, 60));
+        $this->addFrontSystem(new TwinArray(2, 6, 2, 300, 120));
+        $LCVRail = new Catapult(3, 8);
+        $LCVRail->displayName = "LCV Rail";
+        $this->addFrontSystem($LCVRail);
+        $LCVRail = new Catapult(3, 8);
+        $LCVRail->displayName = "LCV Rail";
+	$this->addFrontSystem($LCVRail);
+        $this->addAftSystem(new TwinArray(2, 6, 2, 120, 300));
+        $this->addAftSystem(new TwinArray(2, 6, 2, 60, 240));
+        $this->addAftSystem(new Thruster(4, 16, 0, 4, 2));
+        $this->addAftSystem(new Thruster(4, 16, 0, 4, 2));
                 
+        $this->addLeftSystem(new Thruster(4, 15, 0, 6, 3));
+        $LCVRail = new Catapult(3, 8);
+        $LCVRail->displayName = "LCV Rail";        
+        $this->addLeftSystem($LCVRail);
+        $this->addLeftSystem(new TwinArray(2, 6, 2, 180, 360));
+  
+        $this->addRightSystem(new Thruster(4, 15, 0, 6, 4));
+        $LCVRail = new Catapult(3, 8);
+        $LCVRail->displayName = "LCV Rail";        
+        $this->addRightSystem($LCVRail);
+        $this->addRightSystem(new TwinArray(2, 6, 2, 0, 180));
+        
+        
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
-        $this->addFrontSystem(new Structure( 5, 56));
-        $this->addAftSystem(new Structure( 4, 48));
-        $this->addLeftSystem(new Structure( 4, 48));
-        $this->addRightSystem(new Structure( 4, 48));
-        $this->addPrimarySystem(new Structure( 5, 48));
-
- $this->hitChart = array(
+        $this->addFrontSystem(new Structure( 5, 52));
+        $this->addAftSystem(new Structure( 4, 52));
+        $this->addLeftSystem(new Structure( 5, 52));
+        $this->addRightSystem(new Structure( 5, 52));
+        $this->addPrimarySystem(new Structure( 5, 55));
+        
+        $this->hitChart = array(
         		0=> array(
-        				11 => "Structure",
+        				8 => "Structure",
+					9 => "Cargo",
+        				11 => "Jump Engine",
         				14 => "Scanner",
         				16 => "Engine",
         				17 => "Hangar",
@@ -77,35 +82,33 @@ class DCommandCarrier extends BaseShip{
         				20 => "C&C",
         		),
         		1=> array(
-        				5 => "Thruster",
-        				8 => "Standard Particle Beam",
-        				12 => "Hangar",
+        				4 => "Thruster",
+        				6 => "LCV Rail",
+        				10 => "Assault Laser",
+        				12 => "Twin Array",
         				18 => "Structure",
         				20 => "Primary",
         		),
         		2=> array(
-        				6 => "Thruster",
-        				9 => "Jump Engine",
-        				11 => "Standard Particle Beam",
+        				8 => "Thruster",
+        				10 => "Twin Array",
         				18 => "Structure",
         				20 => "Primary",
         		),
         		3=> array(
-        				6 => "Thruster",
-        				8 => "Standard Particle Beam",
-					12 => "Hangar",
+        				4 => "Thruster",
+        				5 => "Twin Array",
+					7 => "LCV Rail",
         				18 => "Structure",
         				20 => "Primary",
         		),
         		4=> array(
-        				6 => "Thruster",
-        				8 => "Standard Particle Beam",
-					12 => "Hangar",
+        				4 => "Thruster",
+        				5 => "Twin Array",
+					7 => "LCV Rail",
         				18 => "Structure",
         				20 => "Primary",
         		),
         );
     }
 }
-
-?>
