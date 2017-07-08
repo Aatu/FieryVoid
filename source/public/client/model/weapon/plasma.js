1eaf03a5948d163313e4311138024017d60c3a3d
Zero1627 <reisen@poczta.onet.pl>
2017-07-08 11:53:29 +0200

    Update plasma.js

--- source/public/client/model/weapon/plasma.js
+++ source/public/client/model/weapon/plasma.js
@@ -72,3 +72,10 @@ var PlasmaGun = function(json, ship)
 PlasmaGun.prototype = Object.create( Plasma.prototype );
 PlasmaGun.prototype.constructor = PlasmaGun;
 
+var RogolonLtPlasmaGun = function(json, ship)
+{
+    Plasma.call( this, json, ship);
+}
+RogolonLtPlasmaGun.prototype = Object.create( Plasma.prototype );
+RogolonLtPlasmaGun.prototype.constructor = RogolonLtPlasmaGun;
+
