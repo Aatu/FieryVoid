3ba272512be6027ede7db73b559b1ab56bdd3652
Zero1627 <reisen@poczta.onet.pl>
2017-07-25 13:49:51 +0200

    Update baLightGunboatEscort.php

--- source/server/model/ships/smallraces/baLightGunboatEscort.php
+++ source/server/model/ships/smallraces/baLightGunboatEscort.php
@@ -1,5 +1,5 @@
 <?php
-class LightGunboatEscort extends MediumShip{
+class BALightGunboatEscort extends MediumShip{
     
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
