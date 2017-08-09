5900d7d15d50c67a842c5856f66cd5afe4d702e9
Zero1627 <reisen@poczta.onet.pl>
2017-08-09 15:54:55 +0200

    Update autoload.php

--- source/autoload.php
+++ source/autoload.php
@@ -79,7 +79,7 @@ spl_autoload_register(
                 
                 // ***custom Drakh***
                 //weapons and systems
-                'drakhraidercontroller' => '/server/model/systems/baseSystems.php',
+                //'drakhraidercontroller' => '/server/model/systems/baseSystems.php',
                 'absorbtionshield' => '/server/model/weapons/customs.php',
                 'customphasedisruptor' => '/server/model/weapons/customs.php',
                 'customltphasedisruptorship' => '/server/model/weapons/customs.php', //ship weapon
