d85ff2cf7c2eb4021ba0802aeb121a8d2210df61
Zero1627 <reisen@poczta.onet.pl>
2017-08-11 15:01:02 +0200

    reactor overload

--- source/server/handlers/firing.php
+++ source/server/handlers/firing.php
@@ -440,14 +440,26 @@ class Firing{
 		foreach($reactorList as $reactorCurr){
 			//is it overloading?...
 			if( $reactorCurr->isOverloading($gamedata->turn) ){ //primed for self destruct!
+				$remaining =  $reactorCurr->getRemainingHealth();
+				$armour =  $reactorCurr->armour;
+				$toDo = $remaining + $armour;
+
+				$damageEntry = new DamageEntry(-1, $ship->id, -1, $gamedata->turn, $reactorCurr->id, $toDo, $armour, 0, -1, true, "", "plasma");
+				$damageEntry->updated = true;
+
+				 $reactorCurr->damage[] = $damageEntry;
+				/*
 				$fireOrder =  new FireOrder(-1, "Reactor overload", $ship->id,  $ship->id, $this->id, -1, 
 					$gamedata->turn, 'flash', 100, 1, 1, 1, 0, null, null, 'plasma');
 				$fireOrder->updated = true;
 				$fireOrder->addToDB = true;
+				*/
+				/*
         			$damageEntry = new DamageEntry(-1, $ship->id, $fireOrder->id, $gamedata->turn, $reactorCurr->id, 1, 0, 0, -1, true, 
 					"Reactor overload", "plasma");
 				$damageEntry->updated = true;
 				$reactorCurr->damage[] = $damageEntry;
+				*/
 			}
 		}
 
