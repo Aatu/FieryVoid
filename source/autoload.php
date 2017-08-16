20792f92133b9ea56bc669b6e7e2b90d03f9953d
Zero1627 <reisen@poczta.onet.pl>
2017-08-16 09:20:56 +0200

    Update autoload.php

--- source/autoload.php
+++ source/autoload.php
@@ -569,7 +569,7 @@ spl_autoload_register(
                 'tethyspolice' => '/server/model/ships/EA/tethysPolice.php',
                 //'thunderboltstarfury' => '/server/model/ships/EA/thunderboltStarfury.php',
                 //'thunderboltstarfurynav' => '/server/model/ships/EA/thunderboltStarfuryNav.php',
-                'tigerstarfury' => '/server/model/ships/EA/tigerStarfury.php',           
+                //'tigerstarfury' => '/server/model/ships/EA/tigerStarfury.php',           
                 //'warlock' => '/server/model/ships/EA/warlock.php',//original version
                 //'warlockmlpa' => '/server/model/ships/EA/warlockMlpa.php',//version with new MLPAs
             
