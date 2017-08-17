8fbc1f8d59b1e643cb47c24200bc5db8e660ae63
Zero1627 <reisen@poczta.onet.pl>
2017-08-17 12:05:52 +0200

    Update godBeta.php

--- source/server/model/ships/EA/godBeta.php
+++ source/server/model/ships/EA/godBeta.php
@@ -5,7 +5,7 @@ class GODBeta extends OSAT{
         parent::__construct($id, $userid, $name,  $slot);
         
 		$this->pointCost = 700;
-		$this->faction = "EA defenses";
+		$this->faction = 'EA';//"EA defenses";
         $this->phpclass = "GODBeta";
         $this->imagePath = "img/ships/god.png";
         $this->shipClass = "GOD Heavy Satellite (Beta)";
