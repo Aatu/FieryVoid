451247afcbe06fadacdb29937e8ef98333b2fb3b
Zero1627 <reisen@poczta.onet.pl>
2017-09-18 10:09:09 +0200

    news!

--- source/public/games.php
+++ source/public/games.php
@@ -44,13 +44,17 @@
             $(document).ready(function(){
 
                 var header = document.getElementById("newsHeader");
-                    header.innerHTML = "Latest News 23rd of August 2017";
+                    header.innerHTML = "Latest News 18th of September 2017";
                    
 
                 var news = document.getElementById("newsEntry");
                     news.innerHTML += "The latest update includes:";
+		    /*
                     news.innerHTML += "<br>";
                     news.innerHTML += "- code optimization - now You can play larger games than ever before! (not unlimited, but 'dangerous' sized games should now be 'all right'). Go and try!";
+		    */
+		    news.innerHTML += "<br>";
+                    news.innerHTML += "- minor fixes";
                     news.innerHTML += "<br>";
                     news.innerHTML += "<br>- Did You ever have a ship that's still there, but You want to get rid of it? Now You can - just set reactor to overload!<br>";
                     news.innerHTML += "<br>";
