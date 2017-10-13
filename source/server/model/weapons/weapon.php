dc4b11a188c810a58987de740e9636c7056ce120
Zero1627 <reisen@poczta.onet.pl>
2017-10-13 13:34:58 +0200

    Update weapon.php

--- source/server/model/weapons/weapon.php
+++ source/server/model/weapons/weapon.php
@@ -75,7 +75,7 @@ class Weapon extends ShipSystem{
     public $dualWeapon = false;
     public $canChangeShots = false;
     public $isPrimaryTargetable = true; //can this system be targeted by called shot if it's on PRIMARY?
-	
+	public $useAmmo = false; //does this weapon track ammo usage?
 
     public $shots = 1;
 	public  $shotsArray = array();
