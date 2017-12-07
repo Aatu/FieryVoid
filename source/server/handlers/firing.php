e1f862c8dac33f844bd49bb04b5167439623ba76
Zero1627 <reisen@poczta.onet.pl>
2017-12-08 00:38:42 +0100

    Update firing.php

--- source/server/handlers/firing.php
+++ source/server/handlers/firing.php
@@ -216,7 +216,6 @@ $a = 0	 ; $b = 0;
 			$shotToIntercept = self::getBestInterception($gamedata, $currInterceptor, $allIncomingShots);
 			if ($shotToIntercept != null){
 $a++;
-throw new Exception("firing automateIntercept before addToInterceptionTotal");
 				self::addToInterceptionTotal($gamedata, $shotToIntercept, $currInterceptor, true); //add numbers AND create order
 throw new Exception("firing automateIntercept after addToInterceptionTotal");				
 			}
