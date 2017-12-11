8cc6cc9922ca397e157cc638abc3a7a481bbe3ee
Zero1627 <reisen@poczta.onet.pl>
2017-12-11 11:42:18 +0100

    Update Effects.js

--- source/public/client/Effects.js
+++ source/public/client/Effects.js
@@ -282,7 +282,11 @@ window.effects = {
             }/*else if (obj1.firingMode !== obj2.firingMode){
                 return obj1.firingMode-obj2.firingMode; 
             }*/
-            else return obj1.id-obj2.id;//database ID - hopefully that'll keep correct display order!          
+            else {
+                var $val = obj1.shooterid - obj2.shooterid;
+                if ($val == 0) $val = obj1.id - obj2.id;
+                return $val
+            } 
             //else return obj1.shooterid - obj2.shooterid;
         });
 
