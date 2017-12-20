3cd54cae641e28e60867f1d2fbb86edcdd103869
Zero1627 <reisen@poczta.onet.pl>
2017-12-20 11:44:57 +0100

    Update technicalTargetDrone.php

--- source/server/model/ships/customs/technicalTargetDrone.php
+++ source/server/model/ships/customs/technicalTargetDrone.php
@@ -20,13 +20,15 @@ class technicalTargetDrone extends BaseShip{
 		$this->pivotcost = 4;
 
 		
-		$this->addPrimarySystem(new Reactor(6, 35, 0, 0));
+		$this->addPrimarySystem(new Reactor(6, 35, 20, 0));
 		$this->addFrontSystem(new CnC(1, 1, 0, 0));
 		$this->addFrontSystem(new Scanner(6, 23, 4, 6));
 		$this->addPrimarySystem(new Engine(5, 20, 0, 20, 3));
 		$this->addPrimarySystem(new Hangar(4, 2));
 				$this->addPrimarySystem(new ImperialLaser(2, 12, 5, 0, 360));
 				$this->addPrimarySystem(new ImperialLaser(2, 12, 5, 0, 360));
+		$this->addPrimarySystem(new ParticleImpeder(2, 0, 0, 0, 180));
+		$this->addPrimarySystem(new ParticleImpeder(2, 0, 0, 180, 360));
 		
 		$this->addFrontSystem(new Thruster(4, 10, 0, 4, 1));
 		$this->addFrontSystem(new Thruster(4, 10, 0, 4, 1));
@@ -34,11 +36,11 @@ class technicalTargetDrone extends BaseShip{
 		
 		//new weapon showcase
 		
-		$this->addFrontSystem(new PlasmaWaveTorpedo(3, 0, 0, 300, 60));
-		$this->addFrontSystem(new PlasmaWaveTorpedo(3, 0, 0, 300, 60));
-		$this->addFrontSystem(new StunBeam(2, 0, 0, 0, 360));
-		$this->addFrontSystem(new StunBeam(2, 0, 0, 0, 360));
-		$this->addFrontSystem(new ScatterGun(1, 0, 0, 270, 90));
+		//$this->addFrontSystem(new PlasmaWaveTorpedo(3, 0, 0, 300, 60));
+		//$this->addFrontSystem(new PlasmaWaveTorpedo(3, 0, 0, 300, 60));
+		//$this->addFrontSystem(new StunBeam(2, 0, 0, 0, 360));
+		//$this->addFrontSystem(new StunBeam(2, 0, 0, 0, 360));
+		//$this->addFrontSystem(new ScatterGun(1, 0, 0, 270, 90));
 		
 		/*
 		$this->addFrontSystem(new AssaultLaser(3, 6, 4, 300, 60));
