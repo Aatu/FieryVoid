afc5872b97b3d6c429d66c64712003c63db262b5
Zero1627 <reisen@poczta.onet.pl>
2017-12-31 13:30:20 +0100

    Abbai weapons

--- source/public/client/model/weapon/special.js
+++ source/public/client/model/weapon/special.js
@@ -4,3 +4,11 @@ var TractorBeam = function(json, ship)
 }
 TractorBeam.prototype = Object.create( Weapon.prototype );
 TractorBeam.prototype.constructor = TractorBeam;
+
+
+var CommDisruptor = function(json, ship)
+{
+    Weapon.call( this, json, ship);
+}
+CommDisruptor.prototype = Object.create( Weapon.prototype );
+CommDisruptor.prototype.constructor = CommDisruptor;
