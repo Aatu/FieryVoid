72811a4be4d9bc06c3b5fdc5066c4b66a2ad26be
Zero1627 <reisen@poczta.onet.pl>
2017-12-31 14:02:46 +0100

    Update specialWeapons.php

--- source/server/model/weapons/specialWeapons.php
+++ source/server/model/weapons/specialWeapons.php
@@ -571,8 +571,8 @@ class CommDisruptor extends Weapon{
 	);
 	
     public function setSystemDataWindow($turn){
-      parent::setSystemDataWindow($turn);
-      $this->data["Special"] = "Does no damage, but weakens target's Initiative and Sensors rating next turn";      
+	      $this->data["Special"] = "Does no damage, but weakens target's Initiative and Sensors rating next turn";  
+	      parent::setSystemDataWindow($turn);    
     }	
     
 	function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
@@ -585,7 +585,7 @@ class CommDisruptor extends Weapon{
             }
             parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
         }
-	
+/*
 	protected function onDamagedSystem($ship, $system, $damage, $armour, $gamedata, $fireOrder){ //really no matter what exactly was hit!
 		if ($system->advancedArmor) return; //no effect on Advanced Armor
 		
@@ -623,8 +623,8 @@ class CommDisruptor extends Weapon{
 				}
 			}
 		}
-	}
-	
+	} //endof function onDamagedSystem
+*/
 	public function getDamage($fireOrder){ return  0;   }
 	public function setMinDamage(){   $this->minDamage =  0 ;      }
 	public function setMaxDamage(){   $this->maxDamage =  0 ;      }
