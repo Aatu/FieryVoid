154f7393eaf0b9c98cc6d21a355b2f9df592a591
Zero1627 <reisen@poczta.onet.pl>
2018-01-04 12:45:08 +0100

    Update shipWarsphere.php

--- source/server/model/ships/ipsha/shipWarsphere.php
+++ source/server/model/ships/ipsha/shipWarsphere.php
@@ -22,8 +22,8 @@ class ShipWarsphere extends BaseShip{
 	    $this->isd = 2230;
 	    $this->notes = 'EM hardened';	  
 	    $this->notes .= '<br>-1 critical roll bonus';
-	    public $EMHardened = true; //EM Hardening - some weapons would check for this value!
-	    public $critRollMod = -1; //generalbonus to critical rolls!
+	    $this->EMHardened = true; //EM Hardening - some weapons would check for this value!
+	    $this->critRollMod = -1; //generalbonus to critical rolls!
 		
         $this->forwardDefense = 17;
         $this->sideDefense = 17;
