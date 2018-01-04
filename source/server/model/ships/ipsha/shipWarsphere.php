05a2381b29504f90ead0215834ca80a29c026821
Zero1627 <reisen@poczta.onet.pl>
2018-01-04 12:37:51 +0100

    Ipsha Warsphere

--- source/server/model/ships/ipsha/shipWarsphere.php
+++ source/server/model/ships/ipsha/shipWarsphere.php
@@ -3,102 +3,102 @@ class ShipWarsphere extends BaseShip{
     /*Ipsha general:
      - remember about EM hardening!
      - instead of -2 bonus to dropout/crit when caused by Ion weapon, just add -1 overall crit/dropout bonus
+     - remind player of the above in comments in fleet selection phase!
+     - SIngularity Drive replaced by standard engine
     */
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-		$this->pointCost = 625;
-		$this->faction = "Narn";
-        $this->phpclass = "Gquan";
-        $this->imagePath = "img/ships/gquan.png";
-        $this->shipClass = "G'Quan Heavy Cruiser";
+	$this->pointCost = 1000;
+	$this->faction = "Ipsha";
+        $this->phpclass = "ShipWarsphere";
+        $this->imagePath = "img/ships/IpshaWarsphere.png";
+        $this->shipClass = "Warsphere";    
+	    	    
         $this->shipSizeClass = 3;
-        $this->fighters = array("normal"=>12);
-	    $this->isd = 2242;
+        $this->fighters = array("heavy"=>6);
+	        
+	    $this->limited = 33;
+	    $this->isd = 2230;
+	    $this->notes = 'EM hardened';	  
+	    $this->notes .= '<br>-1 critical roll bonus';
+	    public $EMHardened = true; //EM Hardening - some weapons would check for this value!
+	    public $critRollMod = -1; //generalbonus to critical rolls!
 		
-        $this->forwardDefense = 15;
+        $this->forwardDefense = 17;
         $this->sideDefense = 17;
         
-        $this->turncost = 0.66;
-        $this->turndelaycost = 0.66;
-        $this->accelcost = 3;
-        $this->rollcost = 3;
-        $this->pivotcost = 2;
+        $this->turncost = 1.33;
+        $this->turndelaycost = 1.33;
+        $this->accelcost = 2;
+        $this->rollcost = 0;
+        $this->pivotcost = 0;
+	$this->gravitic = true;
         
-        $this->addPrimarySystem(new Reactor(6, 22, 0, 0));
-        $this->addPrimarySystem(new CnC(6, 20, 0, 0));
-        $this->addPrimarySystem(new Scanner(6, 24, 5, 8));
-        $this->addPrimarySystem(new Engine(6, 20, 0, 12, 3));
-        $this->addPrimarySystem(new JumpEngine(6, 30, 3, 20));
-        $this->addPrimarySystem(new Hangar(6, 14));
         
-        $this->addFrontSystem(new HeavyLaser(4, 8, 6, 300, 60));        
-        $this->addFrontSystem(new Thruster(5, 10, 0, 4, 1));
-        $this->addFrontSystem(new Thruster(5, 10, 0, 4, 1));        
-        $this->addFrontSystem(new HeavyLaser(4, 8, 6, 300, 60));
-        $this->addFrontSystem(new EnergyMine(4, 5, 4, 300, 60));
-        $this->addFrontSystem(new EnergyMine(4, 5, 4, 300, 60));
+        $this->addPrimarySystem(new MagGravReactor(4, 28, 0, 60));
+        $this->addPrimarySystem(new CnC(5, 20, 0, 0));
+        $this->addPrimarySystem(new Scanner(4, 20, 6, 8));
+        $this->addPrimarySystem(new Engine(4, 33, 0, 8, 6));
+        $this->addPrimarySystem(new Hangar(4, 6));
+        
+      
+        $this->addFrontSystem(new GraviticThruster(4, 15, 0, 99, 1));
+        $this->addFrontSystem(new GraviticThruster(4, 15, 0, 99, 1));
+        $this->addFrontSystem(new EmBolter(4, 0, 0, 300, 60));
 		
-        $this->addAftSystem(new TwinArray(3, 6, 2, 90, 270));	
-        $this->addAftSystem(new LightPulse(2, 4, 2, 90, 270));
-        $this->addAftSystem(new LightPulse(2, 4, 2, 90, 270));
-        $this->addAftSystem(new TwinArray(3, 6, 2, 90, 270));
+	    
+        $this->addAftSystem(new GraviticThruster(4, 15, 0, 99, 2));
+        $this->addAftSystem(new GraviticThruster(4, 15, 0, 99, 2));
+        $this->addAftSystem(new EmBolter(4, 0, 0, 120, 240));
+	    
+	    
+	$this->addLeftSystem(new GraviticThruster(4, 20, 0, 99, 3));
+
 		
-        $this->addAftSystem(new Thruster(4, 12, 0, 4, 2));
-        $this->addAftSystem(new Thruster(4, 12, 0, 4, 2));
-        $this->addAftSystem(new Thruster(4, 12, 0, 4, 2));
-		
-        $this->addLeftSystem(new LightPulse(2, 4, 2, 270, 90));
-        $this->addLeftSystem(new TwinArray(3, 6, 2, 270, 90));
-        $this->addLeftSystem(new Thruster(4, 15, 0, 5, 3));
-		
-        $this->addRightSystem(new LightPulse(2, 4, 2, 270, 90));
-        $this->addRightSystem(new TwinArray(3, 6, 2, 270, 90));	
-        $this->addRightSystem(new Thruster(4, 15, 0, 5, 4));
+        $this->addRightSystem(new GraviticThruster(4, 20, 0, 99, 4));
 		
 		
-        $this->addFrontSystem(new Structure(5, 70));
-        $this->addAftSystem(new Structure(4, 50));
-        $this->addLeftSystem(new Structure(4, 70));
-        $this->addRightSystem(new Structure(4, 70));
-        $this->addPrimarySystem(new Structure(6, 50));
+        $this->addFrontSystem(new Structure(4, 60));
+        $this->addAftSystem(new Structure(4, 60));
+        $this->addLeftSystem(new Structure(4, 72));
+        $this->addRightSystem(new Structure(4, 72));
+        $this->addPrimarySystem(new Structure(5, 56));
 		
 		
 		$this->hitChart = array(
 			0=> array(
-				8 => "Structure",
-				11 => "Jump Engine",
-				13 => "Scanner",
-				15 => "Engine",
-				17 => "Hangar",
-				19 => "Reactor",
+				7 => "Structure",
+				10 => "Spark Field",
+				12 => "Hangar",
+				14 => "Scanner",
+				16 => "Engine",
+				18 => "Reactor",
 				20 => "C&C",
 			),
 			1=> array(
-				3 => "Thruster",
-				7 => "Heavy Laser",
-				11 => "Energy Mine",
+				4 => "Thruster",
+				5 => "EM Bolter",
+				12 => "Surge Cannon",
 				18 => "Structure",
 				20 => "Primary",
 			),
 			2=> array(
-				7 => "Thruster",
-				9 => "Twin Array",
-				11 => "Light Pulse Cannon",
+				4 => "Thruster",
+				5 => "EM Bolter",
+				12 => "Surge Cannon",
 				18 => "Structure",
 				20 => "Primary",
 			),
 			3=> array(
 				4 => "Thruster",
-				7 => "Light Pulse Cannon",
-				9 => "Twin Array",
+				11 => "Surge Cannon",
 				18 => "Structure",
 				20 => "Primary",
 			),
 			4=> array(
 				4 => "Thruster",
-				7 => "Light Pulse Cannon",
-				9 => "Twin Array",
+				11 => "Surge Cannon",
 				18 => "Structure",
 				20 => "Primary",
 			),
