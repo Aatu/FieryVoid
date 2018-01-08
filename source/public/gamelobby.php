dacb69261be7bf4fd33e35863b069057b6435117
Zero1627 <reisen@poczta.onet.pl>
2018-01-08 15:56:58 +0100

    move notes to top right

--- source/public/gamelobby.php
+++ source/public/gamelobby.php
@@ -379,7 +379,7 @@
                 <tr>
                     <td class="col1">
                         <div class="icon"><img src="" alt="" border="" width="120" height="120"></div>
-                        <div class="notes"></div>
+                        <!--<div class="notes"></div> -->
                     </td>                
                     <td class="col2">
                         <div id="shipSection_1" class="shipSection" style="margin-top: 15px">
@@ -393,7 +393,8 @@
                         </div>
                     </td>
                     <td class="col3">
-                        <div class="EW">
+			<div class="notes"></div> <!-- Marcin Sawicki - here more readable -->
+                        <div class="EW" visibility="hidden"><!-- Marcin Sawicki - at fleet selection EW window is useless! -->
                             <div class="ewheader"><span class="header">ELECTRONIC WARFARE</span></div>
                             <div class="EWcontainer">
                                 <div class="ewentry"><span class="valueheader">Defensive:</span><span class="value DEW"></span></div>
