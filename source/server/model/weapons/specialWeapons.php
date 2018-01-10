455fd24a5fc3ebf691a87c3c8a7cbd795170c3fc
Zero1627 <reisen@poczta.onet.pl>
2018-01-10 09:31:17 +0100

    Update specialWeapons.php

--- source/server/model/weapons/specialWeapons.php
+++ source/server/model/weapons/specialWeapons.php
@@ -989,7 +989,7 @@ $count = count(SparkFieldHandler::$sparkFields);
 					$validTarget = true;
 					if ($shooter->id == $target->id) $validTarget = false;//does not threaten self!
 					if ($target->isDestroyed()) $validTarget = false; //no point allocating
-					if (in_array($target->id,$alreadyTargeted)) $validTarget = false; //each target only once
+//TEST					if (in_array($target->id,$alreadyTargeted)) $validTarget = false; //each target only once
 					if ($validTarget) {
 						$alreadyTargeted[] = $target->id; //add to list of already targeted units
 						//create appropriate firing order
