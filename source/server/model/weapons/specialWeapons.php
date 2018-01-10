f8b78d281ff4e867d970b53165667e81b4f0b4e3
Zero1627 <reisen@poczta.onet.pl>
2018-01-10 12:59:27 +0100

    Update specialWeapons.php

--- source/server/model/weapons/specialWeapons.php
+++ source/server/model/weapons/specialWeapons.php
@@ -985,7 +985,7 @@ class SparkFieldHandler{
 			foreach($inAoE as $targetID=>$target){
 				if ($shooter->id == $target->id) continue;//does not threaten self!
 				if ($target->isDestroyed()) continue; //no point allocating
-//				if (in_array($target->id,$alreadyTargeted)) continue; //each target only once
+				if (in_array($target->id,$alreadyTargeted,true)) continue; //each target only once
 				$alreadyTargeted[] = $target->id; //add to list of already targeted units
 				//create appropriate firing order
 				$fire = new FireOrder(-1, 'normal', $shooter->id, $target->id, $field->id, -1, $gamedata->turn, 1, 0, 0, 1, 0, 0,  0, null);
