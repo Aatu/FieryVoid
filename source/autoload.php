48571f8a8df7cca8fcae0ad2e7f25d9d42c7ad4e
Zero1627 <reisen@poczta.onet.pl>
2018-01-22 13:48:05 +0100

    Update autoload.php

--- source/autoload.php
+++ source/autoload.php
@@ -356,8 +356,10 @@ spl_autoload_register(
                 'ftrurchin' => '/server/model/ships/ipsha/ftrUrchin.php',                
                 'shipbattleglobe' => '/server/model/ships/ipsha/shipBattleglobe.php',  
                 'shipbattlehex' => '/server/model/ships/ipsha/shipBattlehex.php',
-                'shipcarriercube' => '/server/model/ships/ipsha/shipCarrierCube.php',
+                'shipcarriercube' => '/server/model/ships/ipsha/shipCarrierCube.php', 
+                'shiprhombusescort' => '/server/model/ships/ipsha/shipRhombusEscort.php',
                 'shiptetraship' => '/server/model/ships/ipsha/shipTetraship.php',
+                    'shiptetraescort' => '/server/model/ships/ipsha/shipTetraEscort.php',
                 'shipwarsphere' => '/server/model/ships/ipsha/shipWarsphere.php',
                 
                 
