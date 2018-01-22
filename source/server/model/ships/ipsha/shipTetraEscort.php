79aa7769702b27142d0386bf0e5a19533c728d81
Zero1627 <reisen@poczta.onet.pl>
2018-01-22 13:46:19 +0100

    Update shipTetraEscort.php

--- source/server/model/ships/ipsha/shipTetraEscort.php
+++ source/server/model/ships/ipsha/shipTetraEscort.php
@@ -1,5 +1,6 @@
 <?php
-class ShipTetraship extends MediumShipLeftRight{
+class ShipTetraEscort extends MediumShipLeftRight{
+	/*Variants-5 ship*/
     /*Ipsha general:
      - remember about EM hardening!
      - instead of -2 bonus to dropout/crit when caused by Ion weapon, just add -1 overall crit/dropout bonus
@@ -9,19 +10,20 @@ class ShipTetraship extends MediumShipLeftRight{
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-        $this->pointCost = 350;
+        $this->pointCost = 400;
         $this->faction = "Ipsha";
-        $this->phpclass = "ShipTetraship";
+        $this->phpclass = "ShipTetraEscort";
         $this->imagePath = "img/ships/IpshaTetra.png";
         	$this->canvasSize = 100;
-        $this->shipClass = "Tetraship";    
-	    	    
-        //$this->shipSizeClass = 1;
-        //$this->fighters = array("heavy"=>6);
-	        
+        $this->shipClass = "Tetra Escort";    
+	    
+        $this->variantOf = "Tetraship";    
+	$this->availability = 'uncommon';	        
 	//$this->limited = 33;
-	$this->isd = 2230;
-	$this->notes = 'EM hardened';	  
+	$this->isd = 2235;
+	    
+	$this->notes = 'Eethan Barony only!';
+	$this->notes .= '<br>EM hardened';	  
 	$this->notes .= '<br>-1 critical roll bonus';
 	$this->EMHardened = true; //EM Hardening - some weapons would check for this value!
 	$this->critRollMod = -1; //generalbonus to critical rolls!
@@ -30,7 +32,7 @@ class ShipTetraship extends MediumShipLeftRight{
         $this->sideDefense = 12;
         
         $this->turncost = 0.33;
-        $this->turndelaycost = 0.22;
+        $this->turndelaycost = 0.33;
         $this->accelcost = 1; //1accel/2 decel by original SCS; I gave it just 1 (same as larger Battlehex with similar thruster arrangement)
         $this->rollcost = 0;
         $this->pivotcost = 1;
@@ -40,21 +42,23 @@ class ShipTetraship extends MediumShipLeftRight{
 	    
 	    
         
-	$this->addPrimarySystem(new MagGravReactor(3, 15, 0, 12));
+	$this->addPrimarySystem(new MagGravReactor(3, 16, 0, 13));
 	$this->addPrimarySystem(new CnC(3, 12, 0, 0));
         $this->addPrimarySystem(new Scanner(3, 12, 4, 5));
-        $this->addPrimarySystem(new Engine(4, 16, 0, 3, 2));
+        $this->addPrimarySystem(new Engine(3, 18, 0, 4, 2));
         //$this->addPrimarySystem(new Hangar(4, 6));	
-	$this->addPrimarySystem(new SurgeBlaster(3, 0, 0, 300, 60));
+	$this->addPrimarySystem(new SparkField(3, 0, 0, 0, 360));
         $this->addPrimarySystem(new MagGraviticThruster(4, 15, 0, 99, 1)); 
-        $this->addPrimarySystem(new MagGraviticThruster(4, 10, 0, 99, 2));
-        $this->addPrimarySystem(new MagGraviticThruster(4, 10, 0, 99, 2));	    
+        $this->addPrimarySystem(new MagGraviticThruster(3, 10, 0, 99, 2));
+        $this->addPrimarySystem(new MagGraviticThruster(3, 10, 0, 99, 2));	    
 	    
 	$this->addLeftSystem(new MagGraviticThruster(3, 13, 0, 99, 3));
 	$this->addLeftSystem(new EmPulsar(3, 0, 0, 180, 360));
 	$this->addLeftSystem(new EmPulsar(3, 0, 0, 180, 360));
+	$this->addLeftSystem(new EmPulsar(3, 0, 0, 180, 360));
 		
         $this->addRightSystem(new MagGraviticThruster(3, 13, 0, 99, 4));
+	$this->addRightSystem(new EmPulsar(3, 0, 0, 0, 180));
 	$this->addRightSystem(new EmPulsar(3, 0, 0, 0, 180));
 	$this->addRightSystem(new EmPulsar(3, 0, 0, 0, 180));
 		
@@ -65,7 +69,7 @@ class ShipTetraship extends MediumShipLeftRight{
 		$this->hitChart = array(
 			0=> array(
 				6 => "Thruster",
-				9 => "Surge Blaster",
+				9 => "Spark Field",
 				12 => "Scanner",
 				15 => "Engine",
 				18 => "Reactor",
