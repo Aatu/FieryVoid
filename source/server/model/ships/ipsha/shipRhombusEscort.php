edfc415a4544a01ae7e0287b8a6ab5d82060ee95
Zero1627 <reisen@poczta.onet.pl>
2018-01-22 13:34:46 +0100

    Update shipRhombusEscort.php

--- source/server/model/ships/ipsha/shipRhombusEscort.php
+++ source/server/model/ships/ipsha/shipRhombusEscort.php
@@ -1,5 +1,6 @@
 <?php
-class ShipBattlehex extends HeavyCombatVesselLeftRight{ 
+class ShipRhombusEscort extends HeavyCombatVesselLeftRight{ 
+	/*unofficial ship - from The Great Machine*/
     /*Ipsha general:
      - remember about EM hardening!
      - instead of -2 bonus to dropout/crit when caused by Ion weapon, just add -1 overall crit/dropout bonus
@@ -9,28 +10,28 @@ class ShipBattlehex extends HeavyCombatVesselLeftRight{
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-        $this->pointCost = 500;
+        $this->pointCost = 385;
         $this->faction = "Ipsha";
-        $this->phpclass = "ShipBattlehex";
-        $this->imagePath = "img/ships/IpshaHex.png";
-        $this->shipClass = "Battlehex";    
+        $this->phpclass = "RhombusEscort";
+        $this->imagePath = "img/ships/Rhombus.png";
+        $this->shipClass = "Rhombus Escort Cruiser";    
 	    	    
-        //$this->shipSizeClass = 3;
+        $this->unofficial = true;
         //$this->fighters = array("heavy"=>6);
 	        
 	//$this->limited = 33;
-	$this->isd = 2200;
+	$this->isd = 2209;
 	$this->notes = 'EM hardened';	  
 	$this->notes .= '<br>-1 critical roll bonus';
 	$this->EMHardened = true; //EM Hardening - some weapons would check for this value!
 	$this->critRollMod = -1; //generalbonus to critical rolls!
 		
-        $this->forwardDefense = 16;
+        $this->forwardDefense = 14;
         $this->sideDefense = 13;
         
         $this->turncost = 0.66;
         $this->turndelaycost = 0.66;
-        $this->accelcost = 1;
+        $this->accelcost = 2;
         $this->rollcost = 0;
         $this->pivotcost = 1;
 	$this->gravitic = true;
@@ -39,36 +40,34 @@ class ShipBattlehex extends HeavyCombatVesselLeftRight{
 	    
 	    
         
-	$this->addPrimarySystem(new MagGravReactor(4, 19, 0, 30));
+	$this->addPrimarySystem(new MagGravReactor(4, 20, 0, 20));
 	$this->addPrimarySystem(new CnC(4, 16, 0, 0));
-        $this->addPrimarySystem(new Scanner(4, 14, 4, 6));
-        $this->addPrimarySystem(new Engine(4, 25, 0, 4, 3));
+        $this->addPrimarySystem(new Scanner(4, 12, 4, 5));
+        $this->addPrimarySystem(new Engine(4, 26, 0, 6, 4));
         //$this->addPrimarySystem(new Hangar(4, 6));
-	$this->addPrimarySystem(new ResonanceGenerator(3, 0, 0, 270, 90));
-        $this->addPrimarySystem(new MagGraviticThruster(4, 15, 0, 99, 1));
-        $this->addPrimarySystem(new MagGraviticThruster(4, 11, 0, 99, 2));
-        $this->addPrimarySystem(new MagGraviticThruster(4, 11, 0, 99, 2));	    
+	$this->addPrimarySystem(new SparkField(4, 0, 0, 0, 360));
+        $this->addPrimarySystem(new MagGraviticThruster(3, 15, 0, 99, 1));
+        $this->addPrimarySystem(new MagGraviticThruster(3, 15, 0, 99, 2));	    
 	    
 	$this->addLeftSystem(new MagGraviticThruster(3, 13, 0, 99, 3));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
+	$this->addLeftSystem(new BurstBeam(2, 6, 3, 180, 360));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
+	    
 		
         $this->addRightSystem(new MagGraviticThruster(3, 13, 0, 99, 4));
+	$this->addLeftSystem(new BurstBeam(2, 6, 3, 0, 180));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
 		
 		
-        $this->addLeftSystem(new Structure(4, 60));
-        $this->addRightSystem(new Structure(3, 45));
-        $this->addPrimarySystem(new Structure(3, 45));
+        $this->addLeftSystem(new Structure(4, 40));
+        $this->addRightSystem(new Structure(3, 40));
+        $this->addPrimarySystem(new Structure(3, 40));
 		
 		
 		$this->hitChart = array(
