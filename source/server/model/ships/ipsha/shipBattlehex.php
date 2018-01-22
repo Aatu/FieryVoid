0d67ed129187703962f8914c0c14b16c3b22d61a
Zero1627 <reisen@poczta.onet.pl>
2018-01-22 12:03:08 +0100

    Update shipBattlehex.php

--- source/server/model/ships/ipsha/shipBattlehex.php
+++ source/server/model/ships/ipsha/shipBattlehex.php
@@ -1,5 +1,5 @@
 <?php
-class ShipBattleglobe extends HeavyCombatVesselLeftRight{ //technically a Capital ship with HCV hull arrangement
+class ShipBattlehex extends HeavyCombatVesselLeftRight{ 
     /*Ipsha general:
      - remember about EM hardening!
      - instead of -2 bonus to dropout/crit when caused by Ion weapon, just add -1 overall crit/dropout bonus
@@ -9,83 +9,73 @@ class ShipBattleglobe extends HeavyCombatVesselLeftRight{ //technically a Capita
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-        $this->pointCost = 700;
+        $this->pointCost = 500;
         $this->faction = "Ipsha";
-        $this->phpclass = "ShipBattleglobe";
-        $this->imagePath = "img/ships/IpshaBattleglobe.png";
-        $this->shipClass = "Battleglobe";    
+        $this->phpclass = "ShipBattlehex";
+        $this->imagePath = "img/ships/IpshaHex.png";
+        $this->shipClass = "Battlehex";    
 	    	    
         $this->shipSizeClass = 3;
         //$this->fighters = array("heavy"=>6);
 	        
 	//$this->limited = 33;
-	$this->isd = 2202;
+	$this->isd = 2200;
 	$this->notes = 'EM hardened';	  
 	$this->notes .= '<br>-1 critical roll bonus';
 	$this->EMHardened = true; //EM Hardening - some weapons would check for this value!
 	$this->critRollMod = -1; //generalbonus to critical rolls!
 		
-        $this->forwardDefense = 18;
-        $this->sideDefense = 14;
+        $this->forwardDefense = 16;
+        $this->sideDefense = 13;
         
-        $this->turncost = 1;
-        $this->turndelaycost = 1;
+        $this->turncost = 0.66;
+        $this->turndelaycost = 0.66;
         $this->accelcost = 1;
         $this->rollcost = 0;
-        $this->pivotcost = 2;
+        $this->pivotcost = 1;
 	$this->gravitic = true;
         
-	$this->iniativebonus = 0 *5; //THIS IS A CAPITAL SHIP, despite HCV arrangement!
+	$this->iniativebonus = 6 *5; 
 	    
 	    
         
-        //$this->addPrimarySystem(new MagGravReactor(4, 28, 0, 60));
-	$this->addPrimarySystem(new MagGravReactor(4, 25, 0, 42));
-	$this->addPrimarySystem(new CnC(4, 20, 0, 0));
-        $this->addPrimarySystem(new Scanner(4, 16, 5, 7));
-        $this->addPrimarySystem(new Engine(4, 30, 0, 4, 4));
+	$this->addPrimarySystem(new MagGravReactor(4, 19, 0, 30));
+	$this->addPrimarySystem(new CnC(4, 16, 0, 0));
+        $this->addPrimarySystem(new Scanner(4, 14, 4, 6));
+        $this->addPrimarySystem(new Engine(4, 25, 0, 4, 3));
         //$this->addPrimarySystem(new Hangar(4, 6));
-	$this->addPrimarySystem(new SparkField(4, 0, 0, 0, 360));
-        $this->addPrimarySystem(new MagGraviticThruster(4, 13, 0, 99, 1));
-        $this->addPrimarySystem(new MagGraviticThruster(4, 13, 0, 99, 1));
-        $this->addPrimarySystem(new MagGraviticThruster(4, 13, 0, 99, 2));
-        $this->addPrimarySystem(new MagGraviticThruster(4, 13, 0, 99, 2));	    
+	$this->addPrimarySystem(new ResonanceGenerator(3, 0, 0, 270, 90));
+        $this->addPrimarySystem(new MagGraviticThruster(4, 15, 0, 99, 1));
+        $this->addPrimarySystem(new MagGraviticThruster(4, 11, 0, 99, 2));
+        $this->addPrimarySystem(new MagGraviticThruster(4, 11, 0, 99, 2));	    
 	    
-	$this->addLeftSystem(new MagGraviticThruster(4, 15, 0, 99, 3));
+	$this->addLeftSystem(new MagGraviticThruster(3, 13, 0, 99, 3));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
 		
-        $this->addRightSystem(new MagGraviticThruster(4, 15, 0, 99, 4));
+        $this->addRightSystem(new MagGraviticThruster(3, 13, 0, 99, 4));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
 		
 		
         $this->addLeftSystem(new Structure(4, 60));
-        $this->addRightSystem(new Structure(4, 60));
-        $this->addPrimarySystem(new Structure(4, 52));
+        $this->addRightSystem(new Structure(3, 45));
+        $this->addPrimarySystem(new Structure(3, 45));
 		
 		
 		$this->hitChart = array(
 			0=> array(
 				8 => "Structure",
 				10 => "Thruster",
-				12 => "Spark Field",
+				12 => "Resonance Generator",
 				14 => "Scanner",
 				16 => "Engine",
 				18 => "Reactor",
