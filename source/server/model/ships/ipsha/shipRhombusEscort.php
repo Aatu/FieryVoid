f2d879e777a2812819b7172f75d56c1da92c4cf3
Zero1627 <reisen@poczta.onet.pl>
2018-01-22 15:07:57 +0100

    Update shipRhombusEscort.php

--- source/server/model/ships/ipsha/shipRhombusEscort.php
+++ source/server/model/ships/ipsha/shipRhombusEscort.php
@@ -55,16 +55,16 @@ class ShipRhombusEscort extends HeavyCombatVesselLeftRight{
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
-	$this->addLeftSystem(new MagGraviticThruster(3, 13, 0, 99, 3));
 	$this->addLeftSystem(new BurstBeam(2, 6, 3, 180, 360));
+	$this->addLeftSystem(new MagGraviticThruster(3, 13, 0, 99, 3));
 	    
 		
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
-        $this->addRightSystem(new MagGraviticThruster(3, 13, 0, 99, 4));
 	$this->addRightSystem(new BurstBeam(2, 6, 3, 0, 180));
+        $this->addRightSystem(new MagGraviticThruster(3, 13, 0, 99, 4));
 	    
 		
 		
