b1c4995c41a76acd2d060e49b7299f149c8ac388
Zero1627 <reisen@poczta.onet.pl>
2018-01-22 14:55:02 +0100

    Update shipBattlehex.php

--- source/server/model/ships/ipsha/shipBattlehex.php
+++ source/server/model/ships/ipsha/shipBattlehex.php
@@ -5,6 +5,7 @@ class ShipBattlehex extends HeavyCombatVesselLeftRight{
      - instead of -2 bonus to dropout/crit when caused by Ion weapon, just add -1 overall crit/dropout bonus
      - remind player of the above in comments in fleet selection phase!
      - Singularity Drive replaced by standard engine
+     - add Sensor power demand to Reactor output (it's considered 'baseline' for tabletop!)
     */
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
@@ -39,7 +40,7 @@ class ShipBattlehex extends HeavyCombatVesselLeftRight{
 	    
 	    
         
-	$this->addPrimarySystem(new MagGravReactor(4, 19, 0, 30));
+	$this->addPrimarySystem(new MagGravReactor(4, 19, 0, 30+4));
 	$this->addPrimarySystem(new CnC(4, 16, 0, 0));
         $this->addPrimarySystem(new Scanner(4, 14, 4, 6));
         $this->addPrimarySystem(new Engine(4, 25, 0, 4, 3));
