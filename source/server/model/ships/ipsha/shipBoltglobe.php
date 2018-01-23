bdfe5976d95d1977fe1245e37079f5d8dfb3c2b1
Zero1627 <reisen@poczta.onet.pl>
2018-01-23 09:37:01 +0100

    Update shipBoltglobe.php

--- source/server/model/ships/ipsha/shipBoltglobe.php
+++ source/server/model/ships/ipsha/shipBoltglobe.php
@@ -1,5 +1,6 @@
 <?php
-class ShipBattleglobe extends HeavyCombatVesselLeftRight{ //technically a Capital ship with HCV hull arrangement
+class ShipBoltglobe extends HeavyCombatVesselLeftRight{ //technically a Capital ship with HCV hull arrangement
+	/*Variants-5*/
     /*Ipsha general:
      - remember about EM hardening!
      - instead of -2 bonus to dropout/crit when caused by Ion weapon, just add -1 overall crit/dropout bonus
@@ -10,18 +11,21 @@ class ShipBattleglobe extends HeavyCombatVesselLeftRight{ //technically a Capita
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-        $this->pointCost = 700;
+        $this->pointCost = 750;
         $this->faction = "Ipsha";
-        $this->phpclass = "ShipBattleglobe";
+        $this->phpclass = "ShipBoltglobe";
         $this->imagePath = "img/ships/IpshaBattleglobe.png";
-        $this->shipClass = "Battleglobe";    
+        $this->shipClass = "Boltglobe";    
+        	$this->variantOf = "Battleglobe";    
+	    	$this->availability = 'rare';
 	    	    
         $this->shipSizeClass = 3;
         //$this->fighters = array("heavy"=>6);
 	        
 	//$this->limited = 33;
 	$this->isd = 2202;
-	$this->notes = 'EM hardened';	  
+	$this->notes = 'Eethan Barony only';	 
+	$this->notes .= 'EM hardened';	  
 	$this->notes .= '<br>-1 critical roll bonus';
 	$this->EMHardened = true; //EM Hardening - some weapons would check for this value!
 	$this->critRollMod = -1; //generalbonus to critical rolls!
@@ -41,7 +45,7 @@ class ShipBattleglobe extends HeavyCombatVesselLeftRight{ //technically a Capita
 	    
         
         //$this->addPrimarySystem(new MagGravReactor(4, 28, 0, 60));
-	$this->addPrimarySystem(new MagGravReactor(4, 25, 0, 42+5));
+	$this->addPrimarySystem(new MagGravReactor(4, 28, 0, 45+5));
 	$this->addPrimarySystem(new CnC(4, 20, 0, 0));
         $this->addPrimarySystem(new Scanner(4, 16, 5, 7));
         $this->addPrimarySystem(new Engine(4, 30, 0, 4, 4));
@@ -52,28 +56,20 @@ class ShipBattleglobe extends HeavyCombatVesselLeftRight{ //technically a Capita
         $this->addPrimarySystem(new MagGraviticThruster(4, 13, 0, 99, 2));
         $this->addPrimarySystem(new MagGraviticThruster(4, 13, 0, 99, 2));	    
 	    
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));	    
+	$this->addLeftSystem(new EmBolter(4, 0, 0, 300, 60));
+	$this->addLeftSystem(new EmBolter(4, 0, 0, 300, 60));
+	$this->addLeftSystem(new EmBolter(4, 0, 0, 300, 60));
+	$this->addLeftSystem(new EmBolter(4, 0, 0, 120, 240));
+	$this->addLeftSystem(new EmBolter(4, 0, 0, 120, 240));
+	$this->addLeftSystem(new EmBolter(4, 0, 0, 120, 240));	    
 	$this->addLeftSystem(new MagGraviticThruster(4, 15, 0, 99, 3));
 		
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
+	$this->addRightSystem(new EmBolter(4, 0, 0, 300, 60));
+	$this->addRightSystem(new EmBolter(4, 0, 0, 300, 60));
+	$this->addRightSystem(new EmBolter(4, 0, 0, 300, 60));
+	$this->addRightSystem(new EmBolter(4, 0, 0, 120, 240));
+	$this->addRightSystem(new EmBolter(4, 0, 0, 120, 240));
+	$this->addRightSystem(new EmBolter(4, 0, 0, 120, 240));
         $this->addRightSystem(new MagGraviticThruster(4, 15, 0, 99, 4));
 		
 		
@@ -94,13 +90,13 @@ class ShipBattleglobe extends HeavyCombatVesselLeftRight{ //technically a Capita
 			),
 			3=> array(
 				4 => "Thruster",
-				11 => "Surge Cannon",
+				10 => "EM Bolter",
 				18 => "Structure",
 				20 => "Primary",
 			),
 			4=> array(
 				4 => "Thruster",
-				11 => "Surge Cannon",
+				10 => "EM Bolter",
 				18 => "Structure",
 				20 => "Primary",
 			),
