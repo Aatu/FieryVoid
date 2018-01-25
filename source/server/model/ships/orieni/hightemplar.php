eb9de6fead6528bbdf065b577d8efd7d1ea42759
Zero1627 <reisen@poczta.onet.pl>
2018-01-25 11:07:32 +0100

    ISD, variant

--- source/server/model/ships/orieni/hightemplar.php
+++ source/server/model/ships/orieni/hightemplar.php
@@ -5,11 +5,15 @@ class HighTemplar extends FighterFlight{
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-        $this->pointCost = 240;
+        $this->pointCost = 40*6;
         $this->faction = "Orieni";
         $this->phpclass = "HighTemplar";
-        $this->shipClass = "High Templar Light flight";
+        $this->shipClass = "High Templar Interceptor flight";
         $this->imagePath = "img/ships/highTemplar.png";
+        
+        $this->variantOf = "Templar Interceptor flight";
+        $this->occurence = "uncommon";
+        $this->isd = 2008;
 
         $this->forwardDefense = 7;
         $this->sideDefense = 6;
@@ -31,7 +35,7 @@ class HighTemplar extends FighterFlight{
         for ($i = 0; $i < $toAdd; $i++){
             $armour = array(2, 1, 1, 1);
             $fighter = new Fighter("templar", $armour, 9, $this->id);
-            $fighter->displayName = "High Templar Light Fighter";
+            $fighter->displayName = "High Templar";
             $fighter->imagePath = "img/ships/highTemplar.png";
             $fighter->iconPath = "img/ships/highTemplar_large.png";
 
