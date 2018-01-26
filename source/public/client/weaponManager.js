74a72087ff890190f4832c2e8869589dc61edaa9
Zero1627 <reisen@poczta.onet.pl>
2018-01-26 15:49:28 +0100

    ramming attack

--- source/public/client/weaponManager.js
+++ source/public/client/weaponManager.js
@@ -722,8 +722,8 @@ window.weaponManager = {
 		
 		if (target.Enormous) hitChance+=6;//+6 vs Enormous units
 		if (shooter.Enormous) hitChance+=6;//+6 if ramming unit is Enormous
-		if ((target.size >= 3) && (shooter.size <3))hitChance += 2;//+2 if target is Capital and ramming unit is not
-		if ((shooter.size >= 3) && (target.size <3))hitChance -= 2;//-2 if shooter is Capital and rammed unit is not
+		if ((target.shipSizeClass >= 3) && (shooter.shipSizeClass <3))hitChance += 2;//+2 if target is Capital and ramming unit is not
+		if ((shooter.shipSizeClass >= 3) && (target.shipSizeClass <3))hitChance -= 2;//-2 if shooter is Capital and rammed unit is not
 		if ((shooter.flight) && (!target.flight))hitChance += 4;//+4 for fighter trying to ram a ship
 		var targetSpeed = Math.abs(shipManager.movement.getSpeed(target)); //I think speed cannot be negative, but just in case ;)
 		switch(targetSpeed) {
