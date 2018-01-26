d7bf6c85ba79778a7407c283b3c689836cb424e8
Zero1627 <reisen@poczta.onet.pl>
2018-01-26 14:58:54 +0100

    Update hkShiningLight.php

--- source/server/model/ships/orieni/hkShiningLight.php
+++ source/server/model/ships/orieni/hkShiningLight.php
@@ -6,7 +6,7 @@ class HkShiningLight extends FighterFlight{
         parent::__construct($id, $userid, $name,  $slot);
         
         $this->pointCost = 32*6;
-        $this->faction = "custom";//"Orieni";
+        $this->faction = "Orieni";
         $this->phpclass = "HkShiningLight";
         $this->shipClass = "Shining Light Hunter-Killer flight";
         $this->imagePath = "img/ships/OrieniHK.png";
