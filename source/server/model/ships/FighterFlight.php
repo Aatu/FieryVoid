87b2f9f9a6064d4ea808319e8bee28ffd8b4e740
Zero1627 <reisen@poczta.onet.pl>
2018-01-31 18:29:03 +0100

    Update FighterFlight.php

--- source/server/model/ships/FighterFlight.php
+++ source/server/model/ships/FighterFlight.php
@@ -191,7 +191,7 @@
 				if ($system instanceof RammingAttack) $alreadyRamming = true;
 			}
 			if(!$alreadyRamming){			
-				$fighter->addAftSystem(new RammingAttack(0, 0, 360, 0, 0));
+//TEST				$fighter->addAftSystem(new RammingAttack(0, 0, 360, 0, 0));
 			}
 		}		
 		
