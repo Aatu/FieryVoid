ad85d7ff4ad2c7706748808539c6c3108a459828
Venlesh <w_lackner@gmx.at>
2018-02-04 11:55:30 +0100

    Update lystala.php

--- source/server/model/ships/abbai_old/lystala.php
+++ source/server/model/ships/abbai_old/lystala.php
@@ -5,7 +5,7 @@ class Lystala extends BaseShip{
         parent::__construct($id, $userid, $name,  $slot);
         
 	$this->pointCost = 415;
-	$this->faction = "Abbai";
+	$this->faction = "Abbai_old";
         $this->phpclass = "Lystala";
         $this->imagePath = "img/ships/AbbaiKastona.png";
         $this->shipClass = "Lystala Patrol Cruiser";
