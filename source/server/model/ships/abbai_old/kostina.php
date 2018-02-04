7f9e81b8716e649d6a5d75b0d813cf477e29953c
Venlesh <w_lackner@gmx.at>
2018-02-04 11:55:15 +0100

    Update kostina.php

--- source/server/model/ships/abbai_old/kostina.php
+++ source/server/model/ships/abbai_old/kostina.php
@@ -5,7 +5,7 @@ class Kostina extends BaseShip{
         parent::__construct($id, $userid, $name,  $slot);
         
 	$this->pointCost = 525;
-	$this->faction = "Abbai";
+	$this->faction = "Abbai_old";
         $this->phpclass = "Kostina";
         $this->imagePath = "img/ships/AbbaiKastona.png";
         $this->shipClass = "Kostina Carrier";
