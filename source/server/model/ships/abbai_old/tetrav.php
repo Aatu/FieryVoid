76e716223a20ba6818785b051e51d4ed8a63a141
Zero1627 <reisen@poczta.onet.pl>
2018-02-07 10:43:39 +0100

    Update tetrav.php

--- source/server/model/ships/abbai_old/tetrav.php
+++ source/server/model/ships/abbai_old/tetrav.php
@@ -5,7 +5,7 @@ class Tetrav extends HeavyCombatVessel{
         parent::__construct($id, $userid, $name,  $slot);
         
 	$this->pointCost = 400;
-	$this->faction = "Abbai_old";
+	$this->faction = "Abbai (WotCR)";
         $this->phpclass = "Tetrav";
         $this->imagePath = "img/ships/AbbaiTetrav.png";
         $this->shipClass = "Tetrav Heavy Frigate";
