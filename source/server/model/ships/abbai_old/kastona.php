063c78174d58ee103a5ede15a429ef6f34ac2820
Zero1627 <reisen@poczta.onet.pl>
2018-02-07 10:40:23 +0100

    Update kastona.php

--- source/server/model/ships/abbai_old/kastona.php
+++ source/server/model/ships/abbai_old/kastona.php
@@ -5,7 +5,7 @@ class Kastona extends BaseShip{
         parent::__construct($id, $userid, $name,  $slot);
         
 	$this->pointCost = 530;
-	$this->faction = "Abbai_old";
+	$this->faction = "Abbai (WotCR)";
         $this->phpclass = "Kastona";
         $this->imagePath = "img/ships/AbbaiKastona.png";
         $this->shipClass = "Kastona Cruiser";
