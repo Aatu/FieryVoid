3613cddad99e71caa30d7c16db64903d4da4b5a9
Zero1627 <reisen@poczta.onet.pl>
2018-02-07 11:14:18 +0100

    Update lokita.php

--- source/server/model/ships/abbai/lokita.php
+++ source/server/model/ships/abbai/lokita.php
@@ -33,31 +33,35 @@ class Lokita extends BaseShip{
  	$this->addPrimarySystem(new Hangar(5, 2));
         $this->addPrimarySystem(new ShieldGenerator(5, 16, 4, 3));
    
-        $this->addFrontSystem(new Thruster(4, 10, 0, 3, 1));
-        $this->addFrontSystem(new Thruster(4, 10, 0, 3, 1));
+
         $this->addFrontSystem(new CombatLaser(3, 0, 0, 300, 60));
         $this->addFrontSystem(new CombatLaser(3, 0, 0, 300, 60));
         $this->addFrontSystem(new QuadArray(3, 0, 0, 240, 60));
         $this->addFrontSystem(new QuadArray(3, 0, 0, 300, 120));
         $this->addFrontSystem(new GraviticShield(0, 6, 0, 3, 300, 360));
         $this->addFrontSystem(new GraviticShield(0, 6, 0, 3, 0, 60));
+        $this->addFrontSystem(new Thruster(4, 10, 0, 3, 1));
+        $this->addFrontSystem(new Thruster(4, 10, 0, 3, 1));
+	    
 
-        $this->addAftSystem(new Thruster(4, 12, 0, 5, 2));
-        $this->addAftSystem(new Thruster(4, 12, 0, 5, 2));
+
         $this->addAftSystem(new JumpEngine(4, 14, 4, 32));
         $this->addAftSystem(new QuadArray(3, 0, 0, 300, 120));
         $this->addAftSystem(new GraviticShield(0, 6, 0, 3, 90, 270));
         $this->addAftSystem(new GraviticShield(0, 6, 0, 3, 120, 180));
+        $this->addAftSystem(new Thruster(4, 12, 0, 5, 2));
+        $this->addAftSystem(new Thruster(4, 12, 0, 5, 2));
 
         $this->addLeftSystem(new GraviticShield(0, 6, 0, 2, 240, 300));
-        $this->addLeftSystem(new Thruster(3, 13, 0, 6, 3));
         $this->addLeftSystem(new Hangar(3, 6));
         $this->addLeftSystem(new Particleimpeder(2, 0, 0, 180, 360));
+        $this->addLeftSystem(new Thruster(3, 13, 0, 6, 3));
 
         $this->addRightSystem(new GraviticShield(0, 6, 0, 2, 60, 120));
-        $this->addRightSystem(new Thruster(3, 13, 0, 6, 4));
         $this->addRightSystem(new Hangar(3, 6));
         $this->addRightSystem(new Particleimpeder(2, 0, 0, 0, 180));
+        $this->addRightSystem(new Thruster(3, 13, 0, 6, 4));
+	    
         
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
         $this->addFrontSystem(new Structure(4, 36));
