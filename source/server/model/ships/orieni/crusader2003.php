478f6ad5fe21479bdc51027eb4ef0b0c4a78040b
Zero1627 <reisen@poczta.onet.pl>
2018-02-12 11:30:54 +0100

    Update crusader2003.php

--- source/server/model/ships/orieni/crusader2003.php
+++ source/server/model/ships/orieni/crusader2003.php
@@ -1,5 +1,5 @@
 <?php
-class Crusader extends MediumShip{
+class Crusader2003 extends MediumShip{
     
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
