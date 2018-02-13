bfb150b707258ada75a60cf18a90d972d258fe35
Aatu Riuttamäki <aatu.riuttamaki@gmail.com>
2018-02-13 18:48:38 +0200

    New news

--- source/public/games.php
+++ source/public/games.php
@@ -1,139 +1,126 @@
 <?php
-    include_once 'global.php';
- 
-	if (!isset($_SESSION["user"]) || $_SESSION["user"] == false){
-		header('Location: index.php');
+include_once 'global.php';
+
+if (!isset($_SESSION["user"]) || $_SESSION["user"] == false) {
+    header('Location: index.php');
 //      return;
-	}
-	
-	$games = Manager::getTacGames($_SESSION["user"]);
-	
-	$games = json_encode($games, JSON_NUMERIC_CHECK);
-	
+}
+
+$games = Manager::getTacGames($_SESSION["user"]);
+
+$games = json_encode($games, JSON_NUMERIC_CHECK);
+
 ?>
 
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<title>Fiery Void - Games</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<link href="styles/base.css" rel="stylesheet" type="text/css">
-		<link href="styles/lobby.css" rel="stylesheet" type="text/css">
-		<link href="styles/games.css" rel="stylesheet" type="text/css">
-        <link href="styles/confirm.css" rel="stylesheet" type="text/css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<!--		<script src="client/helper.js"></script>-->
-        <script src="client/games.js"></script>
-		<script src="client/ajaxInterface.js"></script>
-		<script src="client/player.js"></script>
-        <script src="client/mathlib.js"></script>
-        <script src="client/UI/confirm.js"></script>
-		<script> 
-			jQuery(function($){
-            
-				gamedata.parseServerData(<?php print($games); ?>);
-                ajaxInterface.startPollingGames();
+<head>
+    <title>Fiery Void - Games</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link href="styles/base.css" rel="stylesheet" type="text/css">
+    <link href="styles/lobby.css" rel="stylesheet" type="text/css">
+    <link href="styles/games.css" rel="stylesheet" type="text/css">
+    <link href="styles/confirm.css" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <!--		<script src="client/helper.js"></script>-->
+    <script src="client/games.js"></script>
+    <script src="client/ajaxInterface.js"></script>
+    <script src="client/player.js"></script>
+    <script src="client/mathlib.js"></script>
+    <script src="client/UI/confirm.js"></script>
+    <script>
+        jQuery(function ($) {
 
-				gamedata.thisplayer = <?php print($_SESSION["user"]); ?>;                
-			});
+            gamedata.parseServerData(<?php print($games); ?>);
+            ajaxInterface.startPollingGames();
 
-            function loadFireList(){
-                ajaxInterface.getFirePhaseGames();
-            }
+            gamedata.thisplayer = <?php print($_SESSION["user"]); ?>;
+        });
 
-            $(document).ready(function(){
+        function loadFireList() {
+            ajaxInterface.getFirePhaseGames();
+        }
 
-                var header = document.getElementById("newsHeader");
-                    header.innerHTML = "Latest News 21st of December 2017";
-                   
+    </script>
+</head>
+<body>
 
-                var news = document.getElementById("newsEntry");
-                    news.innerHTML += "<big><b>Merry Christmas and a Happy New Year 2018</b></big>!<br>May our little world be still here for 2019.";
-                    news.innerHTML += "<br><br>";
-                    news.innerHTML += "The latest update includes:";
-                    news.innerHTML += "<br><br>";
-                    news.innerHTML += "- fixed some elusive targeting-related bugs (primarily concerning range 0)";
-		    news.innerHTML += "<br>";
-                    news.innerHTML += "- large update regarding split section choice and interception routines - highlights below; interception should be much more reliable now!";
-		    news.innerHTML += "<br>";
-                    news.innerHTML += "-- multi-weapon housings (eg. Twin Array) may now intercept independently for each barrel ";
-                    news.innerHTML += "<br>";
-                    news.innerHTML += "-- ballistic weapons may now be automatically intercepted (although manual interception is still available)";
-                    news.innerHTML += "<br>";
-                    news.innerHTML += "-- improved selection algorithms - now Your defensive grid should be better at recognizing threats; still nothing like human, of course";  
-		    news.innerHTML += "<br>";
-                    news.innerHTML += "-- last but not least - brand new collection of BUGS!";
-                    news.innerHTML += "<br><br>";
-                    news.innerHTML += "Enjoy and report BUGS on FB. Also force reload (ctrl+F5) whenever something weird happens.";
-
-                    
-                    news.innerHTML += "<br><br>----------<b>LINKS</b>----------";
-                    news.innerHTML +="<br><a href=\"https://www.facebook.com/groups/218482691602941/files/\" target=\"_blank\">FV FAQ</a> - differences from B5Wars and known bugs";
-                    news.innerHTML +="<br><a href=\"http://b5warsvault.wikidot.com/\" target=\"_blank\">B5Wars Vault</a> - B5Wars rules and LOTS of related stuff";
-                    news.innerHTML +="<br><a href=\"http://fieryvoid.net/reg.php\">Register</a> new player account";
-                    news.innerHTML +="<br><a href=\"http://fieryvoid.net/chpass.php\">Change password</a> of Your account";
-                     
-                    	
-           });
-		</script>
-	</head>
-	<body>
-	
-        <img src="img/logo.png">
+<img src="img/logo.png">
 <!--        <div class="helphide" style="float:right" onclick="window.helper.onClickHelpHide()">
         <img id="helphideimg" src="img/greyvir.jpg" height="30" width="30">	
         </div>-->
-        <div class="panel news">
-            <div id="newsHeader">
-            </div>
-            <div id="newsEntry">
-            </div>
-        </div>
-        <div class="panel large">
-			<div class="logout"><a href="logout.php">LOGOUT</a></div>
-			
-            <table class="gametable">
-                <tr><td class="centered">ACTIVE GAMES</td><td class="centered">STARTING GAMES</td><td class="centered">ACTIVE FIRE IN:</td></tr>
-                <tr>
-                    <td><div class="gamecontainer active subpanel">
-                        <div class="notfound">No active games</div>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="gamecontainer lobby subpanel">
-                        <div class="notfound">No starting games</div>
-                        </div>
-                        
-                    </td>
-                    <td>
-                        <div id="fireList" class="gamecontainer fire subpanel">
-                        </div>
-                    </td>
-                    <td>
-                        <a class="link" href="creategame.php">CREATE GAME</a>
-                        <input type="button" id="loadFireButton" onclick="loadFireList()" value="LOAD ACTIVE FIRE">
+<div class="panel news">
+    <div id="newsHeader">Latest News 13th of February 2018</div>
+    <div id="newsEntry">
+        <h3>Good news everyone!</h3><br>Our little world is looking awesome, now in <del>hi-def 3d</del> faster 2d!
+        <br><br>
+        The latest update includes:
+        <br><br>
+        - New webGL renderer for fiery void, allowing better performance and more confusing bugs.
+        <br>
+        - Replay feature that allows anyone to replay a whole game from start to finish.
+        <br>
+        - last but not least - brand new collection of BUGS!";
+        <br><br>
+        Enjoy and report BUGS on FB. Also force reload (ctrl+F5) whenever something weird happens.
 
-                    </td>
-                </tr>
-            </table>
-			
-			
-		</div>
-        <div id="globalchat" class="panel large" style="height:150px;">
-        <?php 
-            $chatgameid = 0;
-            $chatelement = "#globalchat";
-            include("chat.php")
-        ?>
-        </div>
-        
+        <br><br>----------<b>LINKS</b>----------
+        <br><a href=\"https://www.facebook.com/groups/218482691602941/files/\" target=\"_blank\">FV FAQ</a> - differences from B5Wars and known bugs
+        <br><a href=\"http://b5warsvault.wikidot.com/\" target=\"_blank\">B5Wars Vault</a> - B5Wars rules and LOTS of related stuff
+        <br><a href=\"http://fieryvoid.net/reg.php\">Register</a> new player account
+        <br><a href=\"http://fieryvoid.net/chpass.php\">Change password</a> of Your account
+    </div>
+</div>
+<div class="panel large">
+    <div class="logout"><a href="logout.php">LOGOUT</a></div>
+
+    <table class="gametable">
+        <tr>
+            <td class="centered">ACTIVE GAMES</td>
+            <td class="centered">STARTING GAMES</td>
+            <td class="centered">ACTIVE FIRE IN:</td>
+        </tr>
+        <tr>
+            <td>
+                <div class="gamecontainer active subpanel">
+                    <div class="notfound">No active games</div>
+                </div>
+            </td>
+            <td>
+                <div class="gamecontainer lobby subpanel">
+                    <div class="notfound">No starting games</div>
+                </div>
+
+            </td>
+            <td>
+                <div id="fireList" class="gamecontainer fire subpanel">
+                </div>
+            </td>
+            <td>
+                <a class="link" href="creategame.php">CREATE GAME</a>
+                <input type="button" id="loadFireButton" onclick="loadFireList()" value="LOAD ACTIVE FIRE">
+
+            </td>
+        </tr>
+    </table>
+
+
+</div>
+<div id="globalchat" class="panel large" style="height:150px;">
+    <?php
+    $chatgameid = 0;
+    $chatelement = "#globalchat";
+    include("chat.php")
+    ?>
+</div>
+
 <!--        <div id="globalhelp" class="helppanel">
         <?php
 //        	$messagelocation='games.php';
 //        	$ingame=false;
 //        	include("helper.php")
-        ?>
+?>
         </div>-->
 
-	</body>
+</body>
 </html>
