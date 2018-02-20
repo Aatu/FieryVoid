cb98f1001a4d5a110b66961050a405021bd458d6
Zero1627 <reisen@poczta.onet.pl>
2018-02-20 13:30:14 +0100

    Update benevolent.php

--- source/server/model/ships/orieni/benevolent.php
+++ source/server/model/ships/orieni/benevolent.php
@@ -33,13 +33,13 @@ class Benevolent extends BaseShip{
         $this->addPrimarySystem(new JumpEngine(5, 30, 6, 25));
         $this->addPrimarySystem(new SMissileRack(5, 6, 0, 0, 360));
         
-        $this->addFrontSystem(new Thruster(3, 12, 0, 3, 1));
-        $this->addFrontSystem(new Thruster(3, 12, 0, 3, 1));
         $this->addFrontSystem(new HeavyLaserLance(3, 6, 4, 240, 60));
         $this->addFrontSystem(new RapidGatling(2, 4, 1, 240, 120));
         $this->addFrontSystem(new RapidGatling(2, 4, 1, 240, 120));
         $this->addFrontSystem(new RapidGatling(2, 4, 1, 240, 120));
         $this->addFrontSystem(new HeavyLaserLance(3, 6, 4, 300, 120));
+        $this->addFrontSystem(new Thruster(3, 12, 0, 3, 1));
+        $this->addFrontSystem(new Thruster(3, 12, 0, 3, 1));
 
         $this->addAftSystem(new RapidGatling(1, 4, 1, 60, 300));
         $this->addAftSystem(new RapidGatling(1, 4, 1, 60, 300));
@@ -47,13 +47,13 @@ class Benevolent extends BaseShip{
         $this->addAftSystem(new Thruster(3, 15, 0, 3, 2));
         $this->addAftSystem(new Thruster(3, 15, 0, 3, 2));
         
+        $this->addLeftSystem(new RapidGatling(2, 4, 1, 180, 360));
+        $this->addLeftSystem(new RapidGatling(2, 4, 1, 180, 360));
         $this->addLeftSystem(new Thruster(4, 20, 0, 6, 3));
-        $this->addLeftSystem(new RapidGatling(2, 4, 1, 180, 360));
-        $this->addLeftSystem(new RapidGatling(2, 4, 1, 180, 360));
 
+        $this->addRightSystem(new RapidGatling(2, 4, 1, 0, 180));
+        $this->addRightSystem(new RapidGatling(2, 4, 1, 0, 180));
         $this->addRightSystem(new Thruster(4, 20, 0, 6, 4));
-        $this->addRightSystem(new RapidGatling(2, 4, 1, 0, 180));
-        $this->addRightSystem(new RapidGatling(2, 4, 1, 0, 180));
 
 		//structures
         $this->addFrontSystem(new Structure(4, 48));
