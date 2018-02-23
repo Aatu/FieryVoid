4eeba52ce0bff591e29bce2dd313bfe7c4f3fd7f
Zero1627 <reisen@poczta.onet.pl>
2018-02-23 13:14:59 +0100

    Update emperorsTransport.php

--- source/server/model/ships/centauri/emperorsTransport.php
+++ source/server/model/ships/centauri/emperorsTransport.php
@@ -11,6 +11,7 @@ class EmperorsTransport extends BaseShip{
 	    $this->variantOf = "Primus Battlecruiser";
         $this->occurence = "special";
         $this->fighters = array("normal"=>12);
+	    $this->isd = 2202;
 	    
 	    
 	    $this->notes = "Emperor's personal transport, scenario unit only";
@@ -38,19 +39,19 @@ class EmperorsTransport extends BaseShip{
         $this->addFrontSystem(new Thruster(6, 10, 0, 3, 1));
         $this->addFrontSystem(new Thruster(6, 10, 0, 3, 1));
 
-        $this->addAftSystem(new Thruster(5, 8, 0, 2, 2));
-        $this->addAftSystem(new Thruster(5, 8, 0, 3, 2));
-        $this->addAftSystem(new Thruster(5, 8, 0, 3, 2));
-        $this->addAftSystem(new Thruster(5, 8, 0, 2, 2));
 	$this->addAftSystem(new JumpEngine(6, 25, 3, 16));
+        $this->addAftSystem(new Thruster(5, 8, 0, 2, 2));
+        $this->addAftSystem(new Thruster(5, 8, 0, 3, 2));
+        $this->addAftSystem(new Thruster(5, 8, 0, 3, 2));
+        $this->addAftSystem(new Thruster(5, 8, 0, 2, 2));
         
+	$this->addLeftSystem(new TwinArray(3, 6, 2, 180, 60));
+	$this->addLeftSystem(new TwinArray(3, 6, 2, 180, 60));
 	$this->addLeftSystem(new Thruster(5, 15, 0, 5, 3));
-	$this->addLeftSystem(new TwinArray(3, 6, 2, 180, 60));
-	$this->addLeftSystem(new TwinArray(3, 6, 2, 180, 60));
 
+	$this->addRightSystem(new TwinArray(3, 6, 2, 300, 180));
+	$this->addRightSystem(new TwinArray(3, 6, 2, 300, 180));
 	$this->addRightSystem(new Thruster(5, 15, 0, 5, 4));
-	$this->addRightSystem(new TwinArray(3, 6, 2, 300, 180));
-	$this->addRightSystem(new TwinArray(3, 6, 2, 300, 180));
         
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
         $this->addFrontSystem(new Structure( 5, 42));
@@ -58,6 +59,46 @@ class EmperorsTransport extends BaseShip{
         $this->addLeftSystem(new Structure( 5, 56));
         $this->addRightSystem(new Structure( 5, 56));
         $this->addPrimarySystem(new Structure( 7, 40));
+	    
+	    
+
+	//d20 hit chart
+        $this->hitChart = array(
+            0=> array( //PRIMARY
+                    9 => "Structure",
+                    12 => "Scanner",
+                    15 => "Engine",
+                    17 => "Hangar",
+                    19 => "Reactor",
+                    20 => "C&C",
+            ),
+            1=> array( //Forward
+                    3 => "Thruster",
+                    7 => "Twin Array",
+                    18 => "Structure",
+                    20 => "Primary",
+            ),
+            2=> array( //Aft
+                    7 => "Thruster",
+                    12 => "Jump Engine",
+                    18 => "Structure",
+                    20 => "Primary",
+            ),
+            3=> array( //Port
+                    3 => "Thruster",
+                    8 => "Twin Array",
+                    18 => "Structure",
+                    20 => "Primary",
+            ),
+            4=> array( //Starboard
+                    3 => "Thruster",
+                    8 => "Twin Array",
+                    18 => "Structure",
+                    20 => "Primary",
+            )
+        );
+	    
+	    
     }
 }
 ?>
