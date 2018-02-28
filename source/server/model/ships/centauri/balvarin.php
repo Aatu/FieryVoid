6695cc807efdcb6513cb4ada358107673c11b826
Zero1627 <reisen@poczta.onet.pl>
2018-02-28 15:40:26 +0100

    hit chart

--- source/server/model/ships/centauri/balvarin.php
+++ source/server/model/ships/centauri/balvarin.php
@@ -11,7 +11,7 @@ class Balvarin extends BaseShip{
         $this->imagePath = "img/ships/balvarin.png";
         $this->shipClass = "Balvarin Carrier";
         $this->shipSizeClass = 3;
-        $this->fighters = array("medium"=>36);
+        $this->fighters = array("medium"=>36); //12 in each hangar
 	    $this->isd = 2192;
 
         $this->forwardDefense = 16;
@@ -38,17 +38,17 @@ class Balvarin extends BaseShip{
         $this->addAftSystem(new Thruster(5, 15, 0, 5, 2));
         $this->addAftSystem(new TwinArray(3, 6, 2, 90, 270));
 	
-	$this->addLeftSystem(new Thruster(5, 15, 0, 5, 3));
-	$this->addLeftSystem(new Thruster(5, 8, 0, 3, 1));
 	$this->addLeftSystem(new TwinArray(3, 6, 2, 180, 0));
 	$this->addLeftSystem(new GuardianArray(3, 4, 2, 180, 0));
         $this->addLeftSystem(new Hangar(5, 12));
+	$this->addLeftSystem(new Thruster(5, 15, 0, 5, 3));
+	$this->addLeftSystem(new Thruster(5, 8, 0, 3, 1));
 		
-	$this->addRightSystem(new Thruster(5, 15, 0, 5, 4));
-	$this->addRightSystem(new Thruster(5, 8, 0, 3, 1));
 	$this->addRightSystem(new TwinArray(3, 6, 2, 0, 180));
 	$this->addRightSystem(new GuardianArray(3, 4, 2, 0, 180));
         $this->addRightSystem(new Hangar(5, 12));
+	$this->addRightSystem(new Thruster(5, 15, 0, 5, 4));
+	$this->addRightSystem(new Thruster(5, 8, 0, 3, 1));
         
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
         $this->addFrontSystem(new Structure( 6, 40));
@@ -56,6 +56,47 @@ class Balvarin extends BaseShip{
         $this->addLeftSystem(new Structure( 5, 60));
         $this->addRightSystem(new Structure( 5, 60));
         $this->addPrimarySystem(new Structure( 7, 39));
+	    
+	    
+
+	//d20 hit chart
+        $this->hitChart = array(
+            0=> array( //PRIMARY
+                    8 => "Structure",
+                    10 => "Scanner",
+                    13 => "Engine",
+                    15 => "Jump Engine",
+                    17 => "Hangar",
+                    19 => "Reactor",
+                    20 => "C&C",
+            ),
+            1=> array( //Forward
+                    2 => "Thruster",
+                    8 => "Twin Array",
+                    18 => "Structure",
+                    20 => "Primary",
+            ),
+            2=> array( //Aft
+                    7 => "Thruster",
+                    18 => "Structure",
+                    20 => "Primary",
+            ),
+            3=> array( //Port
+                    3 => "Thruster",
+                    7 => "Battle Laser",
+                    11 => "Twin Array",
+                    18 => "Structure",
+                    20 => "Primary",
+            ),
+            4=> array( //Starboard
+                    3 => "Thruster",
+                    7 => "Battle Laser",
+                    11 => "Twin Array",
+                    18 => "Structure",
+                    20 => "Primary",
+            )
+        ); 
+	    
     }
 }
 ?>
