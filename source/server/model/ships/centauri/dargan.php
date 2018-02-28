d1f2ec0b8e5ff7aa7e3c4e9318269672372457b6
Zero1627 <reisen@poczta.onet.pl>
2018-02-28 13:47:59 +0100

    rearrange + hit chart

--- source/server/model/ships/centauri/dargan.php
+++ source/server/model/ships/centauri/dargan.php
@@ -12,6 +12,8 @@ class Dargan extends BaseShip{
         $this->shipSizeClass = 3;
         $this->fighters = array("Rutarian"=>12);
         $this->limited = 10;
+	    
+	    $this->isd = 2258;
         
         $this->forwardDefense = 15;
         $this->sideDefense = 16;
@@ -21,6 +23,9 @@ class Dargan extends BaseShip{
         $this->accelcost = 3;
         $this->rollcost = 2;
         $this->pivotcost = 3;
+	    
+	    $this->notes = 'Chameleon Sensors (no effect in game).'.
+	    $this->notes .= '<br>Rutarian-capable.'.
 		
         
          
@@ -28,30 +33,30 @@ class Dargan extends BaseShip{
         $this->addPrimarySystem(new CnC(7, 18, 0, 0));
         $this->addPrimarySystem(new ElintScanner(6, 20, 4, 10));
         $this->addPrimarySystem(new Engine(6, 20, 0, 12, 2));
-		$this->addPrimarySystem(new Hangar(6, 14));
+	$this->addPrimarySystem(new Hangar(6, 14));
         $this->addPrimarySystem(new TwinArray(4, 6, 2, 90, 270));
         
 		
-        $this->addFrontSystem(new Thruster(5, 10, 0, 3, 1));
-        $this->addFrontSystem(new Thruster(5, 10, 0, 3, 1));
         $this->addFrontSystem(new TwinArray(3, 6, 2, 180, 60));
         $this->addFrontSystem(new TwinArray(3, 6, 2, 180, 60));
-		$this->addFrontSystem(new TwinArray(3, 6, 2, 300, 180));
-		$this->addFrontSystem(new TwinArray(3, 6, 2, 300, 180));
+	$this->addFrontSystem(new TwinArray(3, 6, 2, 300, 180));
+	$this->addFrontSystem(new TwinArray(3, 6, 2, 300, 180));
         $this->addFrontSystem(new Mattercannon(4, 7, 4, 300, 60));
+        $this->addFrontSystem(new Thruster(5, 10, 0, 3, 1));
+        $this->addFrontSystem(new Thruster(5, 10, 0, 3, 1));
 
-        $this->addAftSystem(new Thruster(4, 8, 0, 3, 2));
-		$this->addAftSystem(new Thruster(5, 14, 0, 6, 2));
-        $this->addAftSystem(new Thruster(4, 8, 0, 3, 2));
         $this->addAftSystem(new JumpEngine(5, 20, 3, 16));
+        $this->addAftSystem(new Thruster(4, 8, 0, 3, 2));
+	$this->addAftSystem(new Thruster(5, 14, 0, 6, 2));
+        $this->addAftSystem(new Thruster(4, 8, 0, 3, 2));
         
-		$this->addLeftSystem(new Thruster(5, 15, 0, 5, 3));
-		$this->addLeftSystem(new BattleLaser(3, 6, 6, 240, 0));
-		$this->addLeftSystem(new Mattercannon(3, 7, 4, 240, 0));
+	$this->addLeftSystem(new BattleLaser(3, 6, 6, 240, 0));
+	$this->addLeftSystem(new Mattercannon(3, 7, 4, 240, 0));
+	$this->addLeftSystem(new Thruster(5, 15, 0, 5, 3));
 		
-		$this->addRightSystem(new Thruster(5, 15, 0, 5, 4));
-		$this->addRightSystem(new BattleLaser(3, 6, 6, 0, 120));
-		$this->addRightSystem(new Mattercannon(3, 7, 4, 0, 120));
+	$this->addRightSystem(new BattleLaser(3, 6, 6, 0, 120));
+	$this->addRightSystem(new Mattercannon(3, 7, 4, 0, 120));
+	$this->addRightSystem(new Thruster(5, 15, 0, 5, 4));
 		
 
         
@@ -62,5 +67,49 @@ class Dargan extends BaseShip{
         $this->addLeftSystem(new Structure( 5, 52));
         $this->addRightSystem(new Structure( 5, 52));
         $this->addPrimarySystem(new Structure( 7, 39));
+	    
+	    
+	    
+  
+	//d20 hit chart
+        $this->hitChart = array(
+            0=> array( //PRIMARY
+                    8 => "Structure",
+                    10 => "Twin Array",
+                    13 => "ELINT Scanner",
+                    15 => "Engine",
+                    17 => "Hangar",
+                    19 => "Reactor",
+                    20 => "C&C",
+            ),
+            1=> array( //Forward
+                    3 => "Thruster",
+                    5 => "Matter Cannon",
+                    9 => "Twin Array",
+                    18 => "Structure",
+                    20 => "Primary",
+            ),
+            2=> array( //Aft
+                    7 => "Thruster",
+                    12 => "Jump Engine",
+                    18 => "Structure",
+                    20 => "Primary",
+            ),
+            3=> array( //Port
+                    3 => "Thruster",
+                    6 => "Battle Laser",
+                    9 => "Matter Cannon",
+                    18 => "Structure",
+                    20 => "Primary",
+            ),
+            4=> array( //Starboard
+                    3 => "Thruster",
+                    6 => "Battle Laser",
+                    9 => "Matter Cannon",
+                    18 => "Structure",
+                    20 => "Primary",
+            )
+        );
+	    
     }
 }
