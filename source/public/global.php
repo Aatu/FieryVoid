2bf2cf2c40a65567b94424eff623e08a5c30c097
Aatu <aatu.riuttamaki@fraktio.fi>
2018-06-27 12:22:57 +0300

    Try to add session lifetime

--- source/public/global.php
+++ source/public/global.php
@@ -2,6 +2,8 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 require_once dirname(__DIR__) . '/autoload.php';
+ini_set('session.gc_maxlifetime', 20 * 24 * 3600);
+session_set_cookie_params(20 * 24 * 3600);
 session_start();
 require_once dirname(__DIR__) . '/server/varconfig.php' ;
 ?>
