af9661b88bc81fc4da4df526e3dcf1c48d9fe7fd
Aatu Riuttamäki <aatu.riuttamaki@gmail.com>
2018-07-03 00:15:55 +0300

    Change php ini settings

--- source/public/global.php
+++ source/public/global.php
@@ -2,8 +2,6 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 require_once dirname(__DIR__) . '/autoload.php';
-ini_set('session.gc_maxlifetime', 20 * 24 * 3600);
-session_set_cookie_params(20 * 24 * 3600);
 session_start();
 require_once dirname(__DIR__) . '/server/varconfig.php' ;
 ?>
