0c28a1fa7eb5d773ca1b09b67e02facc0d8ea37d
Aatu Riuttamäki <aatu.riuttamaki@gmail.com>
2018-07-09 17:39:08 +0300

    Define database host so that vagrant still works

--- source/server/varconfig.php
+++ source/server/varconfig.php
@@ -2,6 +2,7 @@
 $database_name='B5CGM';
 $database_user='aatu';
 $database_password='Kiiski';
+$database_host = 'localhost';
 //$database_user='root'; //
 //$database_password=''; 
 $secret_phrase='molecular pulsar'; 
