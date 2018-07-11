c405b6f983084a62c87a45cd030e3d6716c57951
Aatu Riuttamäki <aatu.riuttamaki@gmail.com>
2018-07-11 11:15:29 +0300

    Bring back unanimated starfield

--- source/public/client/renderer/terrain/StarField.js
+++ source/public/client/renderer/terrain/StarField.js
@@ -17,7 +17,6 @@ window.StarField = (function(){
 
     StarField.prototype.create = function()
     {
-        return;
         this.cleanUp();
 
         this.emitterContainer = new ParticleEmitterContainer(this.webglScene.scene, this.starCount, StarParticleEmitter);
@@ -61,12 +60,13 @@ window.StarField = (function(){
 
     StarField.prototype.render = function()
     {
-        return;
 
         if (! this.emitterContainer) {
             this.create();
         }
 
+        
+        return;
         var deltaTime = new Date().getTime() - this.lastAnimationTime;
         this.totalAnimationTime += deltaTime;
         this.emitterContainer.render(0, this.totalAnimationTime, 0, 0, this.zoomChanged);
