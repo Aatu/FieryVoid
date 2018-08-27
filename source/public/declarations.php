5769b4df225ab5bd5095418cedf97b1445eecd4c
Zero1627 <reisen@poczta.onet.pl>
2018-08-27 11:28:51 +0200

    declarations tab

--- source/public/declarations.php
+++ source/public/declarations.php
@@ -13,9 +13,9 @@ window showing current declarations (fire/EW)
 
 <div class="chatcontainer">
     <div class = "chatMessages"> <!-- buttons directing what's to be displayed -->
-      Side: <input type="button" value="Own" onclick="callOwn();"> <input type="button" value="Enemy" onclick="callEnemy();"> 
-      <br>Content: <input type="button" value="EW" onclick="callEW();"> <input type="button" value="Fire" onclick="callFire();"> 
-      <br>Display by: <input type="button" value="Source" onclick="callSource();"> <input type="button" value="Target" onclick="callTarget();"> 
+      Side: <input type="button" value="Own" onclick="declarations.callOwn();"> <input type="button" value="Enemy" onclick="declarations.callEnemy();"> 
+      <br>Content: <input type="button" value="EW" onclick="declarations.callEW();"> <input type="button" value="Fire" onclick="declarations.callFire();"> 
+      <br>Display by: <input type="button" value="Source" onclick="declarations.callSource();"> <input type="button" value="Target" onclick="declarations.callTarget();"> 
     </div>
   
     <div id="declarationsActual" class="chatMessages"> <!-- actual declarations tab, filled on button press -->
