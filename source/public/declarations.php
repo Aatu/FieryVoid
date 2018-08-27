2b569ebbf6c547c3e955954395002b91b2a7b67e
Zero1627 <reisen@poczta.onet.pl>
2018-08-27 08:02:51 +0200

    declarations

--- source/public/declarations.php
+++ source/public/declarations.php
@@ -1,1 +1,27 @@
+<?php 
+/*
+window showing current declarations (fire/EW)
+*/
+?>
 
+<link href="styles/chat.css" rel="stylesheet" type="text/css">
+
+
+<script>
+  
+  
+</script>
+
+
+
+<div class="chatcontainer">
+    <div class = "chatMessages"> <!-- buttons directing what's to be displayed -->
+      Side: <input type="button" value="Own" onclick="Declarations.callOwn();"> <input type="button" value="Enemy" onclick="Declarations.callEnemy();"> 
+      <br>Content: <input type="button" value="EW" onclick="Declarations.callEW();"> <input type="button" value="Fire" onclick="Declarations.callFire();"> 
+      <br>Display by: <input type="button" value="Source" onclick="Declarations.callSource();"> <input type="button" value="Target" onclick="Declarations.callTarget();"> 
+      <br>
+    </div>
+    <div id="declarationsActual" class="chatMessages"> <!-- actual declarations tab, filled on button press -->
+        
+    </div>
+</div>
