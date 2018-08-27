0678d42db4cb7170a3e1e845a383d620e5b8732f
Zero1627 <reisen@poczta.onet.pl>
2018-08-27 08:48:58 +0200

    declarations window

--- source/public/declarations.php
+++ source/public/declarations.php
@@ -8,7 +8,52 @@ window showing current declarations (fire/EW)
 
 
 <script>
-  
+  var GlobalSide = '';
+  var GlobalContent = '';
+  var GlobalDisplay = '';
+
+  //writes actual content to declarationsActual div
+  function fillDeclarationsActual() {
+    //fix data (if not done yet)
+    if(GlobalSide=='') GlobalSide = 'Own';
+    if(GlobalContent=='') GlobalContent = 'EW';
+    if(GlobalDisplay=='') GlobalDisplay = 'Source';
+    
+    //prepare data
+    
+    
+    //prepare text
+    
+    
+    //display text
+    
+
+  }
+
+  function callOwn() {
+    GlobalSide = 'Own';
+    fillDeclarationsActual();
+  }  
+  function callEnemy() {
+    GlobalSide = 'Enemy';
+    fillDeclarationsActual();
+  }    
+  function callEW() {
+    GlobalContent = 'EW';
+    fillDeclarationsActual();
+  }      
+  function callFire() {
+    GlobalContent = 'Fire';
+    fillDeclarationsActual();
+  }  
+  function callSource() {
+    GlobalDisplay = 'Source';
+    fillDeclarationsActual();
+  }    
+  function callTarget() {
+    GlobalDisplay = 'Target';
+    fillDeclarationsActual();
+  }    
   
 </script>
 
@@ -16,12 +61,12 @@ window showing current declarations (fire/EW)
 
 <div class="chatcontainer">
     <div class = "chatMessages"> <!-- buttons directing what's to be displayed -->
-      Side: <input type="button" value="Own" onclick="Declarations.callOwn();"> <input type="button" value="Enemy" onclick="Declarations.callEnemy();"> 
-      <br>Content: <input type="button" value="EW" onclick="Declarations.callEW();"> <input type="button" value="Fire" onclick="Declarations.callFire();"> 
-      <br>Display by: <input type="button" value="Source" onclick="Declarations.callSource();"> <input type="button" value="Target" onclick="Declarations.callTarget();"> 
-      <br>
+      Side: <input type="button" value="Own" onclick="callOwn();"> <input type="button" value="Enemy" onclick="callEnemy();"> 
+      <br>Content: <input type="button" value="EW" onclick="callEW();"> <input type="button" value="Fire" onclick="callFire();"> 
+      <br>Display by: <input type="button" value="Source" onclick="callSource();"> <input type="button" value="Target" onclick="callTarget();"> 
     </div>
+  
     <div id="declarationsActual" class="chatMessages"> <!-- actual declarations tab, filled on button press -->
-        
     </div>
+  
 </div>
