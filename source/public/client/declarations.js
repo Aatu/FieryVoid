1ea92d811c66339b24c3c727b08d497307cc6c64
Zero1627 <reisen@poczta.onet.pl>
2018-08-28 14:46:45 +0200

    declarations tab

--- source/public/client/declarations.js
+++ source/public/client/declarations.js
@@ -148,6 +148,147 @@ window.declarations = {
     return txt;
   }, //endof function readDeclarationsEW
   
+	
+ 
+  //reads appropriate Fire declarations into table
+  readDeclarationsFire: function readDeclarationsEW(){
+	function dispShipNew() {
+		this.id = -1;
+		this.name = "";
+		this.class = "";
+		this.value = "";
+	}
+	function dispFireNew() {
+		this.name = "";
+		this.oppName = "";
+		this.oppClass = "";
+		this.count = 0;
+		this.chance = "";
+	}
+	  
+    var dispShips = new Array(); 
+    var dispShip = new dispShipNew();
+    var dispEWEntry = new dispEWNew();
+    for (var i in gamedata.ships){
+      var ship = gamedata.ships[i];
+      if( (declarations.GlobalSide=='Own' && declarations.GlobalDisplay=='Source' && gamedata.isMyShip(ship)) //own ship, own ew, by source
+        || (declarations.GlobalSide!='Own' && declarations.GlobalDisplay=='Source' && !gamedata.isMyShip(ship)) //enemy ship, enemy EW, by source
+        || (declarations.GlobalSide=='Own' && declarations.GlobalDisplay!='Source' && !gamedata.isMyShip(ship)) //enemy ship, own ew, by target
+        || (declarations.GlobalSide!='Own' && declarations.GlobalDisplay!='Source' && gamedata.isMyShip(ship)) //own ship, enemy EW, by target
+      ){
+	dispShip = new dispShipNew();
+        dispShip.id = ship.id;
+        dispShip.name = ship.name;
+        dispShip.class = ship.shipClass;
+        dispShip.value = ship.pointCost;
+        dispShip.EW = new Array();
+        //now all EW entries...either own or incoming!
+        if (ship.flight){//for fighters, show jinking in all circumstances
+	  dispEWEntry = new dispEWNew();	
+          dispEWEntry.name = 'jinking';
+          dispEWEntry.targetName = '';
+          dispEWEntry.targetClass = '';
+          dispEWEntry.value = shipManager.movement.getJinking(ship);
+          dispShip.EW.push(dispEWEntry);
+        }else{//for ships, show DEW in all circumstances
+          dispEWEntry = new dispEWNew();	
+          dispEWEntry.name = 'DEW';
+          dispEWEntry.targetName = '';
+          dispEWEntry.targetClass = '';
+          dispEWEntry.value = ew.getDefensiveEW(ship);
+          dispShip.EW.push(dispEWEntry);
+        }
+        if(declarations.GlobalDisplay=='Source'){ //by source - display EW dished out by self!
+          if (!ship.flight){ //fighters do not emit any EW            
+            for (var e in ship.EW) {
+              var EWentry = ship.EW[e];
+	      if (EWentry.type != 'DEW'){ //DEW already listed
+		      dispEWEntry = new dispEWNew();		    
+		      dispEWEntry.name = EWentry.type;
+		      dispEWEntry.value = EWentry.amount;
+		      if (EWentry.targetid>0){
+			var targetUnit = gamedata.getShip(EWentry.targetid);
+			dispEWEntry.targetName = targetUnit.name;
+			dispEWEntry.targetClass = targetUnit.shipClass;
+		      }else{//targetless EW                
+			dispEWEntry.targetName = '';
+			dispEWEntry.targetClass = '';
+		      }              
+		      dispShip.EW.push(dispEWEntry);
+	      }
+            }
+          }
+        }else{ //by target - display EW dished out at self BY OPPONENT! (for fighters - CCEW)
+          for (var j in gamedata.ships){
+            var srcShip = gamedata.ships[j]; 
+            if (srcShip.team != ship.team){ //enemy ships only
+              for (var e in srcShip.EW) {
+                var EWentry = srcShip.EW[e];
+                if (EWentry.targetID == ship.id //self is target
+                  || (ship.flight && EWentry.type == 'CCEW') //self is fighter and EWentry is CCEW
+                ){
+		  dispEWEntry = new dispEWNew();	 	
+                  dispEWEntry.name = EWentry.type;
+                  dispEWEntry.value = EWentry.amount;
+                  dispEWEntry.targetName = srcShip.name; //source, in this case
+                  dispEWEntry.targetClass = srcShip.shipClass;
+		  dispShip.EW.push(dispEWEntry);
+                }
+              }
+            }
+          }
+        }
+        dispShips.push(dispShip);
+      }
+    }    
+      //sort ships by value
+      dispShips.sort(function(a, b){
+	if (a.flight && !b.flight){//fighters always after ships
+		return -1;
+	      }else if (!a.flight && b.flight){
+		return 1;
+	      }else if (a.value > b.value){ //more valuable units first
+		return 1;
+	      }else if (a.value < b.value){
+		return -1;
+	      }
+	      else return 0;
+        });
+    
+    var txt = '';
+    //change to text
+    for (var i in dispShips){
+      var shpEntry = dispShips[i];
+      txt += '<big><b>' + shpEntry.name + ' <i>(' + shpEntry.class + ')</i> ' + '</b></big>';
+      //reset EW button - for own ships
+	/* ...maybe that won't be necessary...
+      if ( gamedata.gamephase == 1 && declarations.GlobalSide=='Own' && declarations.GlobalDisplay=='Source') {//Initial phase, displaying own ships
+        if (shpEntry.EW.count > 1 ){ //has something besides DEW!
+          txt += '<input type="button" value="Reset EW" onclick="declarations.doResetEW(' + shpEntry.id + ');">';
+        }
+      }
+      */
+      txt += '<br>';
+      //now actual entries
+      for(var e in shpEntry.EW){
+      	var EWentry = shpEntry.EW[e];
+	txt += ' -- ' + EWentry.name + ' <b>' + EWentry.value + '</b>';
+	if (EWentry.targetName != ''){
+	  if (declarations.GlobalDisplay=='Source'){
+	    txt += ' at '  ;
+	  }else{
+	    txt += ' by ';
+	  }
+          txt += EWentry.targetName + ' <i>('+ EWentry.targetClass +')</i>';
+	}
+	txt += '<br>';
+      }
+    }
+    
+    return txt;
+  }, //endof function readDeclarationsFire
+	
+	
   
   //writes actual content to declarationsActual div
   fillDeclarationsActual: function fillDeclarationsActual() {
@@ -161,7 +302,7 @@ window.declarations = {
     if(declarations.GlobalContent=='EW'){ //display EW declarations
       srcData = declarations.readDeclarationsEW();
     }else{ //display fire declarations
-      
+      srcData = declarations.readDeclarationsFire();
     }
     
     //prepare text
