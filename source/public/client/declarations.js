0fa2d19732465a2fc0a24ba92c2ce82cf4fc16eb
Zero1627 <reisen@poczta.onet.pl>
2018-08-29 12:15:47 +0200

    declarations tab

--- source/public/client/declarations.js
+++ source/public/client/declarations.js
@@ -164,6 +164,8 @@ window.declarations = {
 		this.wpnName = "";
 		this.oppName = "";
 		this.oppClass = "";
+		this.oppId = -1;
+		this.calledid = -1;
 		this.count = 0;
 		this.chanceMin = 1000;
 		this.chanceMax = -1000;
@@ -196,38 +198,50 @@ window.declarations = {
 	      var actSys = systemsTab[actSysNo];
 	      if (actSys.fireOrders.length > 0){
 		for (var fireNo = 0; fireNo < actSys.fireOrders.length; fireNo++){
+		  var weapon = ship.systems[sysNo];
 		  var order = ship.systems[sysNo].fireOrders[fireNo];
 		  if (order.type.indexOf('intercept') !== -1){ //this is actual offensive fire!
 		    var dispFireEntry = new dispFireNew();
-			  
-			  
-			  
+		    dispFireEntry.wpnName = weapon.displayName + ' ('+ weapon.firingModes[order.firingMode] +')';
+	            if (order.calledid > -1 ){
+		      dispFireEntry.wpnName += ' CALLED';
+		      dispFireEntry.calledid = order.calledid;
+		    }
+		    dispFireEntry.oppId = order.targetid;
+	            //if such order exists, on list, find it; else fill basic data and add to list
+	            var alreadyExists = false;
+	            for (var existingEntry in dispShip.fire){
+		      var extEntry = dispShip.fire[existingEntry];
+		      if ( extEntry.wpnName == dispFireEntry.wpnName && extEntry.oppId == dispFireEntry.oppId ){
+			dispFireEntry = extEntry;    
+			alreadyExists = true;
+		      }
+		    }
+	            var targetUnit;
+	            if (dispFireEntry.oppId > -1){
+		      targetUnit = gamedata.getShip(dispFireEntry.oppId);
+		    }
+	            if(!alreadyExists){ //fill initial data
+		      if (dispFireEntry.oppId > -1){
+			dispFireEntry.oppName = targetUnit.name; 
+                  	dispFireEntry.oppClass = targetUnit.shipClass;
+		      }
+		      dispFireEntry.calledid = order.calledid;
+		      dispShip.fire.push(dispFireEntry);
+		    }
+	            dispFireEntry.count++;
+	            if(dispFireEntry.oppId > -1){ //fire at actual target
+		      var toHit = weaponManager.calculateHitChange(dispShip, targetUnit, weapon, order.calledid);
+		      if (toHit < dispFireEntry.chanceMin) dispFireEntry.chanceMin = toHit;
+		      if (toHit > dispFireEntry.chanceMax) dispFireEntry.chanceMax = toHit;
+		    }			  
 		  }
-		}      
-		      
-		    
+		}    
 	      }
 	    }
-	  }
-		
-            for (var e in ship.EW) {
-              var EWentry = ship.EW[e];
-	      if (EWentry.type != 'DEW'){ //DEW already listed
-		      dispEWEntry = new dispEWNew();		    
-		      dispEWEntry.name = EWentry.type;
-		      dispEWEntry.value = EWentry.amount;
-		      if (EWentry.targetid>0){
-			var targetUnit = gamedata.getShip(EWentry.targetid);
-			dispEWEntry.targetName = targetUnit.name;
-			dispEWEntry.targetClass = targetUnit.shipClass;
-		      }else{//targetless EW                
-			dispEWEntry.targetName = '';
-			dispEWEntry.targetClass = '';
-		      }              
-		      dispShip.EW.push(dispEWEntry);
-	      }
-            }
+	  }		
         }else{ //by target - display EW dished out at self BY OPPONENT! (for fighters - CCEW)
+		/*
           for (var j in gamedata.ships){
             var srcShip = gamedata.ships[j]; 
             if (srcShip.team != ship.team){ //enemy ships only
@@ -246,6 +260,7 @@ window.declarations = {
               }
             }
           }
+	  */
         }
         dispShips.push(dispShip);
       }
@@ -269,31 +284,29 @@ window.declarations = {
     for (var i in dispShips){
       var shpEntry = dispShips[i];
       txt += '<big><b>' + shpEntry.name + ' <i>(' + shpEntry.class + ')</i> ' + '</b></big>';
-      //reset EW button - for own ships
-	/* ...maybe that won't be necessary...
-      if ( gamedata.gamephase == 1 && declarations.GlobalSide=='Own' && declarations.GlobalDisplay=='Source') {//Initial phase, displaying own ships
-        if (shpEntry.EW.count > 1 ){ //has something besides DEW!
-          txt += '<input type="button" value="Reset EW" onclick="declarations.doResetEW(' + shpEntry.id + ');">';
-        }
-      }
-      */
       txt += '<br>';
       //now actual entries
-      for(var e in shpEntry.EW){
-      	var EWentry = shpEntry.EW[e];
-	txt += ' -- ' + EWentry.name + ' <b>' + EWentry.value + '</b>';
-	if (EWentry.targetName != ''){
-	  if (declarations.GlobalDisplay=='Source'){
-	    txt += ' at '  ;
-	  }else{
-	    txt += ' by ';
+      for(var f in shpEntry.fire){
+      	var salvo = shpEntry.fire[f];
+	txt += ' -- ' + salvo.count + 'x <b>' + salvo.wpnName + '</b>';
+	if (declarations.GlobalDisplay=='Source'){
+	  txt += ' at '  ;
+	}else{
+	  txt += ' by ';
+	}
+	if (salvo.oppName != ''){	  
+          txt += salvo.oppName + ' <i>('+ salvo.targetClass +')</i>';
+	  txt += ', ' + salvo.chanceMin;
+	  if (salvo.chanceMax > salvo.chanceMin){
+	     txt += '..' + salvo.chanceMax;
 	  }
-          txt += EWentry.targetName + ' <i>('+ EWentry.targetClass +')</i>';
+	  txt += '%';
+	}else{
+	  txt += 'hex';	
 	}
 	txt += '<br>';
       }
     }
-    
     return txt;
   }, //endof function readDeclarationsFire
 	
