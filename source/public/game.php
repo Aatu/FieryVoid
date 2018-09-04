0eb79b986e63e8fdd5567b7dd0fce0b0919a8598
Aatu <aatu.riuttamaki@fraktio.fi>
2018-09-04 13:08:23 +0300

    Enable background images

--- source/public/game.php
+++ source/public/game.php
@@ -520,7 +520,7 @@
 <div id="playerSettings"></div>
 <div id="shipThrust"></div>
 <div id="pagecontainer" oncontextmenu="return false;">
-    <div id="background" style="background-image:url(img/maps/<?php /* print($serverdata ? $serverdata->background : ""); */ ?>)"></div>
+    <div id="background" style="background-image:url(img/maps/<?php print($serverdata ? $serverdata->background : ""); ?>)"></div>
     <div id="webgl" class="tacticalcanvas"></div>
 
 
