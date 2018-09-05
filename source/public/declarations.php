d1f9f2374e420d4f3f4dc57d47b269f6869975a7
Zero1627 <reisen@poczta.onet.pl>
2018-09-05 14:05:45 +0200

    Update declarations.php

--- source/public/declarations.php
+++ source/public/declarations.php
@@ -12,7 +12,7 @@ window showing current declarations (fire/EW)
 
 
 <div class="chatcontainer">
-    <div class = "chatMessages"> <!-- buttons directing what's to be displayed -->
+    <div id="declarationsButtons" class = "chatMessages"> <!-- buttons directing what's to be displayed -->
       Side: <input type="button" value="Own" onclick="window.declarations.callOwn();"> <input type="button" value="Enemy" onclick="declarations.callEnemy();"> 
       <br>Content: <input type="button" value="EW" onclick="declarations.callEW();"> <input type="button" value="Fire" onclick="declarations.callFire();"> 
       <br>Display by: <input type="button" value="Source" onclick="declarations.callSource();"> <input type="button" value="Target" onclick="declarations.callTarget();"> 
