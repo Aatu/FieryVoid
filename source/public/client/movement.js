b52f6b995c7a4e06181840ea9bc613b53a87ac81
Zero1627 <reisen@poczta.onet.pl>
2018-10-22 14:55:44 +0200

    getPositionAtStartOfTurn

--- source/public/client/movement.js
+++ source/public/client/movement.js
@@ -1212,7 +1212,7 @@ shipManager.movement = {
         //replacement:
         for (var i = ship.movement.length - 1; i >= 0; i--) {
             var move = ship.movement[i];
-            if (move.turn != currentTurn) { //first move from earlier turn! this is what we need!
+            if (move.turn < currentTurn) { //first move from earlier turn! this is what we need!
                 break; //get out of loop
             } //if such a move is not found, first move of current turn would do - should be turn 1 and deployment move
         }        
