4689387c687ebf5441658e8a807b938026e077d1
Zero1627 <reisen@poczta.onet.pl>
2018-12-18 12:47:48 +0100

    ship creation

--- source/server/model/ships/streib/adjudicator.php
+++ source/server/model/ships/streib/adjudicator.php
@@ -37,26 +37,26 @@ class Adjudicator extends BaseShip{
         
         
         $this->addAftSystem(new Engine(9, 11, 0, 9, 2)); 
+        $this->addAftSystem(new JumpEngine(9, 10, 4, 10));
+        $this->addAftSystem(new Thruster(9, 3, 0, 2, 2));
         $this->addAftSystem(new Thruster(9, 8, 0, 7, 2));
-        $this->addAftSystem(new Thruster(9, 8, 0, 7, 2));
-        $this->addAftSystem(new MediumBurstBeam(6, 7, 6, 60, 240));
         $this->addAftSystem(new ImprovedBlastLaser(7, 10, 8, 120, 240));
-        $this->addAftSystem(new MediumBurstBeam(6, 7, 6, 120, 300));
+        $this->addAftSystem(new DualBurstBeam(6, 8, 5, 90, 270));
         
         $this->addLeftSystem(new CnC(9, 12, 0, 0));
-        $this->addLeftSystem(new Thruster(3, 13, 0, 5, 4));
-        $this->addLeftSystem(new CargoBay(9, 9));
-        $this->addLeftSystem(new ImprovedBlastLaser(7, 10, 8, 300, 60));
-        $this->addLeftSystem(new HeavyBurstBeam(7, 9, 8, 300, 60));
-        $this->addLeftSystem(new DualBurstBeam(5, 8, 5, 180, 60));
-        $this->addLeftSystem(new EMWaveDisruptor(6, 8, 5, 180, 60));
-        
-        $this->addRightSystem(new JumpEngine(9, 10, 4, 10));
-        $this->addRightSystem(new Thruster(3, 13, 0, 5, 3));
+        $this->addLeftSystem(new Thruster(8, 10, 0, 5, 4));
+        $this->addLeftSystem(new ImprovedBlastLaser(7, 10, 8, 240, 0));
+        $this->addLeftSystem(new BurstPulseCannon(6, 6, 7, 180, 360));
+        $this->addLeftSystem(new SurgeLaser(6, 0, 0, 120, 360));
+	            
+        $this->addRightSystem(new Thruster(8, 10, 0, 5, 3));
         $this->addRightSystem(new Scanner(8, 8, 5, 12));
-        $this->addRightSystem(new CargoBay(9, 9));
-        $this->addRightSystem(new DualBurstBeam(5, 8, 5, 300, 180));
-        $this->addRightSystem(new EMWaveDisruptor(6, 8, 4, 300, 180));
+        $this->addRightSystem(new ImprovedBlastLaser(7, 10, 8, 120, 240));
+        $this->addRightSystem(new SurgeLaser(6, 0, 0, 300, 120));
+        $this->addRightSystem(new SurgeLaser(6, 0, 0, 60, 240));
+        $this->addRightSystem(new EMWaveDisruptor(6, 8, 5, 0, 180));
+	    
+	    
         
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
         $this->addFrontSystem(new Structure(9, 40));
@@ -64,46 +64,46 @@ class Adjudicator extends BaseShip{
         $this->addLeftSystem(new Structure(9, 40));
         $this->addRightSystem(new Structure(9, 40));
         $this->addPrimarySystem(new Structure(9, 40));
+	    
         $this->hitChart = array(
             0 => array(
-                12 => "Structure",
-                14 => "Tractor Beam",
-                17 => "Engine",
+                13 => "Structure",
+                15 => "Tractor Beam",
+                17 => "Hangar",
                 20 => "Reactor",
             ),
             1 => array(
                 3 => "Thruster",
-                7 => "Medium Burst Beam",
-                9 => "Burst Pulse Cannon",
+                7 => "Improved Blast Laser",
+                9 => "Medium Burst Beam",
+                10 => "EM-Wave Disruptor",
                 18 => "Structure",
                 20 => "Primary",
             ),
             2 => array(
-                6 => "Thruster",
-                8 => "Improved Blast Laser",
-                10 => "Medium Burst Beam",
+                5 => "Thruster",
+                7 => "Improved Blast Laser",
+                8 => "Dual Burst Beam",
+                10 => "Engine",
+                12 => "Jump Engine",
                 18 => "Structure",
                 20 => "Primary",
             ),
             3 => array(
                 3 => "Thruster",
                 5 => "Improved Blast Laser",
-                7 => "Heavy Burst Beam",
-                9 => "Dual Burst Beam",
-                10 => "EM-Wave Disruptor",
-                11 => "Cargo Bay",
-                13 => "Jump Engine",
+                7 => "Burst Pulse Cannon",
+                8 => "Surge Laser",
+                9 => "C&C",
                 18 => "Structure",
                 20 => "Primary",
             ),
             4 => array(
                 3 => "Thruster",
-                5 => "Dual Burst Beam",
-                7 => "EM-Wave Disruptor",
-                9 => "Scanner",
-                10 => "C&C",
-                11 => "Hangar",
-                12 => "Cargo Bay",
+                5 => "Improved Blast Laser",
+                7 => "Surge Laser",
+                9 => "EM-Wave Disruptor",
+                10 => "Scanner",
                 18 => "Structure",
                 20 => "Primary",
             ),
