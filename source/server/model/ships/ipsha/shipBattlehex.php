a1b7fa4ff5b007e698ab2c8164874d79cbfd7353
Zero1627 <reisen@poczta.onet.pl>
2019-07-04 08:30:24 +0200

    visual arc layout

--- source/server/model/ships/ipsha/shipBattlehex.php
+++ source/server/model/ships/ipsha/shipBattlehex.php
@@ -52,20 +52,20 @@ class ShipBattlehex extends HeavyCombatVesselLeftRight{
         $this->addPrimarySystem(new MagGraviticThruster(4, 11, 0, 99, 2));	    
 
 	    
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));
-	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));	    
+	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 120, 240));	
+	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
+	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));
+	$this->addLeftSystem(new SurgeCannon(3, 0, 0, 300, 60));    
 	$this->addLeftSystem(new MagGraviticThruster(3, 13, 0, 99, 3));
 		
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
-	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
 	$this->addRightSystem(new SurgeCannon(3, 0, 0, 120, 240));
+	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
+	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
+	$this->addRightSystem(new SurgeCannon(3, 0, 0, 300, 60));
         $this->addRightSystem(new MagGraviticThruster(3, 13, 0, 99, 4));
 		
 		
