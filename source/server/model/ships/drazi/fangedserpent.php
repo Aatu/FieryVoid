24691b6fa9f40a09e1cfaf89447012d8d07480f5
Zero1627 <reisen@poczta.onet.pl>
2019-09-11 11:48:57 +0200

    Update fangedserpent.php

--- source/server/model/ships/drazi/fangedserpent.php
+++ source/server/model/ships/drazi/fangedserpent.php
@@ -34,8 +34,8 @@ class FangedSerpent extends SuperHeavyFighter{
         $fighter->addFrontSystem(new PairedParticleGun(330, 30, 5));
         $fighter->addFrontSystem(new FighterMissileRack(6, 330, 30));
         $fighter->addFrontSystem(new FighterMissileRack(6, 330, 30));
-
-        $particleBlaster = new ParticleBlasterFtr(0, 330, 30);
+        
+        $particleBlaster = new ParticleBlasterFtr(330, 30, 1); //$startArc, $endArc, $nrOfShots
         $fighter->addFrontSystem($particleBlaster);
         
         $this->addSystem($fighter);
