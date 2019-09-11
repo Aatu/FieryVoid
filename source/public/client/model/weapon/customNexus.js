0471f481e68235eef443450bed67ee6bfb5551aa
Zero1627 <reisen@poczta.onet.pl>
2019-09-11 16:50:22 +0200

    Chaff Launcher

--- source/public/client/model/weapon/customNexus.js
+++ source/public/client/model/weapon/customNexus.js
@@ -4,3 +4,9 @@ var NexusKineticBoxLauncher = function NexusKineticBoxLauncher(json, ship) {
 NexusKineticBoxLauncher.prototype = Object.create(Weapon.prototype);
 NexusKineticBoxLauncher.prototype.constructor = NexusKineticBoxLauncher;
 
+
+var NexusChaffLauncher = function NexusChaffLauncher(json, ship) {
+    Weapon.call(this, json, ship);
+};
+NexusChaffLauncher.prototype = Object.create(Weapon.prototype);
+NexusChaffLauncher.prototype.constructor = NexusChaffLauncher;
