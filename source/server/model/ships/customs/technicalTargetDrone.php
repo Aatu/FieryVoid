60a8249b1e4b07d4cf148704f46a2ec511b7771d
Zero1627 <reisen@poczta.onet.pl>
2019-09-11 10:34:47 +0200

    Update technicalTargetDrone.php

--- source/server/model/ships/customs/technicalTargetDrone.php
+++ source/server/model/ships/customs/technicalTargetDrone.php
@@ -58,6 +58,8 @@ class technicalTargetDrone extends BaseShip{
 		$this->addFrontSystem(new CustomBPALight(3, 0, 0, 240, 120));
 		$this->addFrontSystem(new CustomBPAMedium(3, 0, 0, 240, 120));
 		$this->addFrontSystem(new CustomBPAHeavy(3, 0, 0, 240, 120));
+		$this->addFrontSystem(new NexusKineticBoxLauncher(1, 0, 0, 270, 180));
+		$this->addFrontSystem(new NexusKineticBoxLauncher(1, 0, 0, 270, 180));
 		
 		$this->addAftSystem(new CommDisruptor(3, 0, 0, 0, 360));
 		$this->addAftSystem(new CommJammer(3, 0, 0, 0, 360));		
