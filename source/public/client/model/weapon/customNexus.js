77343db603baee3f9bcd2a7456a458b6cf1cabb6
Zero1627 <reisen@poczta.onet.pl>
2019-09-11 10:29:41 +0200

    Update customNexus.js

--- source/public/client/model/weapon/customNexus.js
+++ source/public/client/model/weapon/customNexus.js
@@ -1,1 +1,6 @@
+var NexusKineticBoxLauncher = function NexusKineticBoxLauncher(json, ship) {
+    Weapon.call(this, json, ship);
+};
+NexusKineticBoxLauncher.prototype = Object.create(Weapon.prototype);
+NexusKineticBoxLauncher.prototype.constructor = NexusKineticBoxLauncher;
 
