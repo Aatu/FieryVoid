1517ca2f7b459beab1e13bd04dfdc2214bb02b77
Zero1627 <reisen@poczta.onet.pl>
2019-09-12 13:03:36 +0200

    Update BrixadiiPursuitFrigate2108.php

--- source/server/model/ships/ZNexusBrixadii/BrixadiiPursuitFrigate2108.php
+++ source/server/model/ships/ZNexusBrixadii/BrixadiiPursuitFrigate2108.php
@@ -4,26 +4,26 @@ class BrixadiiPursuitFrigate2108 extends HeavyCombatVessel{
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-        $this->pointCost = 335;
+        $this->pointCost = 320;
         $this->faction = "ZNexus Brixadii";
-        $this->phpclass = "PursuitFrigate2108";
-        $this->imagePath = "img/ships/pursuitFrigateTemp.png";
+        $this->phpclass = "BrixadiiPursuitFrigate2108";
+        $this->imagePath = "img/ships/Nexus/PursuitFrigate.png";
         $this->shipClass = "Pursuit Frigate (2108)";
-			$this->variantOf = "Pursuit Frigate (1989)";
-			$this->occurence = "common";
-			$this->unofficial = true;
-        $this->isd = 2108;
+		$this->variantOf = "Pursuit Frigate";
+		$this->occurence = "common";
+		$this->unofficial = true;
+       		$this->isd = 2108;
         
         $this->forwardDefense = 10;
         $this->sideDefense = 12;
         
-		$this->agile = true;
+	$this->agile = true;
         $this->turncost = 0.33;
         $this->turndelaycost = 0.50;
         $this->accelcost = 1;
         $this->rollcost = 1;
         $this->pivotcost = 1;
-        $this->iniativebonus = 60;
+        $this->iniativebonus = 12*5;
         
          
         $this->addPrimarySystem(new Reactor(4, 9, 0, 0));
@@ -35,24 +35,24 @@ class BrixadiiPursuitFrigate2108 extends HeavyCombatVessel{
         $this->addPrimarySystem(new Thruster(2, 14, 0, 4, 4));
       
         $this->addFrontSystem(new Thruster(3, 14, 0, 6, 1));
-		$this->addFrontSystem(new ParticleProjector(2, 6, 1, 180, 60));
-		$this->addFrontSystem(new ParticleProjector(2, 6, 1, 270, 90));
-		$this->addFrontSystem(new ParticleProjector(2, 6, 1, 270, 90));
-		$this->addFrontSystem(new ParticleProjector(2, 6, 1, 300, 180));
-		$this->addFrontSystem(new KineticBoxLauncher(2, 4, 0, 300, 60));
+	$this->addFrontSystem(new ParticleProjector(2, 6, 1, 180, 60));
+	$this->addFrontSystem(new ParticleProjector(2, 6, 1, 270, 90));
+	$this->addFrontSystem(new ParticleProjector(2, 6, 1, 270, 90));
+	$this->addFrontSystem(new ParticleProjector(2, 6, 1, 300, 180));
+	$this->addFrontSystem(new NexusKineticBoxLauncher(2, 4, 0, 300, 60));
                 
         $this->addAftSystem(new Thruster(3, 10, 0, 4, 2));
         $this->addAftSystem(new Thruster(3, 10, 0, 4, 2));
-        $this->addAftSystem(new LightParticleProjector(1, 3, 1, 120, 0));
-        $this->addAftSystem(new LightParticleProjector(1, 3, 1, 0, 240));
-		$this->addAftSystem(new ChaffLauncher(2, 2, 1, 0, 360));
+        $this->addAftSystem(new NexusParticleProjectorLight(1, 0, 0, 120, 0));
+        $this->addAftSystem(new NexusParticleProjectorLight(1, 0, 0, 0, 240));
+	$this->addAftSystem(new NexusChaffLauncher(2, 0, 0, 0, 360));
         
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
         $this->addPrimarySystem(new Structure( 3, 40));
         $this->hitChart = array(
             0=> array(
                     9 => "Thruster",
-					10 => "Hangar",
+		    10 => "Hangar",
                     13 => "Scanner",
                     17 => "Engine",
                     19 => "Reactor",
@@ -60,14 +60,14 @@ class BrixadiiPursuitFrigate2108 extends HeavyCombatVessel{
             ),
             1=> array(
                     5 => "Thruster",
-					6 => "Kinetic Box Launcher",
+		    6 => "Kinetic Box Launcher",
                     10 => "Particle Projector",
-					17 => "Structure",
+		    17 => "Structure",
                     20 => "Primary",
             ),
             2=> array(
                     7 => "Thruster",
-					8 => "Chaff Launcher",
+		    8 => "Chaff Launcher",
                     10 => "Light Particle Projector",
                     17 => "Structure",
                     20 => "Primary",
