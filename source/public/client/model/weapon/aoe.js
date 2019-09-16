c9d9f68785b51358f0a9f50713830dc3a9547e72
Zero1627 <reisen@poczta.onet.pl>
2019-09-16 13:12:24 +0200

    Update aoe.js

--- source/public/client/model/weapon/aoe.js
+++ source/public/client/model/weapon/aoe.js
@@ -8,7 +8,6 @@ Aoe.prototype.constructor = Aoe;
 
 var EnergyMine = function EnergyMine(json, ship) {
     Aoe.call(this, json, ship);
-    this.targetsShips = false;
 };
 EnergyMine.prototype = Object.create(Aoe.prototype);
 EnergyMine.prototype.constructor = EnergyMine;
