8af4680f111c935cfc3871f130e285ff86dd4432
Zero1627 <reisen@poczta.onet.pl>
2019-09-24 11:43:47 +0200

    Update wlcChlonasEsKashiDD2198.php

--- source/server/model/ships/ZCustomChLonas/wlcChlonasEsKashiDD2198.php
+++ source/server/model/ships/ZCustomChLonas/wlcChlonasEsKashiDD2198.php
@@ -7,11 +7,11 @@ class wlcChlonasEsKashiDD2198 extends HeavyCombatVesselLeftRight{
         $this->pointCost = 400;
         $this->phpclass = "wlcChlonasEsKashiDD2198";
         $this->imagePath = "img/ships/kraasus.png";
-        $this->shipClass = "Es'Kashi Destroyer (2198)";
+        $this->shipClass = "Es'Kashi Destroyer";
         //$this->fighters = array("medium"=>6);
         
 	$this->faction = "Ch'Lonas";
-        $this->variantOf = "Es'Kashi Destroyer";
+        //$this->variantOf = "Es'Kashi Destroyer";
 	$this->isd = 2198;
 	$this->unofficial = true;
 	    
