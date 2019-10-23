38b485cfd7aebfce011526a13b3a2d993b4f184a
Zero1627 <reisen@poczta.onet.pl>
2019-10-23 14:09:09 +0200

    Update kendari.php

--- source/server/model/ships/centauri_old/kendari.php
+++ source/server/model/ships/centauri_old/kendari.php
@@ -4,7 +4,7 @@ class Kendari extends BaseShip{
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-		$this->pointCost = 540;
+		$this->pointCost = 500;
         $this->faction = "Centauri (WotCR)";
         $this->phpclass = "Kendari";
         $this->imagePath = "img/ships/kendari.png";
@@ -12,6 +12,7 @@ class Kendari extends BaseShip{
         $this->shipSizeClass = 3;
         $this->fighters = array("normal"=>6);
         $this->limited = 33;
+	    $this->isd = 1912;
         
         $this->forwardDefense = 15;
         $this->sideDefense = 16;
@@ -24,15 +25,14 @@ class Kendari extends BaseShip{
          
         $this->addPrimarySystem(new Reactor(6, 14, 0, 0));
         $this->addPrimarySystem(new CnC(6, 16, 0, 0));
-        $this->addPrimarySystem(new ElintScanner(5, 20, 4, 10));
+        $this->addPrimarySystem(new ElintScanner(5, 25, 4, 9));
         $this->addPrimarySystem(new Engine(5, 18, 0, 8, 3));
-		$this->addPrimarySystem(new Hangar(4, 8));      
+	$this->addPrimarySystem(new Hangar(4, 8));      
 		
-        $this->addFrontSystem(new Thruster(4, 8, 0, 3, 1));
-        $this->addFrontSystem(new Thruster(4, 8, 0, 3, 1));        
-
-        $this->addFrontSystem(new LightParticleBeamShip(2, 2, 1, 240, 120));
-        $this->addFrontSystem(new LightParticleBeamShip(2, 2, 1, 240, 120));
+        $this->addFrontSystem(new Thruster(3, 8, 0, 3, 1));
+        $this->addFrontSystem(new Thruster(3, 8, 0, 3, 1)); 
+        $this->addFrontSystem(new Particle Projector(2, 6, 1, 270, 90));
+        $this->addFrontSystem(new Particle Projector(2, 6, 1, 270, 90));
         $this->addFrontSystem(new SentinelPointDefense(1, 4, 1, 240, 60));
         $this->addFrontSystem(new SentinelPointDefense(1, 4, 1, 300, 120));
 
@@ -41,19 +41,59 @@ class Kendari extends BaseShip{
         $this->addAftSystem(new JumpEngine(5, 25, 3, 20));
         $this->addAftSystem(new SentinelPointDefense(1, 4, 1, 120, 240));
         
-		$this->addLeftSystem(new Thruster(4, 15, 0, 5, 3));
-        $this->addLeftSystem(new LightParticleBeamShip(2, 2, 1, 180, 360));
-        $this->addLeftSystem(new LightParticleBeamShip(2, 2, 1, 180, 360));
+	$this->addLeftSystem(new Thruster(3, 15, 0, 5, 3));
+        $this->addLeftSystem(new Particle Projector(2, 6, 1, 180, 360));
         
-		$this->addRightSystem(new Thruster(4, 15, 0, 5, 4));
-        $this->addRightSystem(new LightParticleBeamShip(2, 2, 1, 0 , 180));
-        $this->addRightSystem(new LightParticleBeamShip(2, 2, 1, 0 , 180));     
+	$this->addRightSystem(new Thruster(3, 15, 0, 5, 4));
+        $this->addRightSystem(new Particle Projector(2, 6, 1, 0 , 180));     
         
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
         $this->addFrontSystem(new Structure( 4, 40));
-        $this->addAftSystem(new Structure( 4, 44));
+        $this->addAftSystem(new Structure( 3, 44));
         $this->addLeftSystem(new Structure( 4, 45));
         $this->addRightSystem(new Structure( 4, 45));
         $this->addPrimarySystem(new Structure( 5, 40));
+	    
+	    
+	//d20 hit chart
+	$this->hitChart = array(
+		
+		0=> array(
+			10 => "Structure",
+			13 => "ELINT Scanner",
+			15 => "Engine",
+			17 => "Hangar",
+			19 => "Reactor",
+			20 => "C&C",
+		),
+		1=> array(
+			6 => "Thruster",
+			8 => "Sentinel Point Defense",
+			10 => "Particle Projector",
+			18 => "Structure",
+			20 => "Primary",
+		),
+		2=> array(
+			6 => "Thruster",
+			7 => "Sentinel Point Defense",
+			12 => "Jump Engine",
+			18 => "Structure",
+			20 => "Primary",
+		),
+		3=> array(
+			6 => "Thruster",
+			9 => "Particle Projector",
+			18 => "Structure",
+			20 => "Primary",
+		),
+		4=> array(
+			6 => "Thruster",
+			9 => "Particle Projector",
+			18 => "Structure",
+			20 => "Primary",
+		),
+	);
+	    
     }
 }
+?>
