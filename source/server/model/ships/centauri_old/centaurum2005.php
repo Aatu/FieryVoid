f1c95f425e81d60138000f8dea2eb20e846723af
Zero1627 <reisen@poczta.onet.pl>
2019-10-24 23:34:40 +0200

    Update centaurum2005.php

--- source/server/model/ships/centauri_old/centaurum2005.php
+++ source/server/model/ships/centauri_old/centaurum2005.php
@@ -1,17 +1,18 @@
 <?php
-class Centaurum extends BaseShip{
+class Centaurum2005 extends BaseShip{
     
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
 		$this->pointCost = 850;
         $this->faction = "Centauri (WotCR)";
-        $this->phpclass = "Centaurum";
+        $this->phpclass = "Centaurum2005";
         $this->imagePath = "img/ships/octurion.png";
-        $this->shipClass = "Centaurum Battleship";
+        $this->shipClass = "Centaurum Battleship (2005)";
+        $this->variantOf = "Centaurum Battleship";
         $this->shipSizeClass = 3;
-        $this->limited = 33;
-        $this->fighters = array("normal"=>12);
+        $this->limited = 33; //limited deployment
+        $this->fighters = array("heavy"=>12);
 	    $this->isd = 2005;
         $this->forwardDefense = 17;
         $this->sideDefense = 19;
@@ -41,18 +42,20 @@ class Centaurum extends BaseShip{
         $this->addAftSystem(new Thruster(4, 12, 0, 3, 2));
         $this->addAftSystem(new Thruster(4, 12, 0, 3, 2));
         $this->addAftSystem(new Thruster(4, 10, 0, 2, 2));
-		$this->addAftSystem(new JumpEngine(6, 24, 4, 20));      
+	$this->addAftSystem(new JumpEngine(6, 24, 4, 20));      
         $this->addAftSystem(new LightParticleBeamShip(3, 2, 1, 90, 270));  
         $this->addAftSystem(new AssaultLaser(4, 6, 4, 180, 300));
-		$this->addAftSystem(new AssaultLaser(4, 6, 4, 120, 240));
+	$this->addAftSystem(new AssaultLaser(4, 6, 4, 120, 240));
         $this->addAftSystem(new AssaultLaser(4, 6, 4, 120, 240));
         $this->addAftSystem(new AssaultLaser(4, 6, 4, 60, 180));
+	    
 		$this->addLeftSystem(new Thruster(4, 20, 0, 6, 3));
         $this->addLeftSystem(new AssaultLaser(4, 6, 4, 240, 360));
         $this->addLeftSystem(new LightParticleBeamShip(3, 2, 1, 180, 360));
         $this->addLeftSystem(new LightParticleBeamShip(3, 2, 1, 180, 360));
         $this->addLeftSystem(new LightParticleBeamShip(3, 2, 1, 180, 360));
         $this->addLeftSystem(new LightParticleBeamShip(3, 2, 1, 180, 360));
+	    
 		$this->addRightSystem(new Thruster(4, 20, 0, 6, 4));
         $this->addRightSystem(new AssaultLaser(4, 6, 4, 0, 120));
         $this->addRightSystem(new LightParticleBeamShip(3, 2, 1, 0, 180));
@@ -67,5 +70,52 @@ class Centaurum extends BaseShip{
         $this->addLeftSystem(new Structure( 5, 60));
         $this->addRightSystem(new Structure( 5, 60));
         $this->addPrimarySystem(new Structure( 6, 56));
+	    
+
+	    
+	//d20 hit chart
+	$this->hitChart = array(
+		
+		0=> array(
+			9 => "Structure",
+			12 => "Scanner",
+			15 => "Engine",
+			17 => "Hangar",
+			19 => "Reactor",
+			20 => "C&C",
+		),
+		1=> array(
+			5 => "Thruster",
+			8 => "Assault Laser",
+			9 => "Light Particle Beam",
+			11 => "Heavy Plasma Cannon",
+			18 => "Structure",
+			20 => "Primary",
+		),
+		2=> array(
+			5 => "Thruster",
+			8 => "Jump Engine",
+			13 => "Assault Laser",
+			14 => "Light Particle Beam",
+			18 => "Structure",
+			20 => "Primary",
+		),
+		3=> array(
+			5 => "Thruster",
+			7 => "Assault Laser",
+			10 => "Light Particle Beam",
+			18 => "Structure",
+			20 => "Primary",
+		),
+		4=> array(
+			5 => "Thruster",
+			7 => "Assault Laser",
+			10 => "Light Particle Beam",
+			18 => "Structure",
+			20 => "Primary",
+		),
+	);
+	    
     }
 }
+?>
