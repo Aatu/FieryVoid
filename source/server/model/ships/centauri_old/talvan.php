2c725b2cbfa076418d129ac54096a4dc71f1b9f7
Zero1627 <reisen@poczta.onet.pl>
2019-10-25 12:53:29 +0200

    Talvan done

--- source/server/model/ships/centauri_old/talvan.php
+++ source/server/model/ships/centauri_old/talvan.php
@@ -1,5 +1,4 @@
 <?php
-/*NOT FINISHED YET*/
 class Talvan extends BaseShip{
     
     function __construct($id, $userid, $name,  $slot){
@@ -32,64 +31,52 @@ class Talvan extends BaseShip{
         $this->addPrimarySystem(new Engine(5, 20, 0, 10, 3));
 		$this->addPrimarySystem(new Hangar(5, 2));
         
-        $this->addFrontSystem(new Thruster(4, 15, 0, 4, 1));
-        $this->addFrontSystem(new Thruster(4, 15, 0, 4, 1));
-        $this->addFrontSystem(new LightParticleBeamShip(3, 2, 1, 240, 120));
-		$this->addFrontSystem(new AssaultLaser(4, 6, 4, 300, 60));
-        $this->addFrontSystem(new AssaultLaser(4, 6, 4, 300, 60));
-        $this->addFrontSystem(new HeavyPlasma(3, 8, 5, 300, 60));
-        $this->addFrontSystem(new HeavyPlasma(3, 8, 5, 300, 60));
+        $this->addFrontSystem(new Thruster(4, 10, 0, 3, 1));
+        $this->addFrontSystem(new Thruster(4, 10, 0, 3, 1));
+        $this->addFrontSystem(new ImperialLaser(3, 8, 6, 300, 60));
+        $this->addFrontSystem(new ParticleProjector(3, 0, 0, 240, 60));
+        $this->addFrontSystem(new ParticleProjector(3, 0, 0, 240, 60));
+        $this->addFrontSystem(new ParticleProjector(3, 0, 0, 300, 120));
+        $this->addFrontSystem(new ParticleProjector(3, 0, 0, 300, 120));
+	    	    
+	    
 		
-        $this->addAftSystem(new Thruster(4, 10, 0, 2, 2));
-        $this->addAftSystem(new Thruster(4, 12, 0, 3, 2));
-        $this->addAftSystem(new Thruster(4, 12, 0, 3, 2));
-        $this->addAftSystem(new Thruster(4, 10, 0, 2, 2));
-	$this->addAftSystem(new JumpEngine(6, 24, 4, 20));      
-        $this->addAftSystem(new LightParticleBeamShip(3, 2, 1, 90, 270));  
-        $this->addAftSystem(new AssaultLaser(4, 6, 4, 180, 300));
-	$this->addAftSystem(new AssaultLaser(4, 6, 4, 120, 240));
-        $this->addAftSystem(new AssaultLaser(4, 6, 4, 120, 240));
-        $this->addAftSystem(new AssaultLaser(4, 6, 4, 60, 180));
+        $this->addAftSystem(new Thruster(4, 14, 0, 6, 2));
+        $this->addAftSystem(new Thruster(4, 14, 0, 6, 2));
+	$this->addAftSystem(new JumpEngine(4, 20, 3, 20));      
 	    
-		$this->addLeftSystem(new Thruster(4, 20, 0, 6, 3));
-        $this->addLeftSystem(new AssaultLaser(4, 6, 4, 240, 360));
-        $this->addLeftSystem(new LightParticleBeamShip(3, 2, 1, 180, 360));
-        $this->addLeftSystem(new LightParticleBeamShip(3, 2, 1, 180, 360));
-        $this->addLeftSystem(new LightParticleBeamShip(3, 2, 1, 180, 360));
-        $this->addLeftSystem(new LightParticleBeamShip(3, 2, 1, 180, 360));
+	$this->addLeftSystem(new Thruster(4, 15, 0, 6, 3));
+        $this->addLeftSystem(new HeavyPlasma(3, 8, 5, 300, 0));
+        $this->addLeftSystem(new TacLaser(3, 5, 4, 240, 360));
 	    
-		$this->addRightSystem(new Thruster(4, 20, 0, 6, 4));
-        $this->addRightSystem(new AssaultLaser(4, 6, 4, 0, 120));
-        $this->addRightSystem(new LightParticleBeamShip(3, 2, 1, 0, 180));
-        $this->addRightSystem(new LightParticleBeamShip(3, 2, 1, 0, 180));
-        $this->addRightSystem(new LightParticleBeamShip(3, 2, 1, 0, 180));
-        $this->addRightSystem(new LightParticleBeamShip(3, 2, 1, 0, 180));
+	$this->addRightSystem(new Thruster(4, 15, 0, 6, 4));
+        $this->addRightSystem(new HeavyPlasma(3, 8, 5, 0, 60));
+        $this->addRightSystem(new TacLaser(3, 5, 4, 0, 120));
        
         
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
-        $this->addFrontSystem(new Structure( 5, 55));
-        $this->addAftSystem(new Structure( 5, 55));
-        $this->addLeftSystem(new Structure( 5, 60));
-        $this->addRightSystem(new Structure( 5, 60));
-        $this->addPrimarySystem(new Structure( 6, 56));
+        $this->addFrontSystem(new Structure( 4, 42));
+        $this->addAftSystem(new Structure( 4, 40));
+        $this->addLeftSystem(new Structure( 4, 54));
+        $this->addRightSystem(new Structure( 4, 54));
+        $this->addPrimarySystem(new Structure( 6, 36));
 	    
 	    
 	//d20 hit chart
 	$this->hitChart = array(
 		
 		0=> array(
-			9 => "Structure",
-			12 => "Scanner",
-			15 => "Engine",
+			10 => "Structure",
+			13 => "Scanner",
+			16 => "Engine",
 			17 => "Hangar",
 			19 => "Reactor",
 			20 => "C&C",
 		),
 		1=> array(
-			5 => "Thruster",
-			8 => "Assault Laser",
-			9 => "Light Particle Beam",
-			11 => "Heavy Plasma Cannon",
+			3 => "Thruster",
+			5 => "Imperial Laser",
+			9 => "Particle Projector",
 			18 => "Structure",
 			20 => "Primary",
 		),
@@ -102,16 +89,16 @@ class Talvan extends BaseShip{
 			20 => "Primary",
 		),
 		3=> array(
-			5 => "Thruster",
-			7 => "Assault Laser",
-			10 => "Light Particle Beam",
+			3 => "Thruster",
+			6 => "Heavy Plasma Cannon",
+			9 => "Tactical Laser",
 			18 => "Structure",
 			20 => "Primary",
 		),
 		4=> array(
-			5 => "Thruster",
-			7 => "Assault Laser",
-			10 => "Light Particle Beam",
+			3 => "Thruster",
+			6 => "Heavy Plasma Cannon",
+			9 => "Tactical Laser",
 			18 => "Structure",
 			20 => "Primary",
 		),
