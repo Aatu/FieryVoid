9a9d5d6035fe22bcd7f18e3b060e82aa51fce676
Zero1627 <reisen@poczta.onet.pl>
2019-11-17 09:30:35 +0100

    Cascor weapons

--- source/server/model/weapons/ion.php
+++ source/server/model/weapons/ion.php
@@ -46,7 +46,6 @@ class IonCannon extends Raking{
     public $animation = "laser"; //more fitting for Raking, and faster at long range
     public $animationColor = array(127, 0, 255);
     public $animationExplosionScale = 0.25;
-    public $projectilespeed = 10;
     public $animationWidth = 3;//4;
     public $animationWidth2 = 0.3;
     public $trailLength = 24;
@@ -82,13 +81,12 @@ class ImprovedIonCannon extends Raking{
     public $trailColor = array(30, 170, 255);
 
     public $name = "improvedIonCannon";
-    public $displayName = "Imp. Ion Cannon";
-    public $animation = "trail";
+    public $displayName = "Imp. Ion Cannon";   
+    public $animation = "laser"; //more fitting for Raking, and faster at long range
     public $animationColor = array(127, 0, 255);
-    public $animationExplosionScale = 0.30;
-    public $projectilespeed = 10;
-    public $animationWidth = 5;
-    public $trailLength = 24;
+    public $animationExplosionScale = 0.25;
+    public $animationWidth = 3;//4;
+    public $animationWidth2 = 0.3;
     public $intercept = 1;
     public $priority = 8;
 
@@ -113,6 +111,143 @@ class ImprovedIonCannon extends Raking{
     public function setMinDamage(){     $this->minDamage = 17 ;      }
     public function setMaxDamage(){     $this->maxDamage = 35 ;      }
 }
+
+
+    /*Cascor fighter weapon*/
+    class Ionizer extends LinkedWeapon{
+        public $name = "Ionizer";
+        public $iconPath = "ionizer2.png";
+        public $displayName = "Ionizer";        
+        
+        public $animation = "laser"; //deals Standard damage, but that's due to beam having short duration - it's a laser beam in nature
+        public $animationColor = array(160, 0, 255);
+        public $animationExplosionScale = 0.1;
+        public $animationWidth = 2;
+        public $animationWidth2 = 0.2;
+        
+        public $priority = 4;
+        public $intercept = 2;
+        public $loadingtime = 1;
+        public $shots = 2;
+        public $defaultShots = 2;
+        public $rangePenalty = 2;
+        public $fireControl = array(0, 0, 0); // fighters, <mediums, <capitals
+        public $uninterceptable = true;//not-interceptable weapon - described as Ion+Laser originally
+        public $damageType = "Standard"; 
+        public $weaponClass = "Ion"; 
+        
+        function __construct($startArc, $endArc, $nrOfShots = 2){
+            $this->defaultShots = $nrOfShots;
+            $this->shots = $nrOfShots;
+            $this->intercept = $nrOfShots;
+            if($nrOfShots === 1){
+                $this->iconPath = "ionizer1.png";
+            }
+            if($nrOfShots >2){//no special icon for more than 3 linked weapons
+                $this->iconPath = "ionizer3.png";
+            }
+			
+            parent::__construct(0, 1, 0, $startArc, $endArc);
+        }
+        
+        public function setSystemDataWindow($turn){
+            parent::setSystemDataWindow($turn);
+            if (!isset($this->data["Special"])) {
+                $this->data["Special"] = '';
+            }else{
+                $this->data["Special"] .= '<br>';
+            }	    
+            $this->data["Special"] .= "Uninterceptable."; 
+        }
+        
+        public function getDamage($fireOrder){        return Dice::d(3,2)+2;   }
+        public function setMinDamage(){     $this->minDamage = 4 ;      }
+        public function setMaxDamage(){     $this->maxDamage = 8 ;      }
+    }
+
+
+
+    /*Cascor weapon*/
+    class IonicLaser extends Laser{        
+        public $name = "IonicLaser";
+        public $iconPath = "ionicLaser.png";
+        public $displayName = "Ionic Laser";
+        public $animation = "laser";
+        public $animationColor = array(160, 0, 255);
+        public $animationExplosionScale = 0.18;
+        public $animationWidth = 3;
+        public $animationWidth2 = 0.3;
+        public $priority = 8; //light Raking
+        
+        public $loadingtime = 2;
+        
+        public $raking = 10;
+        
+        public $rangePenalty = 0.5;
+        public $fireControl = array(-3, 2, 3); // fighters, <mediums, <capitals 
+        
+        public $damageType = "Raking"; 
+        public $weaponClass = "Ion"; 
+    
+        function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
+            //maxhealth and power reqirement are fixed; left option to override with hand-written values
+            if ( $maxhealth == 0 ) $maxhealth = 6;
+            if ( $powerReq == 0 ) $powerReq = 4;
+            parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
+        }
+        
+        public function setSystemDataWindow($turn){
+            parent::setSystemDataWindow($turn);
+            /* Laser class itself marks Uninterceptable
+            if (!isset($this->data["Special"])) {
+                $this->data["Special"] = '';
+            }else{
+                $this->data["Special"] .= '<br>';
+            }	    
+            $this->data["Special"] .= "Uninterceptable."; 
+            */
+        }
+        
+        public function getDamage($fireOrder){        return Dice::d(10, 3)+8;   }
+        public function setMinDamage(){     $this->minDamage = 11 ;      }
+        public function setMaxDamage(){     $this->maxDamage = 38 ;      }    
+    }
+
+
+    /*Cascor weapon*/
+    class DualIonBolter extends Weapon{
+        public $name = "DualIonBolter";
+        public $displayName = "Light Bolter";
+        public $iconPath = "dualIonBolter.png";
+        
+        public $animation = "trail";
+        public $animationColor = array(127, 0, 255);
+        public $animationExplosionScale = 0.2;
+        public $projectilespeed = 16;
+        public $animationWidth = 3;
+        public $trailLength = 3;
+        
+        public $priority = 3; //damage output 8 is very light
+        public $loadingtime = 1;
+        public $intercept = 2;
+        public $rangePenalty = 1;
+        public $guns = 2;
+        
+        public $damageType = "Standard"; 
+        public $weaponClass = "Ion"; 
+        
+        public $fireControl = array(2, 2, 2); // fighters, <mediums, <capitals
+        function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
+            //maxhealth and power reqirement are fixed; left option to override with hand-written values
+            if ( $maxhealth == 0 ) $maxhealth = 4;
+            if ( $powerReq == 0 ) $powerReq = 4;
+            parent::__construct($armour, $maxhealth, $powerReq, $startArc, $endArc);
+        }
+        
+        public function getDamage($fireOrder){        return 8;   }
+        public function setMinDamage(){     $this->minDamage = 8 ;      }
+        public function setMaxDamage(){     $this->maxDamage = 8 ;      }
+    }
 
 
 ?>
