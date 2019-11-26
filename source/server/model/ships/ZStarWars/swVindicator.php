88f032fb546b0b1978896207264472e421578c6b
Zero1627 <reisen@poczta.onet.pl>
2019-11-26 16:42:02 +0100

    Update swVindicator.php

--- source/server/model/ships/ZStarWars/swVindicator.php
+++ source/server/model/ships/ZStarWars/swVindicator.php
@@ -4,7 +4,7 @@ class swVindicator extends BaseShipNoFwd{
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-	$this->pointCost = 750;
+	$this->pointCost = 625;
 	$this->faction = "ZStarWars";
         $this->phpclass = "swVindicator";
         $this->imagePath = "img/starwars/vindicator.png";
