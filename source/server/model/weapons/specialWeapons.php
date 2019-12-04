c5a71204bcc727e1a36c4b40b73d463184873e8e
Zero1627 <reisen@poczta.onet.pl>
2019-12-04 14:46:23 +0100

    BurstBeam and derivatives overhaul

--- source/server/model/weapons/specialWeapons.php
+++ source/server/model/weapons/specialWeapons.php
@@ -471,7 +471,6 @@ class BurstPulseCannon extends Pulse {
 		$damageEntry = new DamageEntry(-1, $ship->id, -1, $fireOrder->turn, $system->id, $dmgToDo, $armor, 0, $fireOrder->id, $destroyed, "", $this->weaponClass, $fireOrder->shooterid, $this->id);
 		$damageEntry->updated = true;
 		$system->damage[] = $damageEntry;
-		$this->onDamagedSystem($ship, $system, $dmgToDo, $armor, $gamedata, $fireOrder);
 		if($destroyed==true) return; //no point doing anything else if target is destroyed outright
 		if($system->advancedArmor) return;//Advanced Armor will protect the fighter from forced dropout
                 if (!$ship->superheavy){
@@ -604,7 +603,6 @@ class BurstPulseCannon extends Pulse {
 		$damageEntry = new DamageEntry(-1, $ship->id, -1, $fireOrder->turn, $system->id, $dmgToDo, $armor, 0, $fireOrder->id, $destroyed, "", $this->weaponClass, $fireOrder->shooterid, $this->id);
 		$damageEntry->updated = true;
 		$system->damage[] = $damageEntry;
-		$this->onDamagedSystem($ship, $system, $dmgToDo, $armor, $gamedata, $fireOrder);
 		if($destroyed==true) return; //no point doing anything else if target is destroyed outright
 		if($system->advancedArmor) return;//Advanced Armor will protect the fighter from forced dropout
                 if (!$ship->superheavy){
