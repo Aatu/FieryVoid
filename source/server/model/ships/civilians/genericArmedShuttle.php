3cb08d568fe24c54d881e206d2635fd7abed2dde
unknown <reisen@poczta.onet.pl>
2020-05-25 21:48:01 +0200

    Raiders and Civilians shuffling around

--- source/server/model/ships/civilians/genericArmedShuttle.php
+++ source/server/model/ships/civilians/genericArmedShuttle.php
@@ -5,18 +5,18 @@ class genericArmedShuttle extends FighterFlight{
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-		    $this->pointCost = 15*6;
-		    $this->faction = "Civilians";
+		$this->pointCost = 24*6;
+		$this->faction = "Civilians";
         $this->phpclass = "genericArmedShuttle";
         $this->shipClass = "Armed Shuttles";
-		    $this->imagePath = "img/ships/LlortLeteerum.png"; //more appropriate image needed
+		$this->imagePath = "img/ships/LlortLeteerum.png"; //more appropriate image needed
         
 		$this->notes = "Usually housed in common shuttle bays (not mentioned in FV). Most ships can take a pair if they have to.";
 		
         $this->forwardDefense = 8;
         $this->sideDefense = 10;
-        $this->freethrust = 6;
-        $this->offensivebonus = 2;
+        $this->freethrust = 4;
+        $this->offensivebonus = 3;
         $this->jinkinglimit = 0;
         $this->pivotcost = 2; //shuttles have pivot cost higher
         $this->turncost = 0.33;
@@ -34,7 +34,7 @@ class genericArmedShuttle extends FighterFlight{
         $toAdd = $new - $current;
         for ($i = 0; $i < $toAdd; $i++){
 			
-			$armour = array(2, 2, 2, 2);
+			$armour = array(1, 1, 1, 1);
 			$fighter = new Fighter("genericArmedShuttle", $armour, 10, $this->id);
 			$fighter->displayName = "Armed Shuttle";
 			$fighter->imagePath = "img/ships/LlortLeteerum.png";
@@ -42,7 +42,7 @@ class genericArmedShuttle extends FighterFlight{
 			
 			
 			$fighter->addFrontSystem(new PairedParticleGun(330, 30, 2, 1)); //1 gun d6+2
-	    		$fighter->addAftSystem(new RammingAttack(0, 0, 360, $fighter->getRammingFactor(), 0)); //ramming attack
+			$fighter->addAftSystem(new RammingAttack(0, 0, 360, $fighter->getRammingFactor(), 0)); //ramming attack
 			
 			
 			$this->addSystem($fighter);
