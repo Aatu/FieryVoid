b39bdc23e434e9d1073e30005d270aadd2ff031e
melkorium <64601604+melkorium@users.noreply.github.com>
2020-06-13 16:07:18 +0100

    Added Tlorr

--- source/server/model/ships/narn/zzunoffTlorr.php
+++ source/server/model/ships/narn/zzunoffTlorr.php
@@ -1,18 +1,21 @@
 <?php
-class Trakk extends HeavyCombatVessel{
+class Tlorr extends HeavyCombatVessel{
     
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
 		$this->pointCost = 350;
 		$this->faction = "Narn";
-        $this->phpclass = "Trakk";
+        $this->phpclass = "zzunoffTlorr";
         $this->imagePath = "img/ships/trakk.png";
-        $this->shipClass = "T'Rakk Frigate";
+        $this->shipClass = "T'Lorr Light Destroyer";
 	    $this->isd = 2213;
-       
+        $this->fighters = array("medium"=>6);
         
-		
+		$this->variantOf = "T'Rakk Frigate";
+		$this->occurence = "common";
+		$this->unofficial = true;
+        
         $this->forwardDefense = 10;
         $this->sideDefense = 16;
         
@@ -25,16 +28,16 @@ class Trakk extends HeavyCombatVessel{
         $this->iniativebonus = 30;
 
         
-        $this->addPrimarySystem(new Reactor(4, 12, 0, 0));
+        $this->addPrimarySystem(new Reactor(4, 10, 0, 0));
         $this->addPrimarySystem(new CnC(4, 7, 0, 0));
-        $this->addPrimarySystem(new Scanner(4, 12, 3, 6));
+        $this->addPrimarySystem(new Scanner(4, 10, 2, 5));
         $this->addPrimarySystem(new Engine(4, 8, 0, 6, 3));
-		$this->addPrimarySystem(new Hangar(3, 1));
+		$this->addPrimarySystem(new Hangar(3, 7));
 		$this->addPrimarySystem(new Thruster(4, 10, 0, 4, 3));
         $this->addPrimarySystem(new Thruster(4, 10, 0, 4, 4));
         
-        $this->addFrontSystem(new HeavyPlasma(4, 8, 5, 300, 0));
-        $this->addFrontSystem(new HeavyPlasma(4, 8, 5, 0, 60));        
+        $this->addFrontSystem(new MediumPlasma(4, 5, 3, 300, 0));
+        $this->addFrontSystem(new MediumPlasma(4, 5, 3, 0, 60));        
         $this->addFrontSystem(new Thruster(4, 10, 0, 4, 1));
         $this->addFrontSystem(new StdParticleBeam(2, 4, 1, 240, 120));
 		
@@ -60,7 +63,7 @@ class Trakk extends HeavyCombatVessel{
 			),
 			1=> array(
 				4 => "Thruster",
-				8 => "Heavy Plasma Cannon",
+				8 => "Medium Plasma Cannon",
 				10 => "Standard Particle Beam",
 				18 => "Structure",
 				20 => "Primary",
