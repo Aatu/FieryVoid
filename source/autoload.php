07e34538f55a36b97f678a10ae2d17ba5cc57ac1
melkorium <64601604+melkorium@users.noreply.github.com>
2020-08-06 19:08:10 +0100

    Update autoload.php

--- source/autoload.php
+++ source/autoload.php
@@ -1310,7 +1310,8 @@ spl_autoload_register(
 				'croscotuearly' => '/server/model/ships/Descari/CroscotuEarly.php',
 				'croscoturefit'	=> '/server/model/ships/Descari/CroscotuRefit.php',
 				'lightparticlebolt' => '/server/model/weapons/particle.php',
-				'rulnata' => 'server/model/ships/Descari/Rulnata.php',	    
+				'cruscava' => '/server/model/ships/Descari/Cruscava.php',
+				'rulnata' => '/server/model/ships/Descari/Rulnata.php',	    
 		    
 		    	//Random Ships which don't require new mechanics to add
 		    	'tlorr' => '/server/model/ships/narn/zzunoffTlorr.php',
