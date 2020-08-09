f4471d307e42550e1516d745a56bcccc66afc113
gstano <32845840+gstano@users.noreply.github.com>
2020-08-09 00:49:23 -0500

    Add files via upload

---
+++ source/server/model/ships/ZNexusBrixadii/BrixadiiNewDestroyer.php
@@ -0,0 +1,80 @@
+<?php
+class BrixadiiNewDestroyer extends HeavyCombatVessel{
+    
+    function __construct($id, $userid, $name,  $slot){
+        parent::__construct($id, $userid, $name,  $slot);
+        
+        $this->pointCost = 380;
+        $this->faction = "ZNexus Brixadii";
+        $this->phpclass = "BrixadiiNewDestroyer";
+        $this->imagePath = "img/ships/Nexus/BrixadiiBattleDestroyer.png";
+			$this->canvasSize = 200; //img has 200px per side
+        $this->shipClass = "New Destroyer";
+			$this->unofficial = true;
+        $this->isd = 2108;
+        
+        $this->forwardDefense = 12;
+        $this->sideDefense = 13;
+        
+        $this->turncost = 0.33;
+        $this->turndelaycost = 0.50;
+        $this->accelcost = 2;
+        $this->rollcost = 2;
+        $this->pivotcost = 2;
+        $this->iniativebonus = 30;
+        
+         
+        $this->addPrimarySystem(new Reactor(4, 16, 0, 0));
+        $this->addPrimarySystem(new CnC(5, 10, 0, 0));
+        $this->addPrimarySystem(new Scanner(4, 10, 5, 5));
+        $this->addPrimarySystem(new Engine(4, 14, 0, 9, 4));
+        $this->addPrimarySystem(new Hangar(1, 2));
+        $this->addPrimarySystem(new Thruster(3, 14, 0, 6, 3));
+        $this->addPrimarySystem(new Thruster(3, 14, 0, 6, 4));
+      
+        $this->addFrontSystem(new Thruster(3, 14, 0, 6, 1));
+    	$this->addFrontSystem(new NexusParticleAgitator(3, 0, 0, 240, 0));
+        $this->addFrontSystem(new NexusParticleAgitator(3, 0, 0, 0, 120));
+        $this->addFrontSystem(new NexusParticleProjectorLight(2, 0, 0, 180, 60));
+        $this->addFrontSystem(new NexusParticleProjectorLight(2, 0, 0, 300, 180));
+		$this->addFrontSystem(new NexusKineticBoxLauncher(2, 0, 0, 300, 60));
+                
+        $this->addAftSystem(new Thruster(3, 14, 0, 4, 2));
+        $this->addAftSystem(new Thruster(3, 14, 0, 4, 2));
+        $this->addAftSystem(new NexusParticleProjectorLight(1, 0, 0, 90, 270));
+        $this->addAftSystem(new NexusParticleProjectorLight(1, 0, 0, 90, 270));
+		$this->addAftSystem(new NexusChaffLauncher(2, 0, 0, 0, 0));
+        
+        //0:primary, 1:front, 2:rear, 3:left, 4:right;
+        $this->addFrontSystem(new Structure( 3, 35));
+        $this->addAftSystem(new Structure( 3, 32));
+        $this->addPrimarySystem(new Structure( 4, 40));
+        $this->hitChart = array(
+            0=> array(
+                    7 => "Structure",
+                    10 => "Thruster",
+                    13 => "Scanner",
+                    16 => "Engine",
+                    17 => "Hangar",
+                    19 => "Reactor",
+                    20 => "C&C",
+            ),
+            1=> array(
+                    5 => "Thruster",
+                    7 => "Light Particle Projector",
+                    10 => "Particle Agitator",
+					12 => "Kinetic Box Launcher",
+					18 => "Structure",
+                    20 => "Primary",
+            ),
+            2=> array(
+                    7 => "Thruster",
+					8 => "Chaff Launcher",
+                    10 => "Light Particle Projector",
+                    18 => "Structure",
+                    20 => "Primary",
+            ),
+        );
+    }
+}
+?>
