1e58dbcf3a149e878bb00e85fb21bb1d1802d527
melkorium <64601604+melkorium@users.noreply.github.com>
2020-10-23 19:58:16 +0100

    Update autoload.php

--- source/autoload.php
+++ source/autoload.php
@@ -1494,10 +1494,10 @@ spl_autoload_register(
 		    
 		    
 		    	//Corillani
-				'plasmaprojector' =>'/server/model/weapons/plasma.php'
-				'garunda' => '/server/model/ships/Garunda.php' 
+				'plasmaprojector' =>'/server/model/weapons/plasma.php',
 				'mollanta' => '/server/model/ships/corillani/Mollanta.php',
-				'llartol' => '/server/model/ships/corillani/Llartol.php',		
+				'llartol' => '/server/model/ships/corillani/Llartol.php',
+				'garunda' => 'server/model/ships/corillani/Garunda.php',		
 		    	
 		    	
 		    	
