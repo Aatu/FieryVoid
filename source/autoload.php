90bf99018b1100a78ea82ab72c62a5c96a1318c8
Zero1627 <reisen@poczta.onet.pl>
2020-12-26 10:55:48 +0100

    Corillani added

--- source/autoload.php
+++ source/autoload.php
@@ -1600,7 +1600,16 @@ spl_autoload_register(
 				'gaimtoraosat' => '/server/model/ships/gaim/gaimToraOSAT.php',
 				'gaimtracha' => '/server/model/ships/gaim/gaimTracha.php',
 				
-				
+
+				//Corillani
+				'plasmaprojector' => 'server/model/weapons/plasma.php',
+				'conosti' => '/server/model/ships/corillani/Conosti.php',
+				'garunda' => '/server/model/ships/corillani/Garunda.php',
+				'llartol' => '/server/model/ships/corillani/Llartol.php',
+				'mollanta' => '/server/model/ships/corillani/Mollanta.php',
+				'zftrtillinicpn' => '/server/model/ships/corillani/ZFtrTilliniCPN.php',
+				'zftrtillinidoc' => '/server/model/ships/corillani/ZFtrTilliniDoC.php',
+				'zftrtilliniosf' +> '/server/model/ships/corillani/ZFtrTilliniOSF.php',
 				
 				//TEMPORARY campaign-related designs - essentially upgrades of existing ships		    
 				'hyperionpulsearmored' => '/server/model/ships/customs/HyperionPulseArmored.php',
