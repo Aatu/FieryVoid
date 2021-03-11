e937cf49ff7c4b2c2b09c35e183a453d86aa8288
melkorium <64601604+melkorium@users.noreply.github.com>
2021-03-11 21:32:17 +0000

    Update Troshanthi.php

--- source/server/model/ships/drazi/Troshanthi.php
+++ source/server/model/ships/drazi/Troshanthi.php
@@ -9,7 +9,6 @@ class Troshanthi extends FighterFlight{
         $this->phpclass = "Troshanthi";
         $this->shipClass = "Troshanthi Assault Shuttles";
 		    $this->imagePath = "img/ships/DraziTroshanthi.png";
-	    $this->unofficial = true;  		    
         
         $this->forwardDefense = 7;
         $this->sideDefense = 9;
