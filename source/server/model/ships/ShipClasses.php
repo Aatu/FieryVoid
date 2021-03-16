e84ac009b13f1c41fb3739dabfec99a900ea5bf5
melkorium <64601604+melkorium@users.noreply.github.com>
2021-03-16 22:38:57 +0000

    fix

--- source/server/model/ships/ShipClasses.php
+++ source/server/model/ships/ShipClasses.php
@@ -2113,28 +2113,38 @@ class SixSidedShip extends BaseShip{
         parent::__construct($id, $userid, $name,$slot);
     }
     
+    protected function addFrontSystem($system){
+        $this->addSystem($system, 1);
+    }
+    protected function addAftSystem($system){
+        $this->addSystem($system, 2);
+    }
     protected function addLeftFrontSystem($system){
-        $this->addSystem($system, 3);
+        $this->addSystem($system, 31);
     }
     protected function addLeftAftSystem($system){
-        $this->addSystem($system, 3);
+        $this->addSystem($system, 32);
     }
     protected function addRightFrontSystem($system){
-        $this->addSystem($system, 4);
+        $this->addSystem($system, 41);
     }
     protected function addRightAftSystem($system){
-        $this->addSystem($system, 4);
+        $this->addSystem($system, 42);
     }
 
     public function getLocations(){
+        //debug::log("getLocations");         
         $locs = array();
-        $locs[] = array("loc" => 1, "min" => 330, "max" => 30, "profile" => $this->forwardDefense);
-        $locs[] = array("loc" => 4, "min" => 30, "max" => 150, "profile" => $this->sideDefense);
-        $locs[] = array("loc" => 2, "min" => 150, "max" => 210, "profile" => $this->forwardDefense);
-        $locs[] = array("loc" => 3, "min" => 210, "max" => 330, "profile" => $this->sideDefense);
+
+        $locs[] = array("loc" => 1, "min" => 300, "max" => 60, "profile" => $this->forwardDefense);
+        $locs[] = array("loc" => 41, "min" => 0, "max" => 120, "profile" => $this->forwardDefense);
+        $locs[] = array("loc" => 42, "min" => 60, "max" => 180, "profile" => $this->forwardDefense);
+        $locs[] = array("loc" => 2, "min" => 120, "max" => 240, "profile" => $this->forwardDefense);
+        $locs[] = array("loc" => 32, "min" => 180, "max" => 300, "profile" => $this->forwardDefense);
+        $locs[] = array("loc" => 31, "min" => 240, "max" => 360, "profile" => $this->forwardDefense);
+
         return $locs;
-    }
-	
+    }		
 
 }
 
