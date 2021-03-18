5fe22dc0050d9b2f70d9db79aef11f944e21e2ea
melkorium <64601604+melkorium@users.noreply.github.com>
2021-03-18 13:50:42 +0000

    Update SixTester2.php

--- source/server/model/ships/customs/SixTester2.php
+++ source/server/model/ships/customs/SixTester2.php
@@ -1,5 +1,5 @@
 <?php
-class SixTester2 extends SixSidedShipBase{
+class SixTester2 extends SixSidedShip{
 
 	function __construct($id, $userid, $name,  $slot){
 		parent::__construct($id, $userid, $name,  $slot);
