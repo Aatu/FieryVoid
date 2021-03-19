41e3f9d0b10aaf86d8677775540e5b95e334dc48
melkorium <64601604+melkorium@users.noreply.github.com>
2021-03-19 11:57:48 +0000

    Update shipwindow.js

--- source/public/client/UI/shipwindow.js
+++ source/public/client/UI/shipwindow.js
@@ -176,9 +176,10 @@ window.shipWindowManager = {
 
 		var names = ["Primary", "Front", "Aft", "Port", "Starboard"];
 
-		if ( (ship.base && !ship.smallBase) ) {
+		if ( (ship.base && !ship.smallBase) || (ship.SixSidedShip)) {
 			names[1] = "Sections";
 			toDo = 2;
+<<<<<<< HEAD
 		} 
 		if ( (ship.SixSidedShip)) {
 			names[0] = "Primary";
@@ -191,6 +192,9 @@ window.shipWindowManager = {
 			toDo = 7;
 		}
 		else {
+=======
+		} else {
+>>>>>>> parent of fe5fd5f29 (Update shipwindow.js)
 			toDo = 5; //(almost) always try to show all 5 sections, there may be holes
 		}
 
