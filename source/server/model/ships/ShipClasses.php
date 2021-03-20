2f62b22f76337be57542ebca9b70203c4e28dbbf
melkorium <64601604+melkorium@users.noreply.github.com>
2021-03-20 16:38:21 +0000

    Update ShipClasses.php

--- source/server/model/ships/ShipClasses.php
+++ source/server/model/ships/ShipClasses.php
@@ -2187,10 +2187,25 @@ class SixSidedShip extends BaseShip{
 
 } //end of SixSidedShip
 
-class VreeCapital extends SixSidedShip{
+class VreeCapital extends BaseShip{
+    public $SixSidedShip = true;
+ 
      
     function __construct($id, $userid, $name, $slot){
         parent::__construct($id, $userid, $name,$slot);
+    }
+    
+    protected function addLeftFrontSystem($system){
+        $this->addSystem($system, 31);
+    }
+    protected function addLeftAftSystem($system){
+        $this->addSystem($system, 32);
+    }
+    protected function addRightFrontSystem($system){
+        $this->addSystem($system, 41);
+    }
+    protected function addRightAftSystem($system){
+        $this->addSystem($system, 42);
     }
 
     public function getLocations(){
@@ -2207,7 +2222,7 @@ class VreeCapital extends SixSidedShip{
         return $locs;
     }	
 
-}
+}// end fo VreeCapital
 
 
 ?>
