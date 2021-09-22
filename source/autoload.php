2a5c0573a868f5f960e6fa637209f9bb4aa33cc5
gstano <kosnett@gmail.com>
2021-09-21 22:49:47 -0500

    Llort Erlorra structure

--- source/autoload.php
+++ source/autoload.php
@@ -1536,10 +1536,10 @@ spl_autoload_register(
 
 
 				//Nexus ships - Craytan
-                'craytancorvette' => '/server/model/ships/ZNexusCraytan/CraytanCorvette.php',					
-                'craytancorvette2089' => '/server/model/ships/ZNexusCraytan/CraytanCorvette2089.php',					
-                'craytancorvette2126' => '/server/model/ships/ZNexusCraytan/CraytanCorvette2126.php',					
-                'craytanhepron' => '/server/model/ships/ZNexusCraytan/CraytanHepron.php',					
+//                'craytancorvette' => '/server/model/ships/ZNexusCraytan/CraytanCorvette.php',					
+//                'craytancorvette2089' => '/server/model/ships/ZNexusCraytan/CraytanCorvette2089.php',					
+//                'craytancorvette2126' => '/server/model/ships/ZNexusCraytan/CraytanCorvette2126.php',					
+//                'craytanhepron' => '/server/model/ships/ZNexusCraytan/CraytanHepron.php',					
 
 
 
