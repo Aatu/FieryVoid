a99736d40ac31526f64dde7b7ab1488c3d48d3d6
melkorium <64601604+melkorium@users.noreply.github.com>
2021-09-27 13:33:10 +0100

    Vree ships

---
+++ source/server/model/ships/vree/Xill.php
@@ -0,0 +1,127 @@
+<?php
+class Xill extends VreeCapital{
+
+	function __construct($id, $userid, $name,  $slot){
+		parent::__construct($id, $userid, $name,  $slot);
+
+		$this->pointCost = 700;
+		$this->faction = "Vree";
+		$this->phpclass = "Xill";
+		$this->shipClass = "Xill Battle Saucer";
+		$this->isd = 2258;
+		$this->locations = array(41, 42, 2, 32, 31, 1);		
+
+		$this->shipSizeClass = 3;
+		$this->iniativebonus = 0;
+		
+        $this->turncost = 1;
+        $this->turndelaycost = 0.66;
+        $this->accelcost = 4;
+        $this->rollcost = 5;
+        $this->pivotcost = 0;	
+        $this->gravitic = true;        	
+
+		$this->forwardDefense = 15;
+		$this->sideDefense = 15;
+
+		$this->imagePath = "img/ships/VreeXill.png";
+		$this->canvasSize = 200;
+
+		$this->addPrimarySystem(new Reactor(6, 18, 0, 0));
+		$this->addPrimarySystem(new Hangar(6, 1));
+		$this->addPrimarySystem(new CnC(6, 12, 0, 0));
+		$this->addPrimarySystem(new Scanner(6, 14, 9, 10));
+        $this->addPrimarySystem(new Engine(6, 16, 0, 9, 2));
+		$this->addPrimarySystem(new JumpEngine(6, 10, 5, 24));         			
+		$this->addPrimarySystem(new AntimatterShredder(3, 0, 0, 0, 360));
+		$this->addPrimarySystem(new AntimatterShredder(3, 0, 0, 0, 360));
+
+
+        $this->addFrontSystem(new AntiprotonGun(3, 0, 0, 300, 60));
+        $this->addFrontSystem(new Thruster(5, 16, 0, 9, 1));        
+		
+        $this->addAftSystem(new AntiprotonGun(3, 0, 0, 120, 240));
+        $this->addAftSystem(new Thruster(5, 16, 0, 9, 2));       
+        
+		$this->addLeftFrontSystem(new AntiprotonGun(3, 0, 0, 240, 360));
+		$this->addLeftFrontSystem(new Thruster(5, 8, 0, 4, 3));
+				
+		$this->addLeftAftSystem(new AntiprotonGun(3, 0, 0, 180, 300));
+		$this->addLeftAftSystem(new Thruster(5, 8, 0, 5, 3));
+		
+		$this->addRightFrontSystem(new AntiprotonGun(3, 0, 0, 0, 120));
+		$this->addRightFrontSystem(new Thruster(5, 8, 0, 4, 4));
+				
+		$this->addRightAftSystem(new AntiprotonGun(3, 0, 0, 60, 180));
+		$this->addRightAftSystem(new Thruster(5, 8, 0, 5, 4));	
+		
+       
+        //0:primary, 1:front, 2:rear, 3:left, 4:right;
+        $this->addFrontSystem(new Structure( 5, 30));
+        $this->addAftSystem(new Structure( 5, 30));
+        $this->addLeftFrontSystem(new Structure( 5, 30));
+        $this->addLeftAftSystem(new Structure( 5, 30));
+        $this->addRightFrontSystem(new Structure( 5, 30));
+        $this->addRightAftSystem(new Structure( 5, 30));      
+        $this->addPrimarySystem(new Structure( 6, 44));
+	    
+	//d20 hit chart
+        $this->hitChart = array(
+
+            0=> array(
+                    9 => "Structure",
+                    10 => "Jump Engine",
+                    12 => "Scanner",
+                    15 => "Engine",
+                    16 => "Hangar",
+                    19 => "Reactor",
+                    20 => "C&C",
+           		 ),
+            1=> array(
+                    4 => "Thruster",
+                    6 => "Antiproton Gun",
+                    8 => "0:Antimatter Cannon",                    
+                    18 => "Structure",
+                    20 => "Primary",
+           		 ),
+            2=> array(
+                    4 => "Thruster",
+                    6 => "Antiproton Gun",
+                    8 => "0:Antimatter Cannon",                    
+                    18 => "Structure",
+                    20 => "Primary",
+           		 ),
+            31=> array(
+                    4 => "Thruster",
+                    6 => "Antiproton Gun",
+                    8 => "0:Antimatter Cannon",                    
+                    18 => "Structure",
+                    20 => "Primary",
+           		 ),
+            32=> array(
+                    4 => "Thruster",
+                    6 => "Antiproton Gun",
+                    8 => "0:Antimatter Cannon",                    
+                    18 => "Structure",
+                    20 => "Primary",
+           		 ),
+            41=> array(
+                    4 => "Thruster",
+                    6 => "Antiproton Gun",
+                    8 => "0:Antimatter Cannon",                    
+                    18 => "Structure",
+                    20 => "Primary",
+           		 ),
+       		42=> array(
+                    4 => "Thruster",
+                    6 => "Antiproton Gun",
+                    8 => "0:Antimatter Cannon",                    
+                    18 => "Structure",
+                    20 => "Primary",
+           		 ),
+           	);
+       		
+		}
+	}
+		
+?>		
---
+++ source/server/model/ships/vree/Xonn.php
@@ -0,0 +1,142 @@
+<?php
+class Xonn extends VreeCapital{
+
+	function __construct($id, $userid, $name,  $slot){
+		parent::__construct($id, $userid, $name,  $slot);
+
+		$this->pointCost = 1200;
+		$this->faction = "Vree";
+		$this->phpclass = "Xonn";
+		$this->shipClass = "Xonn Dreadnought";
+		$this->isd = 2260;
+		$this->locations = array(41, 42, 2, 32, 31, 1);		
+
+		$this->shipSizeClass = 3;
+		$this->iniativebonus = 0;
+		
+        $this->turncost = 1.5;
+        $this->turndelaycost = 1;
+        $this->accelcost = 5;
+        $this->rollcost = 6;
+        $this->pivotcost = 0;	
+        $this->gravitic = true;        	
+
+		$this->forwardDefense = 16;
+		$this->sideDefense = 16;
+
+		$this->imagePath = "img/ships/VreeXonn.png";
+		$this->canvasSize = 200;
+
+		$this->addPrimarySystem(new Reactor(6, 25, 0, 0));
+		$this->addPrimarySystem(new Hangar(6, 3));
+		$this->addPrimarySystem(new CnC(7, 16, 0, 0));
+		$this->addPrimarySystem(new Scanner(6, 18, 9, 10));
+        $this->addPrimarySystem(new Engine(6, 18, 0, 10, 3));
+		$this->addPrimarySystem(new JumpEngine(7, 16, 6, 24));
+		$this->addPrimarySystem(new AntimatterCannon(4, 0, 0, 0, 360));		         			
+		$this->addPrimarySystem(new AntimatterShredder(4, 0, 0, 0, 360));
+		$this->addPrimarySystem(new AntimatterCannon(4, 0, 0, 0, 360));		
+		$this->addPrimarySystem(new AntimatterShredder(4, 0, 0, 0, 360));
+		$this->addPrimarySystem(new AntimatterCannon(4, 0, 0, 0, 360));		
+
+
+        $this->addFrontSystem(new AntiprotonGun(3, 0, 0, 300, 60));
+        $this->addFrontSystem(new AntiprotonGun(3, 0, 0, 300, 60));        
+        $this->addFrontSystem(new Thruster(5, 20, 0, 10, 1));        
+		
+        $this->addAftSystem(new AntiprotonGun(3, 0, 0, 120, 240));
+        $this->addAftSystem(new AntiprotonGun(3, 0, 0, 120, 240));        
+        $this->addAftSystem(new Thruster(5, 20, 0, 10, 2));       
+        
+		$this->addLeftFrontSystem(new AntiprotonGun(3, 0, 0, 240, 360));
+		$this->addLeftFrontSystem(new AntiprotonGun(3, 0, 0, 240, 360));		
+		$this->addLeftFrontSystem(new Thruster(5, 10, 0, 5, 3));
+				
+		$this->addLeftAftSystem(new AntiprotonGun(3, 0, 0, 180, 300));
+		$this->addLeftAftSystem(new AntiprotonGun(3, 0, 0, 180, 300));		
+		$this->addLeftAftSystem(new Thruster(5, 10, 0, 5, 3));
+		
+		$this->addRightFrontSystem(new AntiprotonGun(3, 0, 0, 0, 120));
+		$this->addRightFrontSystem(new AntiprotonGun(3, 0, 0, 0, 120));		
+		$this->addRightFrontSystem(new Thruster(5, 10, 0, 5, 4));
+				
+		$this->addRightAftSystem(new AntiprotonGun(3, 0, 0, 60, 180));
+		$this->addRightAftSystem(new AntiprotonGun(3, 0, 0, 60, 180));		
+		$this->addRightAftSystem(new Thruster(5, 10, 0, 5, 4));	
+		
+       
+        //0:primary, 1:front, 2:rear, 3:left, 4:right;
+        $this->addFrontSystem(new Structure( 5, 36));
+        $this->addAftSystem(new Structure( 5, 36));
+        $this->addLeftFrontSystem(new Structure( 5, 36));
+        $this->addLeftAftSystem(new Structure( 5, 36));
+        $this->addRightFrontSystem(new Structure( 5, 36));
+        $this->addRightAftSystem(new Structure( 5, 36));      
+        $this->addPrimarySystem(new Structure( 6, 60));
+	    
+	//d20 hit chart
+        $this->hitChart = array(
+
+            0=> array(
+                    9 => "Structure",
+                    10 => "Jump Engine",
+                    13 => "Scanner",
+                    15 => "Engine",
+                    16 => "Hangar",
+                    19 => "Reactor",
+                    20 => "C&C",
+           		 ),
+            1=> array(
+                    4 => "Thruster",
+                    7 => "Antiproton Gun",
+                    8 => "0:Antimatter Cannon",                    
+                    9 => "0:Antimatter Shredder",
+                    18 => "Structure",
+                    20 => "Primary",
+           		 ),
+            2=> array(
+                    4 => "Thruster",
+                    7 => "Antiproton Gun",
+                    8 => "0:Antimatter Cannon",                    
+                    9 => "0:Antimatter Shredder",
+                    18 => "Structure",
+                    20 => "Primary",
+           		 ),
+            31=> array(
+                    4 => "Thruster",
+                    7 => "Antiproton Gun",
+                    8 => "0:Antimatter Cannon",                    
+                    9 => "0:Antimatter Shredder",
+                    18 => "Structure",
+                    20 => "Primary",
+           		 ),
+            32=> array(
+                    4 => "Thruster",
+                    7 => "Antiproton Gun",
+                    8 => "0:Antimatter Cannon",                    
+                    9 => "0:Antimatter Shredder",
+                    18 => "Structure",
+                    20 => "Primary",
+           		 ),
+            41=> array(
+                    4 => "Thruster",
+                    7 => "Antiproton Gun",
+                    8 => "0:Antimatter Cannon",                    
+                    9 => "0:Antimatter Shredder",
+                    18 => "Structure",
+                    20 => "Primary",
+           		 ),
+       		42=> array(
+                    4 => "Thruster",
+                    7 => "Antiproton Gun",
+                    8 => "0:Antimatter Cannon",                    
+                    9 => "0:Antimatter Shredder",
+                    18 => "Structure",
+                    20 => "Primary",
+           		 ),
+           	);
+       		
+		}
+	}
+		
+?>		
