e7d78e220ce1087c6c63e20ce82dcc18ebda9904
Zero1627 <reisen@poczta.onet.pl>
2021-10-12 10:04:47 +0200

    Vulcan ships

---
+++ source/server/model/ships/ZTrekVulcans/VulcanShuttle.php
@@ -0,0 +1,51 @@
+<?php
+class VulcanShuttle extends FighterFlight{
+    
+    function __construct($id, $userid, $name,  $slot){
+        parent::__construct($id, $userid, $name,  $slot);
+        
+        $this->pointCost = 30 *6; //for 6
+        $this->faction = "ZTrek Playtest Federation";
+        $this->phpclass = "VulcanShuttle";
+        $this->shipClass = "Vulcan Shuttles";
+        $this->imagePath = "img/ships/StarTrek/VulcanShuttle.png";
+		$this->unofficial = true;
+		
+        $this->isd = 2151;
+
+        $this->forwardDefense = 6;
+        $this->sideDefense = 8;
+        $this->freethrust = 8;
+        $this->offensivebonus = 4;
+        $this->jinkinglimit = 4;
+        $this->turncost = 0.33;
+		
+		
+		$this->hangarRequired = "Shuttlecraft"; //I took category name from ST wikis
+		$this->unitSize = 1; //counted as singles
+        
+       	$this->iniativebonus = 75;
+        $this->populate();        
+    }
+    public function populate(){
+        $current = count($this->systems);
+        $new = $this->flightSize;
+        $toAdd = $new - $current;
+        for ($i = 0; $i < $toAdd; $i++){   
+            $armour = array(2, 2, 2, 2);
+            $fighter = new Fighter("VulcanShuttle", $armour, 11, $this->id);
+            $fighter->displayName = "Vulcan Shuttle";
+			
+            $fighter->imagePath = "img/ships/StarTrek/VulcanShuttle.png";
+            $fighter->iconPath = "img/ships/StarTrek/VulcanShuttle_Large.png";
+			
+            $frontGun = new LightParticleBeam(300, 60, 2, 1);
+            $frontGun->displayName = "Ultralight Phase Cannon";
+            $fighter->addFrontSystem($frontGun);
+			
+			$fighter->addAftSystem(new RammingAttack(0, 0, 360, $fighter->getRammingFactor(), 0)); //ramming attack
+            $this->addSystem($fighter);
+        }
+    }
+}
+?>
