3140527e93f5c5d59db72637e7774b6544836471
gstano <geoffrey.stano@gmail.com>
2022-03-21 13:25:00 -0500

    Craytan tweaks

--- source/server/model/ships/ZNexusCraytan/CraytanHepron2085.php
+++ source/server/model/ships/ZNexusCraytan/CraytanHepron2085.php
@@ -14,6 +14,8 @@ class CraytanHepron2085 extends HeavyCombatVessel{
 			$this->occurence = "common";
 		$this->unofficial = true;
         $this->isd = 2085;
+
+        $this->fighters = array("assault shuttles"=>4);
 		
         $this->forwardDefense = 14;
         $this->sideDefense = 15;
