59f6fd924aed4273ffb28bdfc2575bc49bf35211
Zero1627 <reisen@poczta.onet.pl>
2022-05-05 11:28:35 +0200

    tournament prize

--- source/autoload.php
+++ source/autoload.php
@@ -2418,7 +2418,9 @@ spl_autoload_register(
 				'vasy' => '/server/model/ships/raiders/Vasy.php',
 				'vyshesh' => '/server/model/ships/civilians/Vyshesh.php',
 				'xotia' => '/server/model/ships/vree/XotiA.php',
-				'xotib' => '/server/model/ships/vree/XotiB.php',												
+				'xotib' => '/server/model/ships/vree/XotiB.php',	
+		    						
+				'zshadowxonn' => '/server/model/ships/vree/ZShadowXonn.php', //tournament prize
 												
 				//antimatter-specific criticals
                 'reducedrangeantimatter' => '/server/model/cricialClasses.php',
