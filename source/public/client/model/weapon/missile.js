4e788e8ce75eb200667f6d011c940b7e60e22a9a
Zero1627 <reisen@poczta.onet.pl>
2022-08-02 10:50:49 +0200

    working on AmmoMagazine

--- source/public/client/model/weapon/missile.js
+++ source/public/client/model/weapon/missile.js
@@ -131,3 +131,69 @@ var AMissileRack = function AMissileRack(json, ship) {
 };
 AMissileRack.prototype = Object.create(Ballistic.prototype);
 AMissileRack.prototype.constructor = AMissileRack;
+
+
+//this is actually a core (technical) system, not a weapon - but closely cooperates with weapons, hence placed here
+var AmmoMagazine = function AmmoMagazine(json, ship) {
+    ShipSystem.call(this, json, ship);
+};
+AmmoMagazine.prototype = Object.create(ShipSystem.prototype);
+AmmoMagazine.prototype.constructor = AmmoMagazine;
+AmmoMagazine.prototype.doIndividualNotesTransfer = function () { //prepare individualNotesTransfer variable - if relevant for this particular system
+    //transfer every round used as a separate entry, by mode name
+	this.individualNotesTransfer = Array();
+    var ammoKeysArray = Object.keys(this.ammoUseArray);
+    for (var i = 0; i < ammoKeysArray.length; i++) {
+		var currAmmoKey = ammoKeysArray[i];
+        var currAmmoUsed = this.ammoUseArray[currAmmoKey];
+		for (var j=0; j<currAmmoUsed; j++){
+		    this.individualNotesTransfer.push(currAmmoKey);
+        }
+	}
+	return true;
+};
+AmmoMagazine.prototype.doVerifyAmmoUsage = function (currShip) { //verify whether desired ammo usage is sustainable by rounds actually present in magazine
+    var toReturn = true;
+    var totalAmmoRequired = 0;
+    this.ammoUseArray = array();
+    //loop through all weapons for current ship (appropriate to phase) - and check for ammo usage
+    for (var i in currShip.systems) {
+        var currWeapon = currShip.systems[i];
+        if ( (currWeapon.checkAmmoMagazine) //this confirms both that it is a weapon and that it requires ammo
+            && ( ((gamedata.gamephase == 1) && (currWeapon.ballistic)) || ((gamedata.gamephase == 3) && (!currWeapon.ballistic)) ) //appropriate phase - Initial for ballistics, Firing Declaration for direct fire
+        ) {
+            //loop through firing orders
+            for (var fo = 0; fo < currWeapon.fireOrders.length; fo++) {
+                var currFireOrder = currWeapon.fireOrders[fo];
+                var modeName = currWeapon.firingModes[currFireOrder.firingMode];
+                //mark ammo drawn (particular ammo)
+                if(this.ammoUseArray[modeName]){
+                    this.ammoUseArray[modeName] += 1;
+                }else{
+                    this.ammoUseArray[modeName] = 1;
+                }
+                //mark ammo drawn (total capacity)
+                var ammoSize = this.ammoSizeArray[modeName];
+                totalAmmoRequired += ammoSize;
+            }
+        }
+    }
+    //check total usage
+    if (totalAmmoRequired > this.remainingAmmo) toReturn = false;
+    //check every individual kind of missile, too
+    var ammoKeysArray = Object.keys(this.ammoUseArray);
+    for (var a = 0; a < ammoKeysArray.length; a++) {
+        var currAmmoKey = ammoKeysArray[a];
+        var ammoPresent = this.ammoCountArray[currAmmoKey];
+        var ammoRequired = this.ammoUseArray[currAmmoKey];
+        if (ammoPresent < ammoRequired) toReturn = false;
+    }
+    return toReturn;
+};
+
+
+var AmmoMissileRackS = function AmmoMissileRackS(json, ship) {
+    Ballistic.call(this, json, ship);
+};
+AmmoMissileRackS.prototype = Object.create(Ballistic.prototype);
+AmmoMissileRackS.prototype.constructor = AmmoMissileRackS;
