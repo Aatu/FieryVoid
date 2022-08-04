0fa7ced699b9d23f106f8519ed6756980ce63989
Zero1627 <reisen@poczta.onet.pl>
2022-08-04 10:25:41 +0200

    additional AM launchers

--- source/autoload.php
+++ source/autoload.php
@@ -2647,7 +2647,13 @@ spl_autoload_register(
                 		'ammomissilep' => '/server/model/systems/baseSystems.php',
 		    
                 		'ammomissileracks' => '/server/model/weapons/missile.php', //missile racks themselves
-                		'ammomissilerackl' => '/server/model/weapons/missile.php', 	    
+                		'ammomissilerackso' => '/server/model/weapons/missile.php',
+                		'ammomissileracka' => '/server/model/weapons/missile.php',
+                		'ammomissilerackb' => '/server/model/weapons/missile.php',
+                		'ammomissilerackl' => '/server/model/weapons/missile.php',
+                		'ammomissileracklh' => '/server/model/weapons/missile.php',
+                		'ammomissilerackr' => '/server/model/weapons/missile.php',
+                		'ammobombrack' => '/server/model/weapons/missile.php',
 		    
                 		'sagittariusam' => '/server/model/ships/EA/sagittariusAM.php', //ships using new style launchers (and magazine of course)
 		    		'olympusam' => '/server/model/ships/EA/olympusAM.php',
