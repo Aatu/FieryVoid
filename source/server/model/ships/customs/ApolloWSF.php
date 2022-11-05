111b0896ba96ad320b76620ffae8a14118584689
melkorium <64601604+melkorium@users.noreply.github.com>
2022-11-05 22:43:47 +0000

    ssf

--- source/server/model/ships/customs/ApolloWSF.php
+++ source/server/model/ships/customs/ApolloWSF.php
@@ -5,7 +5,7 @@ class ApolloWSF extends BaseShip{
         parent::__construct($id, $userid, $name,  $slot);
 
         $this->pointCost = 900;
-  //      $this->faction = "Custom Ships";
+        $this->faction = "Custom Ships";
         $this->phpclass = "ApolloWSF";
         $this->imagePath = "img/ships/apollo.png";
         $this->shipClass = "Apollo Bombardment Cruiser (Beta)";
