04535a7ea1035466f3dc9497480daaa1b6917623
Zero1627 <reisen@poczta.onet.pl>
2023-01-02 10:41:13 +0100

    Rapid Fire shot

--- source/server/model/cricialClasses.php
+++ source/server/model/cricialClasses.php
@@ -438,3 +438,12 @@ class SensorLoss extends Critical{
     }
 }
 
+
+class RapidFire extends Critical{
+    public $description = "Rapid mode engaged - Fire Control reduced.";
+    function __construct($id, $shipid, $systemid, $phpclass, $turn, $turnend = 0){
+            parent::__construct($id, $shipid, $systemid, $phpclass, $turn, $turnend );
+    }
+}
+
+?>
