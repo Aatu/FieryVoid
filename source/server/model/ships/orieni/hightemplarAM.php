3e1e72444548bd6db4ec1001a7b0b9bff8a11fff
unknown <reisen@poczta.onet.pl>
2023-01-11 08:35:30 +0100

    AmmoMagazine fighters - Orieni

--- source/server/model/ships/orieni/hightemplarAM.php
+++ source/server/model/ships/orieni/hightemplarAM.php
@@ -1,18 +1,17 @@
 <?php
 
-class HighTemplar extends FighterFlight{
+class HighTemplarAM extends FighterFlight{
     
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
         $this->pointCost = 40*6;
         $this->faction = "Orieni";
-        $this->phpclass = "HighTemplar";
+        $this->phpclass = "HighTemplarAM";
         $this->shipClass = "High Templar Interceptor flight";
         $this->imagePath = "img/ships/highTemplar.png";
         
-        //$this->variantOf = "Templar Interceptor flight";
-	        $this->variantOf = 'OBSOLETE'; //awaiting all games it's used in, then is to be removed from active ships list
+        $this->variantOf = "Templar Interceptor flight";
         $this->occurence = "uncommon";
         $this->isd = 2008;
 
@@ -42,8 +41,19 @@ class HighTemplar extends FighterFlight{
             $fighter->imagePath = "img/ships/highTemplar.png";
             $fighter->iconPath = "img/ships/highTemplar_large.png";
 
+
+			//ammo magazine itself (AND its missile options)
+			$ammoMagazine = new AmmoMagazine(2); //pass magazine capacity - actual number of rounds, NOT number of salvoes
+			$fighter->addAftSystem($ammoMagazine); //fit to ship immediately
+			$ammoMagazine->addAmmoEntry(new AmmoMissileFB(), 0); //add basic missile as an option - but do NOT load any actual missiles at this moment - so weapon data is actually filled with _something_!
+			$this->enhancementOptionsEnabled[] = 'AMMO_FB';//add enhancement options for missiles - Class-FB
+			$this->enhancementOptionsEnabled[] = 'AMMO_FL';//add enhancement options for missiles - Class-FL
+			$this->enhancementOptionsEnabled[] = 'AMMO_FH';//add enhancement options for missiles - Class-FH
+			$this->enhancementOptionsEnabled[] = 'AMMO_FY';//add enhancement options for missiles - Class-FY
+			//$this->enhancementOptionsEnabled[] = 'AMMO_FD';//add enhancement options for missiles - Class-FD
+
             $fighter->addFrontSystem(new PairedGatlingGun(330, 30));
-            $fighter->addFrontSystem(new FighterMissileRack(2, 330, 30));
+			$fighter->addFrontSystem(new AmmoFighterRack(330, 30, $ammoMagazine, false)); //$startArc, $endArc, $magazine, $base
 
 			$fighter->addAftSystem(new RammingAttack(0, 0, 360, $fighter->getRammingFactor(), 0)); //ramming attack
 			
