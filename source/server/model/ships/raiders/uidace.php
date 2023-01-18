905113a07c7c69cc022518264d99e99e44b14125
melkorium <64601604+melkorium@users.noreply.github.com>
2023-01-18 21:33:00 +0000

    uid

--- source/server/model/ships/raiders/uidace.php
+++ source/server/model/ships/raiders/uidace.php
@@ -7,7 +7,7 @@ class Uidace extends BaseShip{
   $this->pointCost = 550;
   $this->faction = "Raiders";
         $this->phpclass = "Uidace";
-        $this->imagePath = "img/ships/GaimShamor.png"; //Currently using Gaim version of this hull
+        $this->imagePath = "img/ships/Uidace.png"; //Currently using Gaim version of this hull
         $this->shipClass = "Uid'Ac'e Cruiser";
 //        $this->shipSizeClass = 3;
 		$this->canvasSize = 175;
