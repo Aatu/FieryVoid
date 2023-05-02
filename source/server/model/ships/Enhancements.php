988998f1c057b73344370b9ac13c689fc5ca1b15
Zero1627 <reisen@poczta.onet.pl>
2023-05-02 18:09:25 +0200

    merging with Douglas

--- source/server/model/ships/Enhancements.php
+++ source/server/model/ships/Enhancements.php
@@ -532,7 +532,6 @@ class Enhancements{
 			  $enhPriceStep = 0; //flat rate
 			  $ship->enhancementOptions[] = array($enhID, $enhName,0,$enhLimit, $enhPrice, $enhPriceStep,true);
 		  }
-/*
 		  $enhID = 'AMMO_I'; //Interceptor Missiles
 		  if(in_array($enhID, $ship->enhancementOptionsEnabled)){ //option is enabled
 				$ammoClass = new AmmoMissileI();
@@ -544,7 +543,17 @@ class Enhancements{
 			  $enhPriceStep = 0; //flat rate
 			  $ship->enhancementOptions[] = array($enhID, $enhName,0,$enhLimit, $enhPrice, $enhPriceStep,true);
 		  }
-*/
+		  $enhID = 'AMMO_C'; //Chaff Missiles - -15 to hit
+		  if(in_array($enhID, $ship->enhancementOptionsEnabled)){ //option is enabled
+				$ammoClass = new AmmoMissileC();
+				$ammoSize = $ammoClass->size;
+				$actualCapacity = floor($magazineCapacity/$ammoSize);
+			  $enhName = $ammoClass->enhancementDescription;
+			  $enhLimit = $actualCapacity;		
+			  $enhPrice = $ammoClass->getPrice($ship); 
+			  $enhPriceStep = 0; //flat rate
+			  $ship->enhancementOptions[] = array($enhID, $enhName,0,$enhLimit, $enhPrice, $enhPriceStep,true);
+		  }				  
 		  $enhID = 'AMMO_S'; //Stealth Missiles - Target is hidden
 		  if(in_array($enhID, $ship->enhancementOptionsEnabled)){ //option is enabled
 				$ammoClass = new AmmoMissileS();
@@ -556,7 +565,17 @@ class Enhancements{
 			  $enhPriceStep = 0; //flat rate
 			  $ship->enhancementOptions[] = array($enhID, $enhName,0,$enhLimit, $enhPrice, $enhPriceStep,true);
 		  }
-		  
+		  $enhID = 'AMMO_K'; //Starburst Missiles - Fire in Pulse mode
+		  if(in_array($enhID, $ship->enhancementOptionsEnabled)){ //option is enabled
+				$ammoClass = new AmmoMissileK();
+				$ammoSize = $ammoClass->size;
+				$actualCapacity = floor($magazineCapacity/$ammoSize);
+			  $enhName = $ammoClass->enhancementDescription;
+			  $enhLimit = $actualCapacity;		
+			  $enhPrice = $ammoClass->getPrice($ship); 
+			  $enhPriceStep = 0; //flat rate
+			  $ship->enhancementOptions[] = array($enhID, $enhName,0,$enhLimit, $enhPrice, $enhPriceStep,true);
+		  }		  
 	  } //end of magazine-requiring options
 	  
 	  
@@ -1187,13 +1206,20 @@ class Enhancements{
 						break;
 					case 'AMMO_D': //Light Missile						
 						if($ammoMagazine) $ammoMagazine->addAmmoEntry(new AmmoMissileD(), $enhCount, true); //do notify dependent weapons, too!
+						break;						
+					case 'AMMO_I': //Interceptor Missile 						
+						if($ammoMagazine) $ammoMagazine->addAmmoEntry(new AmmoMissileI(), $enhCount, true); //do notify dependent weapons, too!
 						break;
-//					case 'AMMO_I': //Interceptor Missile						
-//						if($ammoMagazine) $ammoMagazine->addAmmoEntry(new AmmoMissileI(), $enhCount, true); //do notify dependent weapons, too!
-//						break;
+					case 'AMMO_C': //Chaff Missile						
+						if($ammoMagazine) $ammoMagazine->addAmmoEntry(new AmmoMissileC(), $enhCount, true); //do notify dependent weapons, too!
+						break;	
 					case 'AMMO_S': //Stealth Missile						
 						if($ammoMagazine) $ammoMagazine->addAmmoEntry(new AmmoMissileS(), $enhCount, true); //do notify dependent weapons, too!
+						break;
+					case 'AMMO_K': //Starburst Missile						
+						if($ammoMagazine) $ammoMagazine->addAmmoEntry(new AmmoMissileK(), $enhCount, true); //do notify dependent weapons, too!
 						break;						
+						
 				}
 			}
 			
