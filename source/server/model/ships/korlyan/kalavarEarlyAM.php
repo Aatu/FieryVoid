880d4026ac13b63e7052943929322fcb4a743132
Zero1627 <reisen@poczta.onet.pl>
2023-05-02 19:30:49 +0200

    Update kalavarEarlyAM.php

--- source/server/model/ships/korlyan/kalavarEarlyAM.php
+++ source/server/model/ships/korlyan/kalavarEarlyAM.php
@@ -7,7 +7,7 @@ class KalavarEarlyAM extends OSAT{
 		$this->pointCost = 240;
 		$this->faction = "Kor-Lyan";
         $this->phpclass = "KalavarEarlyAM";
-        $this->imagePath = "img/ships/korlyan_kalavar2.png";
+        $this->imagePath = "img/ships/korlyan_kalavar.png";
         $this->shipClass = "Kalavar Orbital Satellite (2220)";
 			$this->occurence = "common";
 			$this->variantOf = 'Kalavar Orbital Satellite (2240)';
@@ -25,15 +25,19 @@ class KalavarEarlyAM extends OSAT{
         $this->iniativebonus = 60; 
 
         //ammo magazine itself (AND its missile options)
-        $ammoMagazine = new AmmoMagazine(40); //pass magazine capacity - 20 rounds per launcher, plus reload rack 80
+        $ammoMagazine = new AmmoMagazine(60); //pass magazine capacity - 20 rounds per launcher, plus reload rack 80
         $this->addPrimarySystem($ammoMagazine); //fit to ship immediately
         $ammoMagazine->addAmmoEntry(new AmmoMissileB(), 40); //add full load of basic missiles
+	    $ammoMagazine->addAmmoEntry(new AmmoMissileI(), 20); //add full load of basic missiles  	      
+
+	    $this->enhancementOptionsEnabled[] = 'AMMO_A';//add enhancement options for other missiles - Class-A
+	    $this->enhancementOptionsEnabled[] = 'AMMO_C';//add enhancement options for other missiles - Class-C        
         $this->enhancementOptionsEnabled[] = 'AMMO_L';//add enhancement options for other missiles - Class-L
         $this->enhancementOptionsEnabled[] = 'AMMO_H';//add enhancement options for other missiles - Class-L
         $this->enhancementOptionsEnabled[] = 'AMMO_F';//add enhancement options for other missiles - Class-L
-        $this->enhancementOptionsEnabled[] = 'AMMO_A';//add enhancement options for other missiles - Class-L
         $this->enhancementOptionsEnabled[] = 'AMMO_P';//add enhancement options for other missiles - Class-P
-	    $this->enhancementOptionsEnabled[] = 'AMMO_S';//add enhancement options for other missiles - Class-P
+	    $this->enhancementOptionsEnabled[] = 'AMMO_S';//add enhancement options for other missiles - Class-K	    	    	    	    	    
+	    $this->enhancementOptionsEnabled[] = 'AMMO_K';//add enhancement options for other missiles - Class-S 	        
 
         $this->addPrimarySystem(new Reactor(3, 5, 0, 0));
         $this->addPrimarySystem(new Scanner(3, 5, 2, 4)); 
@@ -43,7 +47,7 @@ class KalavarEarlyAM extends OSAT{
         $this->addAftSystem(new ProximityLaser(3, 6, 6, 0, 180));
         
         $this->addFrontSystem(new AmmoMissileRackL(3, 0, 0, 270, 90, $ammoMagazine, true)); //$armour, $health (0=auto), $power (0=auto), $startArc, $endArc, $magazine, $base
-        $this->addFrontSystem(new MultiDefenseLauncher(2, 'D', 0, 360, true));
+        $this->addFrontSystem(new AmmoMissileRackD(2, 0, 0, 0, 360, $ammoMagazine, false));
         $this->addFrontSystem(new AmmoMissileRackL(3, 0, 0, 270, 90, $ammoMagazine, true)); //$armour, $health (0=auto), $power (0=auto), $startArc, $endArc, $magazine, $base
         
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
