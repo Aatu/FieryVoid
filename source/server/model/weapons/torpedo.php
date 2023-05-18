28ffdb69204ae8a344a29ae7c122b080e35a2ba8
melkorium <64601604+melkorium@users.noreply.github.com>
2023-05-18 13:33:18 +0100

    ts

--- source/server/model/weapons/torpedo.php
+++ source/server/model/weapons/torpedo.php
@@ -427,7 +427,7 @@ class PsionicTorpedo extends Torpedo{ //Powerful Thirdspace weapon that detonate
 			$this->data["Special"] .= "<br>Ignores armor when dealing damage (Advanced Armor is treated as 2 points less).";	
 			$this->data["Special"] .= "<br>Deals Flash damage and reduces armor of facing section (structure and all systems) by 1-3 points (Advanced Armor is immune).";		
 			$this->data["Special"] .= "<br>Ballistic weapon that can use offensive EW.";
-			$this->data["Special"] .= "<br> Damage can be boosted up to " . $this->maxBoostLevel . " times at " . $this->boostEfficiency . " power per extra point of damage.";			
+			$this->data["Special"] .= "<br> Damage can be boosted up to " . $this->maxBoostLevel . " times at " . $this->boostEfficiency . " power per two extra points of damage.";			
 		    $this->data["Special"] .= "<br>Has +1 modifier to critical hits, and +2 to fighter dropout rolls.";			
 		}
         
