be5037086f2f03507ed0db41a75c4e6775e7d354
unknown <reisen@poczta.onet.pl>
2023-06-06 15:17:00 +0200

    Shock Cannon fix

--- source/server/model/weapons/specialWeapons.php
+++ source/server/model/weapons/specialWeapons.php
@@ -279,27 +279,32 @@ class ShockCannon extends Weapon{
 				if (!WeaponEM::isTargetEMResistant($ship,$system)){ //advanced armor prevents non-damaging EM effects
 					$reactor = $ship->getSystemByName("Reactor");
 					$outputMod = -floor($damage/4);
+	
+$crit = new OutputReduced(-1, $ship->id, $reactor->id, "OutputReduced", $gamedata->turn, null, -6);
+$crit->updated = true;
+$reactor->criticals[] =  $crit;
+
 					//modifying how the critical is applied - Marcin Sawicki 06.06.2023
 					while($outputMod<=-4){
-						$crit = new OutputReduced4(-1, $ship->id, $reactor->id, "OutputReduced", $gamedata->turn, $outputMod);
+						$crit = new OutputReduced4(-1, $ship->id, $reactor->id, "OutputReduced4", $gamedata->turn);
 						$crit->updated = true;
 						$reactor->criticals[] =  $crit;
 						$outputMod += 4;
 					}
 					while($outputMod<=-3){
-						$crit = new OutputReduced3(-1, $ship->id, $reactor->id, "OutputReduced", $gamedata->turn, $outputMod);
+						$crit = new OutputReduced3(-1, $ship->id, $reactor->id, "OutputReduced3", $gamedata->turn);
 						$crit->updated = true;
 						$reactor->criticals[] =  $crit;
 						$outputMod += 3;
 					}
 					while($outputMod<=-2){
-						$crit = new OutputReduced2(-1, $ship->id, $reactor->id, "OutputReduced", $gamedata->turn, $outputMod);
+						$crit = new OutputReduced2(-1, $ship->id, $reactor->id, "OutputReduced2", $gamedata->turn);
 						$crit->updated = true;
 						$reactor->criticals[] =  $crit;
 						$outputMod += 2;
 					}
 					while($outputMod<=-1){
-						$crit = new OutputReduced1(-1, $ship->id, $reactor->id, "OutputReduced", $gamedata->turn, $outputMod);
+						$crit = new OutputReduced1(-1, $ship->id, $reactor->id, "OutputReduced1", $gamedata->turn);
 						$crit->updated = true;
 						$reactor->criticals[] =  $crit;
 						$outputMod += 1;
