1c5fd535b45f8e9eed6c7dddc3eb6ed45a0d6a4b
Zero1627 <reisen@poczta.onet.pl>
2023-06-15 16:13:48 +0200

    layout

--- source/server/model/ships/minbari_protectorate/ProtectorateTigarin.php
+++ source/server/model/ships/minbari_protectorate/ProtectorateTigarin.php
@@ -32,14 +32,14 @@ class ProtectorateTigarin extends BaseShip{
             $this->addPrimarySystem(new Hangar(5, 8));
 
                     // weapons arguments: armor, health, power, start arc, end arc
-            $this->addFrontSystem(new FusionCannon(3, 8, 1, 300, 60));
+            $this->addFrontSystem(new FusionCannon(3, 8, 1, 240, 0));
             $this->addFrontSystem(new FusionCannon(3, 8, 1, 300, 60));
             $this->addFrontSystem(new FusionCannon(3, 8, 1, 300, 60));
             $this->addFrontSystem(new NeutronLaser(4, 10, 6, 300, 60));
             $this->addFrontSystem(new GraviticThruster(4, 25, 0, 6, 1));
             $this->addFrontSystem(new NeutronLaser(4, 10, 6, 300, 60));
             $this->addFrontSystem(new FusionCannon(3, 8, 1, 300, 60));
-            $this->addFrontSystem(new FusionCannon(3, 8, 1, 240, 0));
+            $this->addFrontSystem(new FusionCannon(3, 8, 1, 300, 60));
             $this->addFrontSystem(new FusionCannon(3, 8, 1, 0, 120));
 
             $this->addAftSystem(new FusionCannon(3, 8, 1, 180, 300));
