01e86831c2481374c491dade2f12f09b2e60bf5f
gstano <32845840+gstano@users.noreply.github.com>
2023-07-19 21:35:34 -0500

    Velrax Versiss

--- source/server/model/ships/ZNexusVelrax/VelraxVersissRefit.php
+++ source/server/model/ships/ZNexusVelrax/VelraxVersissRefit.php
@@ -9,8 +9,8 @@ class VelraxVersissRefit extends HeavyCombatVesselLeftRight{
         $this->phpclass = "VelraxVersissRefit";
         $this->imagePath = "img/ships/Nexus/VelraxVersiss.png";
         $this->shipClass = "Versiss Combat Scout (2108 refit)";
-			$this->variantOf = "Versiss Combat Scout";
-			$this->occurence = "common";
+			$this->variantOf = "Versythe Explorer";
+			$this->occurence = "rare";
 	    $this->isd = 2108;
         $this->limited = 10;
         $this->canvasSize = 105;
