3b530889d5422f53021e2615a5523f41d0a930e7
melkorium <64601604+melkorium@users.noreply.github.com>
2023-09-07 10:15:35 +0100

    Revert "Revert "TS Balance""
    This reverts commit e4f8c996a529ec29b0538c4287829de57ed00c2c.

--- source/server/model/ships/thirdspace/ThirdspaceBattleship.php
+++ source/server/model/ships/thirdspace/ThirdspaceBattleship.php
@@ -29,7 +29,7 @@ class ThirdspaceBattleship extends BaseShip{
         $this->rollcost = 6;
         $this->pivotcost = 4;
        
-		$this->iniativebonus = 1 *5;   
+		$this->iniativebonus = 2 *5;   
 		
 		/*Thirdspace use their own enhancement set */		
 		Enhancements::nonstandardEnhancementSet($this, 'ThirdspaceShip');				     
