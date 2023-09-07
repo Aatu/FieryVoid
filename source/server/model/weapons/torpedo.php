cb9a0a099508a3c0bd3e0060c78e058120ae08ea
melkorium <64601604+melkorium@users.noreply.github.com>
2023-09-07 19:09:14 +0100

    Update torpedo.php

--- source/server/model/weapons/torpedo.php
+++ source/server/model/weapons/torpedo.php
@@ -323,7 +323,7 @@ class PsionicTorpedo extends Torpedo{ //Powerful Thirdspace weapon that detonate
         public $boostEfficiency = 2;
         public $maxBoostLevel = 3;  
         
-		public $repairPriority = 6;//priority at which system is repaired (by self repair system); higher = sooner, default 4; 0 indicates that system cannot be repaired                 
+		public $repairPriority = 5;//priority at which system is repaired (by self repair system); higher = sooner, default 4; 0 indicates that system cannot be repaired                 
         
         function __construct($armour, $maxhealth, $powerReq, $startArc, $endArc){
             //maxhealth and power reqirement are fixed; left option to override with hand-written values
