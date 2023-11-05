fa915c599e39d5bc241fb0e30948fae194edd5a4
melkorium <64601604+melkorium@users.noreply.github.com>
2023-11-05 15:51:42 +0000

    Revert "Remove KK part 2"
    This reverts commit 4f6a2e93c41cb372282ae8d40bc938f4933be394.

--- source/server/model/ships/orieni/crusaderAM.php
+++ source/server/model/ships/orieni/crusaderAM.php
@@ -37,7 +37,7 @@ class CrusaderAM extends MediumShip{
         $this->enhancementOptionsEnabled[] = 'AMMO_A';//add enhancement options for other missiles - Class-A
         $this->enhancementOptionsEnabled[] = 'AMMO_C';//add enhancement options for other missiles - Class-C        
         $this->enhancementOptionsEnabled[] = 'AMMO_H';//add enhancement options for other missiles - Class-H
- //       $this->enhancementOptionsEnabled[] = 'AMMO_KK';//add enhancement options for other missiles - Class-KK               
+        $this->enhancementOptionsEnabled[] = 'AMMO_KK';//add enhancement options for other missiles - Class-KK               
         $this->enhancementOptionsEnabled[] = 'AMMO_L';//add enhancement options for other missiles - Class-L
 		//By the Book Orieni should have access to missie types: KK, B, A, H, L, C
 		
