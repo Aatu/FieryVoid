82fca5b2a28bb328fe00baad3719b2a5538b9bd3
melkorium <64601604+melkorium@users.noreply.github.com>
2023-11-05 15:51:35 +0000

    Revert "Remove KK missiles for now"
    This reverts commit 87754c2f3ba26a691d7799b45efc2400133a9af5.

--- source/server/model/ships/orieni/skywatch1810AM.php
+++ source/server/model/ships/orieni/skywatch1810AM.php
@@ -36,9 +36,9 @@ class skywatch1810AM extends OSAT
         $this->enhancementOptionsEnabled[] = 'AMMO_A';//add enhancement options for other missiles - Class-A
   //      $this->enhancementOptionsEnabled[] = 'AMMO_C';//add enhancement options for other missiles - Class-C        
         $this->enhancementOptionsEnabled[] = 'AMMO_H';//add enhancement options for other missiles - Class-H
-//		$this->enhancementOptionsEnabled[] = 'AMMO_KK';               
+		$this->enhancementOptionsEnabled[] = 'AMMO_KK';               
   //      $this->enhancementOptionsEnabled[] = 'AMMO_L';//add enhancement options for other missiles - Class-L
-		//By the Book Orieni should have access to missie types: KK, B, A, H, L, C, but L and C not available until 2005 so not enabled on these older ships.
+		//By the Book Orieni should have access to missie types: KK, B, A, H, L, C, but L and C not available until 2005 so not disabled on these older ships.
 		
 		
 		$this->addPrimarySystem(new Reactor(4, 16, 0, 0));
