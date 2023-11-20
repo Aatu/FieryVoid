d6b9a634afb232dc0c491e76ceb00bb5fbc96855
unknown <reisen@poczta.onet.pl>
2023-11-20 17:44:25 +0100

    game description in game

--- source/public/declarations.php
+++ source/public/declarations.php
@@ -16,6 +16,7 @@ window showing current declarations (fire/EW)
       Side: <input type="button" value="Own" onclick="window.declarations.callOwn();"> <input type="button" value="Enemy" onclick="declarations.callEnemy();"> 
       <br>Content: <input type="button" value="EW" onclick="declarations.callEW();"> <input type="button" value="Fire" onclick="declarations.callFire();"> 
       <br>Display by: <input type="button" value="Source" onclick="declarations.callSource();"> <input type="button" value="Target" onclick="declarations.callTarget();"> 
+	  <br>Game description: <input type="button" value="Show description" onclick="declarations.callGameDescriptionActual();"> 
     </div>
   
     <div id="declarationsActual" class=""> <!-- actual declarations tab, filled on button press -->
