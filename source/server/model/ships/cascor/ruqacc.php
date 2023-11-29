7324d2eb7b9d175528537ac7294db8eed4ad1b4e
Zero1627 <reisen@poczta.onet.pl>
2023-11-29 10:04:14 +0100

    layout

--- source/server/model/ships/cascor/ruqacc.php
+++ source/server/model/ships/cascor/ruqacc.php
@@ -22,14 +22,14 @@ class Ruqacc extends OSAT{
         $this->pivotcost = 0;	
         $this->iniativebonus = 60;
 
-        $this->addPrimarySystem(new IonicLaser(2, 0, 0, 270, 90));
-        $this->addPrimarySystem(new IonicLaser(2, 0, 0, 270, 90));
-        $this->addPrimarySystem(new IonTorpedo(2, 5, 4, 270, 90));
-        $this->addPrimarySystem(new IonTorpedo(2, 5, 4, 270, 90));
-        $this->addPrimarySystem(new DualIonBolter(1, 0, 0, 0, 360));
+        $this->addFrontSystem(new IonicLaser(2, 0, 0, 270, 90));
+        $this->addFrontySystem(new IonicLaser(2, 0, 0, 270, 90));
+        $this->addFrontSystem(new IonTorpedo(2, 5, 4, 270, 90));
+        $this->addFrontSystem(new IonTorpedo(2, 5, 4, 270, 90));
+        $this->addFrontSystem(new DualIonBolter(1, 0, 0, 0, 360));
         $this->addPrimarySystem(new Reactor(3, 6, 0, 0));
         $this->addPrimarySystem(new Scanner(3, 4, 2, 4));   
-        $this->addPrimarySystem(new Thruster(2, 4, 0, 0, 2));
+        $this->addAftSystem(new Thruster(2, 4, 0, 0, 2));
                 
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
         $this->addPrimarySystem(new Structure(3, 28));
@@ -37,12 +37,12 @@ class Ruqacc extends OSAT{
 		$this->hitChart = array(
                 0=> array(
                         8 => "Structure",
-                        10 => "Thruster",
-                        13 => "Ionic Laser",
-                        15 => "Ion Torpedo",
+                        10 => "2:Thruster",
+                        13 => "1:Ionic Laser",
+                        15 => "1:Ion Torpedo",
                         17 => "Scanner",
                         19 => "Reactor",
-                        20 => "Dual Ion Bolter",
+                        20 => "1:Dual Ion Bolter",
                 ),
                 1=> array(
                         20 => "Primary",
