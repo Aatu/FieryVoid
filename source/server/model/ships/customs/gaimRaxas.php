2099873194bd2eb9fbd2e6bab607c365b0d0f999
melkorium <64601604+melkorium@users.noreply.github.com>
2024-01-03 09:54:14 +0000

    Raxas Updates

--- source/server/model/ships/customs/gaimRaxas.php
+++ source/server/model/ships/customs/gaimRaxas.php
@@ -37,7 +37,7 @@ class gaimRaxas extends BaseShip{
 		$this->addPrimarySystem(new CnC(6, 16, 0, 0));
 		$this->addPrimarySystem(new Scanner(5, 16, 3, 6));
 		$this->addPrimarySystem(new Engine(5, 13, 0, 8, 4));
-		$this->addPrimarySystem(new Hangar(5, 14));
+		$this->addPrimarySystem(new JumpEngine(4, 20, 5, 48));
 		$this->addPrimarySystem(new MultiphasedCutterL(3, 0, 0, 0, 360));
 		$this->addPrimarySystem(new MultiphasedCutterL(3, 0, 0, 0, 360));
 		
