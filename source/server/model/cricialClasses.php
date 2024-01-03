05633120228543f3cec6de3c1222b49110fd50ee
melkorium <64601604+melkorium@users.noreply.github.com>
2024-01-03 09:35:14 +0000

    QA Update Crit

--- source/server/model/cricialClasses.php
+++ source/server/model/cricialClasses.php
@@ -465,4 +465,14 @@ class LimpetBore extends Critical{
     }
 } 
 
+//For criticalClass.php		
+class MayOverheat extends Critical { //Critical to allow weapons to roll a critical at the end of following turn if they are at risk of overheating e.g. Quad Array.
+    public $description = "May overheat";
+
+    function __construct($id, $shipid, $systemid, $phpclass, $turn, $turnend = 0) {
+//        if ($turnend == 0) $turnend = $turn + 1;
+        parent::__construct($id, $shipid, $systemid, $phpclass, $turn, $turnend );
+    }
+}
+
 ?>
