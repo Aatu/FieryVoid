ad444b91a1ee2e668d765d576a8f7b741f0919d0
melkorium <64601604+melkorium@users.noreply.github.com>
2024-03-18 21:38:46 +0000

    Update F-Rack code for Stealth calc.

--- source/public/client/model/weapon/missile.js
+++ source/public/client/model/weapon/missile.js
@@ -437,27 +437,32 @@ AmmoMissileRackF.prototype.checkIsInRangeFRack = function (shooter, target, weap
        if(!weapon.hextarget){		
 	        var stealthSystem = shipManager.systems.getSystemByName(target, "stealth");
 
-	        if (stealthSystem && distance > 5 && weapon.ballistic) {
-	            return false;
-	        }
+        if (stealthSystem && distance > 5 && weapon.ballistic && target.flight) { //Shouldn't happen, as fighters with stealth can't be targeted over 5 hexes.
+            return false;
+        }
 
-	        var jammer = shipManager.systems.getSystemByName(target, "jammer");
-			if (jammer)
+       var jammer = shipManager.systems.getSystemByName(target, "jammer");
+			if (jammer || stealthSystem)
 			{
-				//check whether it was enabled last turn... if so, allow missile launch :)
-				if (!shipManager.power.isOfflineOnTurn(target, jammer, (gamedata.turn-1) )){
-					/*Improved/Advanced Sensors effect*/
-					var jammerValue = shipManager.systems.getOutput(target, jammer);
-					if (shipManager.hasSpecialAbility(shooter,"AdvancedSensors") || shipManager.systems.getSystemByName(shooter, "fighteradvsensors")) {
-						jammerValue = 0; //negated
-					} else if (shipManager.hasSpecialAbility(shooter,"ImprovedSensors") || shipManager.systems.getSystemByName(shooter, "fighterimprsensors")) {
-						jammerValue = jammerValue * 0.5; //halved
+			var jammerValue = 0;
+					if (jammer && (!shipManager.power.isOfflineOnTurn(target, jammer, (gamedata.turn-1) ))) {//Jammer exists and was enabled last turn.
+						jammerValue = shipManager.systems.getOutput(target, jammer);
 					}
-					range = range / (1+jammerValue);
-					//range = range / (shipManager.systems.getOutput(target, jammer)+1);
+				var stealthValue = 0;	
+					if (stealthSystem && (mathlib.getDistanceBetweenShipsInHex(shooter, target) > 10 && target.shipSizeClass >= 0)){
+					 stealthValue = shipManager.systems.getOutput(target, stealthSystem);
+					} 
+					
+				if(stealthValue > jammerValue) jammerValue = stealthValue;//larger value is used
+				
+				if (shipManager.hasSpecialAbility(shooter,"AdvancedSensors") || shipManager.systems.getSystemByName(shooter, "fighteradvsensors")) {
+					jammerValue = 0; //negated
+				} else if (shipManager.hasSpecialAbility(shooter,"ImprovedSensors") || shipManager.systems.getSystemByName(shooter, "fighterimprsensors")) {
+					jammerValue = jammerValue * 0.5; //halved
 				}
+				range = range / (1+jammerValue);
 			}
-		}
+		}	
         return distance <= range;
     };	
     
