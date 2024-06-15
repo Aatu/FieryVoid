b556851ce7097f4b00e61dd2e52b70f9c4f20ab5
melkorium <64601604+melkorium@users.noreply.github.com>
2024-06-15 20:24:21 +0100

    EA osats

--- source/server/model/ships/EA/hectorAM.php
+++ source/server/model/ships/EA/hectorAM.php
@@ -44,6 +44,7 @@ class HectorAM extends OSAT{
         $this->addAftSystem(new InterceptorMkI(2, 4, 1, 0, 360));
         //$this->addAftSystem(new InterceptorMkI(2, 4, 1, 0, 360));
 
+        $this->addPrimarySystem(new OSATCnC(0, 1, 0, 0));
         $this->addPrimarySystem(new Reactor(4, 7, 0, 0));
         $this->addPrimarySystem(new Scanner(4, 16, 2, 4));   
 
