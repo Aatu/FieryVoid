3e57a246b06b311acdce38ae68284dee85f326c9
melkorium <64601604+melkorium@users.noreply.github.com>
2024-06-16 11:05:56 +0100

    Update ScionBP.php

--- source/server/model/ships/centauri/ScionBP.php
+++ source/server/model/ships/centauri/ScionBP.php
@@ -1,15 +1,15 @@
 <?php
-class scionbp extends SuperHeavyFighter{
+class scionbp extends FighterFlight{
     
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-        $this->pointCost = 40;
+        $this->pointCost = 40 * 6;
         $this->faction = "Centauri Republic";
         $this->phpclass = "scionbp";
         $this->shipClass = "Scion Breaching Pod";
         $this->imagePath = "img/ships/phalan.png";
-	    //$this->isd = 2220;
+	    $this->isd = 2220;
 
         $this->forwardDefense = 9;
         $this->sideDefense = 8;
@@ -17,33 +17,37 @@ class scionbp extends SuperHeavyFighter{
         $this->offensivebonus = 0; //irrelevant
         $this->jinkinglimit = 0;
         $this->turncost = 0.33;
-
-		$this->hangarRequired = 'breaching pods'; //for fleet check
-        $this->iniativebonus = 9 *5;
         
-        $armour = array(2, 2, 2, 2);
-        $fighter = new Fighter("scionbp", $armour, 16, $this->id);
-        $fighter->displayName = "Scion Breaching Pod";
-	$fighter->imagePath = "img/ships/phalan.png";
-	$fighter->iconPath = "img/ships/phalan_large.png";
-	/* no weapons!
-        $fighter->addFrontSystem(new PairedParticleGun(330, 30, 5));
-        $fighter->addFrontSystem(new FighterMissileRack(6, 330, 30));
-        $fighter->addFrontSystem(new FighterMissileRack(6, 330, 30));
-        
-        $particleBlaster = new ParticleBlaster(0, 0, 0, 330, 30);
-        $particleBlaster->fireControl = array(-4, 0, 0);
-        $particleBlaster->loadingtime = 3;
-        $fighter->addFrontSystem($particleBlaster);
-        */
-			$fighter->addAftSystem(new RammingAttack(0, 0, 360, $fighter->getRammingFactor(), 0)); //ramming attack
-			
-        $this->addSystem($fighter);
+        $this->maxFlightSize = 6;//this is an unusual type of 'fighter', limit flight size.      
+		$this->hangarRequired = 'assault shuttles'; //for fleet check
+		$this->unitSize = 1; 
+		
+//		$this->hangarRequired = 'breaching pods'; //for fleet check
+        $this->iniativebonus = 9 *5;		
+        $this->populate();
     }
 
     public function populate(){
-        return;
+
+        $current = count($this->systems);
+        $new = $this->flightSize;
+        $toAdd = $new - $current;
+
+        for ($i = 0; $i < $toAdd; $i++){
+	        $armour = array(2, 2, 2, 2);
+	        $fighter = new Fighter("Scion", $armour, 16, $this->id);
+	        $fighter->displayName = "Scion Breaching Pod";
+			$fighter->imagePath = "img/ships/phalan.png";
+			$fighter->iconPath = "img/ships/phalan_large.png";
+
+			$fighter->addFrontSystem(new Marines(330, 30, 0, false)); //startarc, endarc, damagebonus, elite.
+            
+			$fighter->addAftSystem(new RammingAttack(0, 0, 360, $fighter->getRammingFactor(), 0)); //ramming attack			
+            $this->addSystem($fighter);
+       }
+       
     }
+
 }
 
 ?>
