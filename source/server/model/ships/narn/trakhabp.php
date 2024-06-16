ad5c3a6050e37d92ba74564b232a1fd09f330905
melkorium <64601604+melkorium@users.noreply.github.com>
2024-06-16 11:06:03 +0100

    Narn bp

--- source/server/model/ships/narn/trakhabp.php
+++ source/server/model/ships/narn/trakhabp.php
@@ -5,11 +5,11 @@ class trakhabp extends FighterFlight{
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-		    $this->pointCost = 80*6;
-		    $this->faction = "Narn Regime";
+		$this->pointCost = 80*6;
+		$this->faction = "Narn Regime";
         $this->phpclass = "trakhabp";
         $this->shipClass = "T'Rakh Armed Breaching Pods";
-		    $this->imagePath = "img/ships/NarnTRakh.png";
+		$this->imagePath = "img/ships/NarnTRakh.png";
         
         $this->forwardDefense = 9;
         $this->sideDefense = 10;
@@ -19,8 +19,14 @@ class trakhabp extends FighterFlight{
         $this->pivotcost = 2; //shuttles have pivot cost higher
         $this->turncost = 0.33;
         
-		$this->hangarRequired = 'breaching pods'; //for fleet check; 
-		    $this->iniativebonus = 8*5;
+        $this->maxFlightSize = 6;//this is an unusual type of 'fighter', limit flight size.      
+		$this->hangarRequired = 'assault shuttles'; //for fleet check
+		$this->unitSize = 1; 
+		
+        $this->occurence = 'common';
+		$this->variantOf = "T'Rakh Breaching Pods";		
+		
+		$this->iniativebonus = 8*5;
       
         $this->populate();
     }
@@ -33,13 +39,14 @@ class trakhabp extends FighterFlight{
         for ($i = 0; $i < $toAdd; $i++){
 			
 			$armour = array(4, 4, 4, 4);
-			$fighter = new Fighter("trakhabp", $armour, 19, $this->id);
+			$fighter = new Fighter("Trakh", $armour, 19, $this->id);
 			$fighter->displayName = "T'Rakh";
 			$fighter->imagePath = "img/ships/NarnTRakh.png";
 			$fighter->iconPath = "img/ships/NarnTRakh_Large.png";
 			
-			
+			$fighter->addFrontSystem(new Marines(330, 30, 0, false)); //startarc, endarc, damagebonus, elite.
 			$fighter->addFrontSystem(new PairedParticleGun(330, 30, 5)); //2 guns d6+5
+			
 			$fighter->addAftSystem(new RammingAttack(0, 0, 360, $fighter->getRammingFactor(), 0)); //ramming attack
 			
 			
---
+++ source/server/model/ships/narn/trakhbp.php
@@ -0,0 +1,56 @@
+<?php
+class trakhbp extends FighterFlight{
+    /*T'Rakh Narn Breaching Pod*/
+    
+    function __construct($id, $userid, $name,  $slot){
+        parent::__construct($id, $userid, $name,  $slot);
+        
+		$this->pointCost = 40*6;
+		$this->faction = "Narn Regime";
+        $this->phpclass = "trakhbp";
+        $this->shipClass = "T'Rakh Breaching Pods";
+		$this->imagePath = "img/ships/NarnTRakh.png";
+        
+        $this->forwardDefense = 9;
+        $this->sideDefense = 10;
+        $this->freethrust = 5;
+        $this->offensivebonus = 0;
+        $this->jinkinglimit = 0;
+        $this->pivotcost = 2; //shuttles have pivot cost higher
+        $this->turncost = 0.33;
+        
+        $this->maxFlightSize = 6;//this is an unusual type of 'fighter', limit flight size.      
+		$this->hangarRequired = 'assault shuttles'; //for fleet check
+		$this->unitSize = 1; 
+		$this->iniativebonus = 8*5;
+      
+        $this->populate();
+    }
+    
+    
+    public function populate(){
+        $current = count($this->systems);
+        $new = $this->flightSize;
+        $toAdd = $new - $current;
+        for ($i = 0; $i < $toAdd; $i++){
+			
+			$armour = array(4, 4, 4, 4);
+			$fighter = new Fighter("Trakh", $armour, 19, $this->id);
+			$fighter->displayName = "T'Rakh";
+			$fighter->imagePath = "img/ships/NarnTRakh.png";
+			$fighter->iconPath = "img/ships/NarnTRakh_Large.png";
+			
+			$fighter->addFrontSystem(new Marines(330, 30, 0, false)); //startarc, endarc, damagebonus, elite.
+			
+			$fighter->addAftSystem(new RammingAttack(0, 0, 360, $fighter->getRammingFactor(), 0)); //ramming attack
+			
+			
+			$this->addSystem($fighter);
+			
+			
+		}
+		
+		
+    }
+}
+?>
