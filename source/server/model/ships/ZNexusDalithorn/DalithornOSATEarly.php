6981d2f0c83d020738a1d671ad14b712a98b4628
Zero1627 <reisen@poczta.onet.pl>
2024-08-02 10:02:27 +0200

    Add files via upload

--- source/server/model/ships/ZNexusDalithorn/DalithornOSATEarly.php
+++ source/server/model/ships/ZNexusDalithorn/DalithornOSATEarly.php
@@ -4,7 +4,7 @@ class DalithornOSATEarly extends OSAT{
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
-		$this->pointCost = 180;
+		$this->pointCost = 165;
 		$this->faction = 'ZNexus Dalithorn Commonwealth';
         $this->phpclass = "DalithornOSATEarly";
         $this->imagePath = "img/ships/Nexus/DalithornOSAT_v2.png";
@@ -25,16 +25,15 @@ class DalithornOSATEarly extends OSAT{
         $this->pivotcost = 0;	
         $this->iniativebonus = 60;
 
-        $this->addPrimarySystem(new OSATCnC(0, 1, 0, 0));
-        $this->addPrimarySystem(new NexusCoilgun(2, 10, 4, 300, 60));
-        $this->addPrimarySystem(new NexusLightGasGun(2, 5, 1, 180, 60));
-        $this->addPrimarySystem(new NexusShatterGun(1, 2, 1, 0, 360));
-        $this->addPrimarySystem(new NexusLightGasGun(2, 5, 1, 300, 180));
-        $this->addPrimarySystem(new NexusCoilgun(2, 10, 4, 300, 60));
+        $this->addFrontSystem(new NexusCoilgun(2, 10, 4, 300, 60));
+        $this->addFrontSystem(new NexusLightGasGun(2, 5, 1, 180, 60));
+        $this->addFrontSystem(new NexusShatterGun(1, 2, 1, 0, 360));
+        $this->addFrontSystem(new NexusLightGasGun(2, 5, 1, 300, 180));
+        $this->addFrontSystem(new NexusCoilgun(2, 10, 4, 300, 60));
         $this->addPrimarySystem(new Reactor(4, 12, 0, 0));
         $this->addPrimarySystem(new Scanner(4, 10, 3, 5));
 		$this->addPrimarySystem(new Magazine(4, 12));
-        $this->addPrimarySystem(new Thruster(3, 8, 0, 0, 2));
+        $this->addAftSystem(new Thruster(3, 8, 0, 0, 2));
                 
         //0:primary, 1:front, 2:rear, 3:left, 4:right;
         $this->addPrimarySystem(new Structure(3, 30));
@@ -42,19 +41,33 @@ class DalithornOSATEarly extends OSAT{
 		$this->hitChart = array(
 			0=> array(
 				7 => "Structure",
-				8 => "Magazine",
-				10 => "Thruster",
-				12 => "Coilgun",
-				14 => "Shatter Gun",
-				16 => "Light Gas Gun",
-				18 => "Scanner",
-				20 => "Reactor",
+				8 => "0:Magazine",
+				10 => "2:Thruster",
+				12 => "1:Coilgun",
+				14 => "1:Shatter Gun",
+				16 => "1:Light Gas Gun",
+				18 => "0:Scanner",
+				20 => "0:Reactor",
 			),
 			1=> array(
-				20 => "Primary",
+				7 => "Structure",
+				8 => "0:Magazine",
+				10 => "2:Thruster",
+				12 => "1:Coilgun",
+				14 => "1:Shatter Gun",
+				16 => "1:Light Gas Gun",
+				18 => "0:Scanner",
+				20 => "0:Reactor",
 			),
 			2=> array(
-				20 => "Primary",
+				7 => "Structure",
+				8 => "0:Magazine",
+				10 => "2:Thruster",
+				12 => "1:Coilgun",
+				14 => "1:Shatter Gun",
+				16 => "1:Light Gas Gun",
+				18 => "0:Scanner",
+				20 => "0:Reactor",
 			),
         );
     }
