7105dcb30aba9b0c3470715273c06ad199fef2b5
melkorium <64601604+melkorium@users.noreply.github.com>
2025-01-19 12:41:46 +0100

    Reduce Plasma Web Notes

--- source/server/model/weapons/plasma.php
+++ source/server/model/weapons/plasma.php
@@ -1693,13 +1693,13 @@ class PakmaraPlasmaWeb extends Weapon implements DefensiveSystem{
 			}else{
 				$this->data["Special"] .= '<br>';
 			}
-			$this->data["Special"] .= 'DEFENSIVE MODE - Target a hex with enemy ships during the Firing Phase.';
-			$this->data["Special"] .= '<br>Automatically hits, applying -10 Intercept against enemy fire from hex, plus -2 damage against Antimatter, Laser and Particle attacks.';
-			$this->data["Special"] .= '<br>To affect Ballistic weapons, target hex from where the shot was launched.';			
-			$this->data["Special"] .= '<br>ANTI-FIGHTER MODE - Creates damaging plasma cloud within 3 hexes of ship, this deals D6+2 Plasma damage to all fighters in hex.';
-			$this->data["Special"] .= '<br>Cloud remains in hex until end of next Movement Phase, damaging any fighters that move through it.';			
-			$this->data["Special"] .= '<br>Anti-Fighter mode requires 1 extra power when fired, either from boosting in Initial Orders or from Plasma Batteries.';
-			$this->data["Special"] .= '<br>NOTE - Plasma Webs used in same mode in the same hex are not cumulative, effects are only apply once.';		 
+			$this->data["Special"] .= 'DEFENSIVE - Fire at hex with enemy ships during the Firing Phase.';
+			$this->data["Special"] .= '<br>Applies -10 Intercept against enemy fire from hex, and -2 damage against Antimatter, Laser and Particle attacks.';
+			$this->data["Special"] .= '<br>For Ballistic weapons, target the hex from where shot was launched.';			
+			$this->data["Special"] .= '<br>ANTI-FIGHTER - Creates a cloud dealing D6+2 Plasma damage to all fighters in hex.';
+			$this->data["Special"] .= '<br>Cloud remains during next Movement Phase, damaging any fighters that move through it.';			
+			$this->data["Special"] .= '<br>Requires 1 extra power, from being boosted or Batteries.';
+			$this->data["Special"] .= '<br>NOTE - Plasma Webs are not cumulative, effects only apply once.';		 
 	 }
 
 
