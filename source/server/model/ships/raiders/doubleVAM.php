d3562789d84096d2bbee757e25275e69ac83e47f
melkorium <64601604+melkorium@users.noreply.github.com>
2025-03-03 15:53:23 +0000

    Add Dogfight Missiles to Double-V

--- source/server/model/ships/raiders/doubleVAM.php
+++ source/server/model/ships/raiders/doubleVAM.php
@@ -48,7 +48,8 @@ class DoubleVAM extends FighterFlight{
 		$ammoMagazine = new AmmoMagazine(2); //pass magazine capacity - actual number of rounds, NOT number of salvoes
 		$fighter->addAftSystem($ammoMagazine); //fit to ship immediately
 		$ammoMagazine->addAmmoEntry(new AmmoMissileFB(), 0); //add basic missile as an option - but do NOT load any actual missiles at this moment - so weapon data is actually filled with _something_!
-		$this->enhancementOptionsEnabled[] = 'AMMO_FB';//add enhancement options for missiles - Class-FB		
+		$this->enhancementOptionsEnabled[] = 'AMMO_FB';//add enhancement options for missiles - Class-FB
+		$this->enhancementOptionsEnabled[] = 'AMMO_FY';//add enhancement options for missiles - Class-FY				
 
 		$fighter->addFrontSystem(new PairedParticleGun(330, 30, 3));
 		$fighter->addFrontSystem(new AmmoFighterRack(330, 30, $ammoMagazine, false)); //$startArc, $endArc, $magazine, $base
