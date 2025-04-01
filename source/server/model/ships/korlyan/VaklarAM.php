fc07d2648fe9568ca76e22aa7a3243b16618bb1c
melkorium <64601604+melkorium@users.noreply.github.com>
2025-04-01 10:48:43 +0200

    Remove 0 ammo entries from Ballistic Mine ships

--- source/server/model/ships/korlyan/VaklarAM.php
+++ source/server/model/ships/korlyan/VaklarAM.php
@@ -32,9 +32,9 @@ class VaklarAM extends HeavyCombatVessel{
 	    $this->addPrimarySystem($ammoMagazine); //fit to ship immediately
 	    $ammoMagazine->addAmmoEntry(new AmmoMissileB(), 200); //add full load of basic missiles 
 	    $ammoMagazine->addAmmoEntry(new AmmoMissileI(), 40); //add full load of basic missiles
-	    $ammoMagazine->addAmmoEntry(new AmmoBLMineB(), 0); //add full load of basic missiles
-	    $ammoMagazine->addAmmoEntry(new AmmoBLMineW(), 0); //add full load of basic missiles 
-	    $ammoMagazine->addAmmoEntry(new AmmoBLMineH(), 0); //add full load of basic missiles 	    	     	      	      
+//	    $ammoMagazine->addAmmoEntry(new AmmoBLMineB(), 0); //add full load of basic missiles
+//	    $ammoMagazine->addAmmoEntry(new AmmoBLMineW(), 0); //add full load of basic missiles 
+//	    $ammoMagazine->addAmmoEntry(new AmmoBLMineH(), 0); //add full load of basic missiles 	    	     	      	      
 
 	    $this->enhancementOptionsEnabled[] = 'AMMO_A';//add enhancement options for other missiles - Class-A
 	    $this->enhancementOptionsEnabled[] = 'AMMO_C';//add enhancement options for other missiles - Class-C
