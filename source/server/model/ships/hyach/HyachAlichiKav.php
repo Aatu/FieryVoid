1875f43d9fe98eadd137a3482ae13591ea49d329
melkorium <64601604+melkorium@users.noreply.github.com>
2025-05-14 10:50:49 +0200

    Update Hyach Subs notes and Turn Cost

--- source/server/model/ships/hyach/HyachAlichiKav.php
+++ source/server/model/ships/hyach/HyachAlichiKav.php
@@ -1,6 +1,8 @@
 <?php
 class HyachAlichiKav extends HeavyCombatVessel{
 
+    public $submarine = true;
+
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
 
@@ -12,11 +14,13 @@ class HyachAlichiKav extends HeavyCombatVessel{
 		$this->canvasSize = 100;
         $this->gravitic = true;
         $this->limited = 33;
+        $this->notes = "Stealth ship (see FAQ)";
+        $this->notes .= "<br>Costs +33% to turn when moving in reverse";        
 		
         $this->forwardDefense = 10;
         $this->sideDefense = 12;
-        $this->turncost = 0.66;
-        $this->turndelaycost = 1;
+        $this->turncost = 1;
+        $this->turndelaycost = 0.66;
         $this->accelcost = 3;
         $this->rollcost = 2;
         $this->pivotcost = 3;
