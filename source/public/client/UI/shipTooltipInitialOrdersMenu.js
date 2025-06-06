5373d9ca3637b88b3b2793cd9b36addfb3f1719e
melkorium <64601604+melkorium@users.noreply.github.com>
2025-06-06 12:02:22 +0200

    Remove 'remove OEW' button if ship has no OEW in place

--- source/public/client/UI/shipTooltipInitialOrdersMenu.js
+++ source/public/client/UI/shipTooltipInitialOrdersMenu.js
@@ -13,7 +13,7 @@ window.ShipTooltipInitialOrdersMenu = function () {
         { className: "addCCEW", condition: [isSelf, notFlight], action: addCCEW, info: "Add CCEW" }, 
         { className: "removeCCEW", condition: [isSelf, notFlight, hasCCEW], action: removeCCEW, info: "Remove CCEW" }, 
         { className: "addOEW", condition: [isEnemy, sourceNotFlight], action: addOEW, info: "Add OEW" }, 
-        { className: "removeOEW", condition: [isEnemy, sourceNotFlight], action: removeOEW, info: "Remove OEW" }, 
+        { className: "removeOEW", condition: [isEnemy, sourceNotFlight, hasOEW], action: removeOEW, info: "Remove OEW" }, 
         { className: "addDIST", condition: [isEnemy, isElint, notFlight, isInElintDistance(30), doesNotHaveBDEW, advSensorsCheck], action: getAddOEW('DIST'), info: "Add DIST" }, 
         { className: "removeDIST", condition: [isEnemy, isElint, notFlight, isInElintDistance(30), doesNotHaveBDEW, advSensorsCheck, hasDIST], action: getRemoveOEW('DIST'), info: "Remove DIST" }, 
         { className: "addSOEW", condition: [isFriendly, isElint, notFlight, notSelf, isInElintDistance(30), doesNotHaveBDEW], action: getAddOEW('SOEW'), info: "Add SOEW" }, 
