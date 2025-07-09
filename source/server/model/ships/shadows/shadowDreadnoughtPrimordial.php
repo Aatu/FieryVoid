7990f0bf4340f5afe460a423ea1f6d7ba7c20673
melkorium <64601604+melkorium@users.noreply.github.com>
2025-07-09 15:00:11 +0200

    Grammar!

--- source/server/model/ships/shadows/shadowDreadnoughtPrimordial.php
+++ source/server/model/ships/shadows/shadowDreadnoughtPrimordial.php
@@ -190,7 +190,7 @@ class ShadowDreadnoughtPrimordial extends MediumShip{
         $this->addPrimarySystem(new Structure( 6, 72));
 		
 		/*systems on Shadow ships CANNOT be targeted by called shots!*/
-		$this->notes .= "<br>cannot be targeted by called shots.";
+		$this->notes .= "<br>Cannot be targeted by called shots.";
 		foreach ($this->systems as $sys){
 			$sys->isPrimaryTargetable = false; 
 			$sys->isTargetable = false; //cannot be targeted ever!
