80619b7543299b0582ae0744b669fa93e7293abb
melkorium <64601604+melkorium@users.noreply.github.com>
2025-07-17 10:09:24 +0200

    Update links

--- source/public/games.php
+++ source/public/games.php
@@ -37,7 +37,7 @@ if (isset($_SESSION["user"])) {
   </script>
 </head>
 
-<body  style="background: url('./img/webBackgrounds/lp8.jpg') no-repeat center center fixed; background-size: cover;">
+<body  style="background: url('./img/webBackgrounds/games.jpg') no-repeat center center fixed; background-size: cover;">
 <header class="header">
   <img src="img/logo.png" alt="Fiery Void Logo" class="logo">
   <div class="top-right-row">
@@ -66,7 +66,7 @@ if (isset($_SESSION["user"])) {
       <h3>Rules & Info</h3>
       <div class="links">     
         <div><a href="files/FV_factions.txt" target="_blank" rel="noopener noreferrer">Factions & Tiers:</a> Overview of Fiery Void factions and their approximate strengths.</div>
-        <div><a href="files/FV_FAQ.txt" target="_blank" rel="noopener noreferrer">Fiery Void FAQ:</a> Aide Memoire of rules and differences from Babylon 5 Wars.</div>
+        <div><a href="./faq.php" target="_blank" rel="noopener noreferrer">Fiery Void FAQ:</a> Aide Memoire of rules and differences from Babylon 5 Wars.</div>
         <div><a href="files/enhancements_list.txt" target="_blank" rel="noopener noreferrer">Systems & Enhancements:</a> Details of common systems and unit enhancements e.g. Boarding Actions / Missiles.</div>
         <div><a href="http://b5warsvault.wikidot.com/" target="_blank" rel="noopener noreferrer">Babylon 5 Wars Vault:</a> Huge amount of Babylon 5 Wars rules and info!</div>
       </div> 
