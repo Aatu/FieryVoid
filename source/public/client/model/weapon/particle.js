f4baa7d4b780249be28848936fc1ed1c56919172
melkorium <64601604+melkorium@users.noreply.github.com>
2025-12-26 13:12:35 +0100

    Add Sustained Target Tracker to weapon tooltips

--- source/public/client/model/weapon/particle.js
+++ source/public/client/model/weapon/particle.js
@@ -340,6 +340,19 @@ var ParticleCutter = function ParticleCutter(json, ship) {
 ParticleCutter.prototype = Object.create(Particle.prototype);
 ParticleCutter.prototype.constructor = ParticleCutter;
 
+ParticleCutter.prototype.initializationUpdate = function() {
+    var ship = this.ship;
+	if(shipManager.power.isOverloading(ship, this) && Object.keys(this.sustainedTarget).length > 0){
+        const targetId = Object.keys(this.sustainedTarget)[0];
+        const target = gamedata.getShip(targetId);
+		this.data["Current Target"] = target.name;
+	}else{
+        delete this.data["Current Target"];      
+    }
+
+	return this;
+};
+
 var SolarCannon = function SolarCannon(json, ship) {
     Particle.call(this, json, ship);
 };
