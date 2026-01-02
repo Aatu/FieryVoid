c51fbb39c90ca57b6ff1da14eda990f8fec0bc0d
melkorium <64601604+melkorium@users.noreply.github.com>
2026-01-02 16:09:58 +0100

    Update games.php

--- source/public/games.php
+++ source/public/games.php
@@ -76,8 +76,8 @@ $games = json_encode(Manager::getTacGames($userid), JSON_NUMERIC_CHECK);
 <div class="resources">
       <h3>Rules & Info</h3>
       <div class="links">     
+        <div><a href="./faq.php" target="_blank" rel="noopener noreferrer">Fiery Void FAQ:</a> Aide Memoire of specific rules and differences from Babylon 5 Wars.</div>        
         <div><a href="./factions-tiers.php" target="_blank" rel="noopener noreferrer">Fiery Void: Factions & Tiers:</a> Overview of Fiery Void factions and their relative strengths.</div>
-        <div><a href="./faq.php" target="_blank" rel="noopener noreferrer">Fiery Void FAQ:</a> Aide Memoire of specific rules and differences from Babylon 5 Wars.</div>
         <div><a href="./ammo-options-enhancements.php" target="_blank" rel="noopener noreferrer">Ammo, Options & Enhancements:</a> Details of all the extras available to Fiery Void units e.g. Missiles.</div>
         <div><a href="http://b5warsvault.wikidot.com/" target="_blank" rel="noopener noreferrer">Babylon 5 Wars Vault:</a> Huge repository of Babylon 5 Wars rules and info!</div>
       </div> 
