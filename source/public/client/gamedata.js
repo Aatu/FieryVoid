fd298f87cf4a5ac040b60b2b5af429ffc70efc46
Mathieas-1 <jonathan.vandyke@gmail.com>
2026-01-10 18:45:49 -0800

    Will now hide UI once commit button is pressed.

--- source/public/client/gamedata.js
+++ source/public/client/gamedata.js
@@ -545,6 +545,8 @@ window.gamedata = {
         }
 
         else if (gamedata.gamephase == 2) {
+            UI.shipMovement.hide();
+            
             var zeroSpeedShips = [];
             var activeShips = gamedata.getActiveShips();
             var html = '';
@@ -1085,14 +1087,7 @@ window.gamedata = {
                     foundNMShip = true;                   
                 }
 
-            }
-
-             if (foundNMShip) {
-                mustMoveError += "<br>You need to finish movement before you can commit the turn.";
-                window.confirm.error(mustMoveError, function () { });
-                return false;
-            }
-           
+            }                     
             if (foundPShip) {
                 mustPivotError += "<br>You need to order them to pivot.";
                 window.confirm.error(mustPivotError, function () { });
@@ -1102,6 +1097,12 @@ window.gamedata = {
             if (foundTShip) {
                 negThrustError += "<br>You need to lower channelled thrust before you can commit the turn.";
                 window.confirm.error(negThrustError, function () { });
+                return false;
+            }
+            
+            if (foundNMShip) {
+                mustMoveError += "<br>You need to finish movement before you can commit the turn.";
+                window.confirm.error(mustMoveError, function () { });
                 return false;
             }
            
