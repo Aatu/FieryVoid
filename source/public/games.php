e1143cb23d6c645305fb66d876b3faa7a4f472dc
melkorium <64601604+melkorium@users.noreply.github.com>
2026-01-19 14:38:52 +0100

    Lllort joke

--- source/public/games.php
+++ source/public/games.php
@@ -110,7 +110,9 @@ $defaultGameName = ucfirst($playerName) . "'s Game";
         <li><strong>Ship Selection</strong> - Due to above changes, in certain phases you will now need to double-click with left mouse button to select a ship.</li> 
         <li><strong>Touchscreen Improvements</strong> - Line of Sight tool and Hit Chart displays should now work on mobile/tablet devices.</li>                                               
         <li><strong>Escalation/Nexus Updates</strong> - Several Nexus fixes and a new Gravitic Tracting Rod weapon added for Chouka! Thanks to Geoffrey!</li>  
-        <li><strong>Torvalus Stilettos</strong> - Jammer ability removed following rules clarification.</li>                                                            
+        <li><strong>Torvalus Stilettos</strong> - Jammer ability removed following rules clarification.</li> 
+        <li><strong>Drazi Solar Cannon</strong> - Should now dealt correct damage to ships with damge mitigating systems e.g. Shadow Diffusers.</li> 
+        <li><strong>The Lllort</strong> - Third 'l' added to Lllort faction name..</li>                                                                               
         <li><strong>General Fixes</strong> - Further database optimisation and server stability fixes. Several bug fixes/improvements. Thanks for the reports!</li>                                                    
         <!--<li><strong>6 Jun</strong> - Overlay colors, deployment zone tweaks, UI fixes. Pulsar mine fixed, tooltip/text readability improved.</li>-->
       </ul>
