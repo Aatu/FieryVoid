ea6c32bc540e765bd5d0ee9eef0ff36279cd54fb
melkorium <64601604+melkorium@users.noreply.github.com>
2026-03-03 20:57:09 +0100

    Remove mines from list for now

--- source/server/model/ships/korlyan/captorMineKLB.php
+++ source/server/model/ships/korlyan/captorMineKLB.php
@@ -10,7 +10,7 @@ class captorMineKLB extends Mine{
         $this->imagePath = "img/ships/korlyan_mine.png";
         $this->shipClass = "Kovost Captor Mine";
 		$this->occurence = "common";
-		//$this->variantOf = 'Kalavar Orbital Satellite (2240)';
+		$this->variantOf = 'NONE';
         $this->isd = 2208;
         
         $this->forwardDefense = 12;
