def0e2c3744dca27dc7d8d4c8becaa70bd965e87
melkorium <64601604+melkorium@users.noreply.github.com>
2026-03-12 09:25:01 +0100

    shield arc fix

--- source/public/client/game.legacy.bundle.js
+++ source/public/client/game.legacy.bundle.js
@@ -3108,7 +3108,7 @@ window.ShipIcon = function () {
 
 
     ShipIcon.prototype.showWeaponArc = function (ship, weapon) {
-        if (!(weapon instanceof Weapon && weapon instanceof Thruster && weapon instanceof Shield)) return null; // Only show arcs for weapons
+        if (!(weapon instanceof Weapon) && !(weapon instanceof Thruster) && !(weapon instanceof Shield)) return null; // Only show arcs for weapons
 
         var hexDistance = window.coordinateConverter.getHexDistance();
 
