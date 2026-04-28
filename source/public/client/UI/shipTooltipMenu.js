b3d435bd9c566e316302d0788811022a109465ef
melkorium <64601604+melkorium@users.noreply.github.com>
2026-04-28 11:48:03 +0200

    Touch screen support for new EW button right-click functionality

--- source/public/client/UI/shipTooltipMenu.js
+++ source/public/client/UI/shipTooltipMenu.js
@@ -33,12 +33,13 @@ window.ShipTooltipMenu = function () {
         }, this).forEach(function (buttonData) {
             var element = jQuery(ShipTooltipMenu.buttonTemplate);
             element.addClass(buttonData.className);
-            element.on('click', onClick.bind(this, shipTooltip, buttonData.action, false));
-            if (buttonData.supportsMaxClick) {
-                element.on('contextmenu', onClick.bind(this, shipTooltip, buttonData.action, true));
-            } else {
-                element.on('contextmenu', suppressContextMenu);
-            }
+            element.css({
+                'touch-action': 'manipulation',
+                '-webkit-touch-callout': 'none',
+                '-webkit-user-select': 'none',
+                'user-select': 'none'
+            });
+            bindButton.call(this, element, shipTooltip, buttonData.action, buttonData.supportsMaxClick);
             element.on('mouseover', getMouseOver.call(this, menu, buttonData.info));
             element.on('mouseout', mouseOut.bind(this, menu));
             jQuery(".action-buttons", menu).append(element);
@@ -64,6 +65,61 @@ window.ShipTooltipMenu = function () {
     ShipTooltipMenu.prototype.addButton = function (className, condition, action, info) {
         this.extraButtons.push({ className: className, condition: condition, action: action, info: info });
     };
+
+    var LONG_PRESS_MS = 500;
+
+    function bindButton(element, shipTooltip, action, supportsMaxClick) {
+        var self = this;
+        var longPressTimer = null;
+        var suppressNextClick = false;
+
+        function clearLongPress() {
+            if (longPressTimer) {
+                clearTimeout(longPressTimer);
+                longPressTimer = null;
+            }
+        }
+
+        element.on('click', function (event) {
+            if (suppressNextClick) {
+                suppressNextClick = false;
+                event.preventDefault();
+                event.stopPropagation();
+                return;
+            }
+            onClick.call(self, shipTooltip, action, false, event);
+        });
+
+        if (!supportsMaxClick) {
+            element.on('contextmenu', suppressContextMenu);
+            return;
+        }
+
+        element.on('contextmenu', function (event) {
+            clearLongPress();
+            suppressNextClick = false;
+            onClick.call(self, shipTooltip, action, true, event);
+        });
+
+        element.on('touchstart', function () {
+            clearLongPress();
+            suppressNextClick = false;
+            longPressTimer = setTimeout(function () {
+                longPressTimer = null;
+                suppressNextClick = true;
+                action.call(self, true);
+            }, LONG_PRESS_MS);
+        });
+
+        element.on('touchend', function (event) {
+            clearLongPress();
+            if (suppressNextClick) {
+                event.preventDefault();
+            }
+        });
+
+        element.on('touchmove touchcancel', clearLongPress);
+    }
 
     function onClick(shipTooltip, action, isMaxClick, event) {
         event.preventDefault();
