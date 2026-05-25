2af09af2e606e5c9b2fa3fb281d56ee455a9c148
melkorium <64601604+melkorium@users.noreply.github.com>
2026-05-25 17:05:00 +0200

    Some EA hangar outputs

--- source/server/model/ships/EA/hyperionAlpha.php
+++ source/server/model/ships/EA/hyperionAlpha.php
@@ -29,7 +29,7 @@ class HyperionAlpha extends BaseShip{
         $this->addPrimarySystem(new CnC(4, 16, 0, 0));
         $this->addPrimarySystem(new Scanner(4, 16, 3, 6));
         $this->addPrimarySystem(new Engine(4, 17, 0, 6, 4));
-        $this->addPrimarySystem(new Hangar(4, 8));
+        $this->addPrimarySystem(new Hangar(4, 8, 8));
         $this->addPrimarySystem(new StdParticleBeam(2, 4, 1, 0, 360));
         $this->addPrimarySystem(new StdParticleBeam(2, 4, 1, 0, 360));
         $this->addPrimarySystem(new StdParticleBeam(2, 4, 1, 0, 360));
--- source/server/model/ships/EA/hyperionAssault.php
+++ source/server/model/ships/EA/hyperionAssault.php
@@ -30,7 +30,7 @@ class HyperionAssault extends BaseShip{
         $this->addPrimarySystem(new CnC(5, 16, 0, 0));
         $this->addPrimarySystem(new Scanner(5, 16, 3, 6));
         $this->addPrimarySystem(new Engine(6, 16, 0, 6, 4));
-        $this->addPrimarySystem(new Hangar(4, 16));
+        $this->addPrimarySystem(new Hangar(4, 16, 2));
         $this->addPrimarySystem(new StdParticleBeam(2, 4, 1, 0, 360));
         $this->addPrimarySystem(new StdParticleBeam(2, 4, 1, 0, 360));
         $this->addPrimarySystem(new StdParticleBeam(2, 4, 1, 0, 360));
