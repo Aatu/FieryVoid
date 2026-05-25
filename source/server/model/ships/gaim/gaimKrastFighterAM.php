dd039d6b17247126b2b9fe138d516333559bff04
melkorium <64601604+melkorium@users.noreply.github.com>
2026-05-25 16:55:04 +0200

    Krast change to AM

--- source/server/model/ships/gaim/gaimKrastFighterAM.php
+++ source/server/model/ships/gaim/gaimKrastFighterAM.php
@@ -1,15 +1,15 @@
 <?php
-class gaimKrastFighter extends FighterFlight{
+class gaimKrastFighterAM extends FighterFlight{
     
     function __construct($id, $userid, $name,  $slot){
         parent::__construct($id, $userid, $name,  $slot);
         
         $this->pointCost = 37*6;
         $this->faction = "Gaim Intelligence";
-
-        $this->phpclass = "gaimKrastFighter";
+        $this->variantOf = "OBSELETE";
+        $this->phpclass = "gaimKrastFighterAM";
         $this->shipClass = "Krast Recon flight";
-            $this->variantOf = "OBSELETE";
+        $this->variantOf = "Koist Medium flight";
         $this->imagePath = "img/ships/GaimKoist.png";
 		
         $this->isd = 2252;
@@ -40,13 +40,16 @@ class gaimKrastFighter extends FighterFlight{
             $fighter->imagePath = "img/ships/GaimKoist.png";
             $fighter->iconPath = "img/ships/GaimKoist_large.png";
 
+			//ammo magazine itself (AND its missile options)
+			$ammoMagazine = new AmmoMagazine(2); //pass magazine capacity - actual number of rounds, NOT number of salvoes
+			$fighter->addAftSystem($ammoMagazine); //fit to ship immediately
+			$ammoMagazine->addAmmoEntry(new AmmoMissileFY(), 0); //add basic missile as an option - but do NOT load any actual missiles at this moment - so weapon data is actually filled with _something_!
+			$this->enhancementOptionsEnabled[] = 'AMMO_FY';//add enhancement options for missiles - Class-FY
+
 			$gun = new PairedParticleGun(330, 30, 2);
 			$gun->displayName = "Light Particle Gun";
 			$fighter->addFrontSystem($gun);
-            	$missileRack = new FighterMissileRack(2, 330, 30);
-            	$missileRack->firingModes = array( 1 => "FY" );
-            	$missileRack->missileArray = array( 1 => new MissileFY(330, 30) );
-			$fighter->addFrontSystem($missileRack);	
+			$fighter->addFrontSystem(new AmmoFighterRack(330, 30, $ammoMagazine, false)); //$startArc, $endArc, $magazine,
 			
 			
 			$fighter->addAftSystem(new RammingAttack(0, 0, 360, $fighter->getRammingFactor(), 0)); //ramming attack			
