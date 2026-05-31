f33c2a5ba9d7993fc78bc6ac86039ccc3019102b
melkorium <64601604+melkorium@users.noreply.github.com>
2026-06-01 00:03:45 +0200

    Update gaimOssari.php

--- source/server/model/ships/gaim/gaimOssari.php
+++ source/server/model/ships/gaim/gaimOssari.php
@@ -31,7 +31,7 @@ class gaimOssari extends BaseShip{
         $this->addPrimarySystem(new Scanner(5, 16, 3, 6));
         $this->addPrimarySystem(new Engine(5, 16, 0, 8, 3));
         $this->addPrimarySystem(new JumpEngine(5, 20, 4, 24));
-		$this->addPrimarySystem(new Hangar(4, 4));
+		$this->addPrimarySystem(new Hangar(4, 4, 2));
 		$this->addPrimarySystem(new StdParticleBeam(3, 4, 1, 0, 360));
 
 		$this->addFrontSystem(new Thruster(4, 10, 0, 3, 1));
