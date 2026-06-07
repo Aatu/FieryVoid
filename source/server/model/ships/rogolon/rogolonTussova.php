a8c82b2e6c71c3678d37c6543840ec14e108af68
melkorium <64601604+melkorium@users.noreply.github.com>
2026-06-07 14:02:43 +0200

    Rogolon Hangar fixes

--- source/server/model/ships/rogolon/rogolonTussova.php
+++ source/server/model/ships/rogolon/rogolonTussova.php
@@ -29,7 +29,7 @@ class RogolonTussova extends HeavyCombatVessel{
         $this->addPrimarySystem(new CnC(4, 16, 0, 0));
         $this->addPrimarySystem(new Scanner(4, 12, 4, 6));
         $this->addPrimarySystem(new Engine(4, 10, 0, 8, 3));
-		$this->addPrimarySystem(new Hangar(4, 3));
+		$this->addPrimarySystem(new Hangar(4, 3, 1));
         $this->addPrimarySystem(new Thruster(4, 15, 0, 4, 3));
         $this->addPrimarySystem(new Thruster(4, 15, 0, 4, 4));
         
