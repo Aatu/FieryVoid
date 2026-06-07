80ce0e3236e6e8a2a898f40850d81816b4df8716
melkorium <64601604+melkorium@users.noreply.github.com>
2026-06-07 14:02:46 +0200

    Update MitocJomic.php

--- source/server/model/ships/smallraces/MitocJomic.php
+++ source/server/model/ships/smallraces/MitocJomic.php
@@ -26,7 +26,7 @@ class MitocJomic extends MediumShip{
 		$this->addPrimarySystem(new CnC(5, 9, 0, 0));
 		$this->addPrimarySystem(new Scanner(4, 10, 4, 5));
 		$this->addPrimarySystem(new Engine(4, 11, 0, 6, 1));
-		$this->addPrimarySystem(new Hangar(3, 2));
+		$this->addPrimarySystem(new Hangar(3, 2,1));
 		$this->addPrimarySystem(new Thruster(4, 10, 0, 3, 3));
 		$this->addPrimarySystem(new Thruster(4, 10, 0, 3, 4));
 
