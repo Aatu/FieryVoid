b459f807fa5e173d87e3eec2484a52d61c915b60
melkorium <64601604+melkorium@users.noreply.github.com>
2026-08-17 19:50:08 +0200

    Update games.php

--- source/public/games.php
+++ source/public/games.php
@@ -153,10 +153,10 @@ $defaultGameName = ucfirst($playerName) . "'s Game";
       <ul class="updates-list">
         <!--<li style="--update-colour: #e05b52;"><span class="update-title">Merry Christmas from Fiery Void!</span></li>-->
         <li><span class="update-title">THE KIRISHIAC LORDS</span>A brand-new Ancient faction has been added to Fiery Void!</li>
-        <li><span class="update-title">Pre-Battle Damage & Saved Fleets</span>You now add battle damage to fleets before a game, and save damaged fleets from live games.</li>          
-        <li><span class="update-title">Vorlon Planet Killer</span>Now might not be the best time to visit Centauri Prime...</li>          
-        <li><span class="update-title">Chameleon Sensor Suite</span>The Centauri Dargan Strike Cruiser can now choose to disguise itself as another ship in Fleet Selection.</li> 
-        <li><span class="update-title">Ship Windows & Arcs</span>Ship windows (SCS) and Weapon Arc icons have been updated to improve appearance and functionality.</li>               
+        <li><span class="update-title">Pre-Battle Damage & Saved Fleets</span>You now add battle damage to fleets before a game, and save damaged fleets from live games.</li>                  
+        <li><span class="update-title">System Enhancements</span>New system-level enhancements such as Gunsights now available to choose in Fleet Selection.</li>   
+        <li><span class="update-title">Select Ships Menu</span>Improved menu / icons for when selecting on a hex contianing multiple units.<li>
+        <li><span class="update-title">Player Chat</span>Chat panels refreshed and some emoji options added.<li>                             
         <li><span class="update-title">Discord Notifications</span>You can now add your Discord account details in 'Set-Up Discord Notifications' to get a message when it's your turn. See Fiery Void FAQ for details!</li>
         <!--<li><span class="update-title">General Fixes</span>Many other small bug fixes/updates. Thanks for the reports!</li>-->
       </ul>
