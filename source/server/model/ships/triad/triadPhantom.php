feac109a5fb71163e423b15878415463d3be174b
gstano <geoffrey.stano@gmail.com>
2026-08-18 20:22:40 -0500

    Triad draft

---
+++ source/server/model/ships/triad/triadPhantom.php
@@ -0,0 +1,59 @@
+<?php
+class triadPhantom extends FighterFlight {
+
+    function __construct($id, $userid, $name, $slot) {
+        parent::__construct($id, $userid, $name, $slot);
+
+        $this->pointCost = 390; 
+        $this->faction = "Custom Ships"; 
+        $this->phpclass = "triadPhantom";
+        $this->shipClass = "Neutrality: Phantom Medium Fighter";
+        $this->imagePath = "img/ships/novaSF.png";
+        $this->isd = 'Primordial';
+		$this->factionAge = 4; //1 - Young, 2 - Middleborn, 3 - Ancient, 4 - Primordial
+
+        $this->forwardDefense = 6;
+        $this->sideDefense = 7;
+        $this->freethrust = 15;
+        $this->offensivebonus = 6; // max 6 fighters; updated dynamically
+        $this->jinkinglimit = 8;
+        $this->turncost = 0.33;
+        $this->iniativebonus = 100;
+
+        $this->populate();
+    }
+
+    public function populate() {
+        $current = count($this->systems);
+        $new = $this->flightSize;
+        $toAdd = $new - $current;
+
+        for ($i = 0; $i < $toAdd; $i++) {
+            $armour = array(3, 4, 4, 4); // no armor sections
+            $fighter = new Fighter("Phantom", $armour, 9, $this->id);
+            $fighter->displayName = "Phantom";
+            $fighter->imagePath = "img/ships/novaSF.png";
+            $fighter->iconPath = "img/ships/NovaSF_Large.png";
+
+            $gun = new MatterBolt(330, 30, 0);
+            $gun->displayName = "Matter Bolt";
+            $fighter->addFrontSystem($gun);
+            $fighter->addAftSystem(new RammingAttack(0, 0, 360, $fighter->getRammingFactor(), 0));
+
+            $this->addSystem($fighter);
+        }
+    }
+
+    public function initializationUpdate($gamedata) {
+        parent::initializationUpdate($gamedata);
+
+        // Update offensive bonus dynamically based on alive fighters
+        $alive = 0;
+        foreach ($this->systems as $fighter) {
+            if ($fighter !== null && !$fighter->isDestroyed()) {
+                $alive++;
+            }
+        }
+        $this->offensivebonus = $alive;
+    }
+}
