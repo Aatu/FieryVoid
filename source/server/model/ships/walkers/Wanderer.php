0f5ffe967ae6709a986275c6b556e0a4230f75e2
melkorium <64601604+melkorium@users.noreply.github.com>
2026-09-20 17:49:07 +0200

    Update Wanderer.php

--- source/server/model/ships/walkers/Wanderer.php
+++ source/server/model/ships/walkers/Wanderer.php
@@ -9,7 +9,7 @@ class Wanderer extends BaseShip{
         $this->phpclass = "Wanderer";
         $this->shipClass = "The Wanderer";
         $this->imagePath = "img/ships/WalkerTraveler.png";
-        $this->canvasSize = 225;
+        $this->canvasSize = 250;
 	    $this->isd = 'Ancient';
         $this->shipSizeClass = 3; 
 		$this->factionAge = 3; //1 - Young, 2 - Middleborn, 3 - Ancient, 4 - Primordial
